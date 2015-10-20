51d0e483e53ebc051ecd17e5d686d0c3bf4bc9be
jeremydmiller <jeremydmiller@yahoo.com>
2015-10-20 08:15:42 -0500

    new query syntax w/ question marks for later

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Postgresql as a Document Db and Event Store for .Net Development")]
 [assembly: AssemblyProduct("StructureMap")]
 [assembly: AssemblyCopyright("Copyright 2015 Jeremy D. Miller et al. All rights reserved.")]
-[assembly: AssemblyTrademark("4e5c31398fc801802569bafc8770292ee917f508")]
+[assembly: AssemblyTrademark("1a8a34dc2286c9b1ba960c32c618dce4fb015236")]
 [assembly: AssemblyVersion("0.0.1.50815")]
 [assembly: AssemblyFileVersion("0.0.1.50815")]
 [assembly: AssemblyInformationalVersion("0.0.1.50815")]
