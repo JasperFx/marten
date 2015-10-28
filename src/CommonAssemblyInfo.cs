0c241a6fd22ab7db866eee08c1c9194c27ecb0c6
jeremydmiller <jeremydmiller@yahoo.com>
2015-10-28 09:45:16 -0500

    all ST specs are regression

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Postgresql as a Document Db and Event Store for .Net Development")]
 [assembly: AssemblyProduct("StructureMap")]
 [assembly: AssemblyCopyright("Copyright 2015 Jeremy D. Miller et al. All rights reserved.")]
-[assembly: AssemblyTrademark("da54410b9d5b6617fb5679db55a91b660a3e2470")]
+[assembly: AssemblyTrademark("87dc0e70bdf0c8142b5e512568c7a425667f2343")]
 [assembly: AssemblyVersion("0.0.1.50944")]
 [assembly: AssemblyFileVersion("0.0.1.50944")]
 [assembly: AssemblyInformationalVersion("0.0.1.50944")]
