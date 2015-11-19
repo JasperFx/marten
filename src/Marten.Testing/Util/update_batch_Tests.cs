3fa798afb392729d679f0a3822ebd82dd7681683
jeremydmiller <jeremydmiller@yahoo.com>
2015-11-19 14:57:20 -0600

    UpdateBatch can call sprocs and deletes

---
+++ src/Marten.Testing/Util/update_batch_Tests.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Marten.Schema;
+using Marten.Testing.Fixtures;
+using Marten.Util;
+using NpgsqlTypes;
+using Shouldly;
+
+namespace Marten.Testing.Util
+{
+    public class update_batch_Tests : DocumentSessionFixture
+    {
+        private DocumentMapping theMapping;
+
+        public update_batch_Tests()
+        {
+            theMapping = theContainer.GetInstance<IDocumentSchema>().MappingFor(typeof (Target));
+        }
+
+        public void write_multiple_calls()
+        {
+            // Just forcing the table and schema objects to be created
+            var initialTarget = Target.Random();
+            theSession.Store(initialTarget);
+            theSession.SaveChanges();
+
+            var batch = theContainer.GetInstance<UpdateBatch>();
+
+            var target1 = Target.Random();
+            var target2 = Target.Random();
+            var target3 = Target.Random();
+
+            var upsertName = theMapping.UpsertName;
+
+            batch.Sproc(upsertName).Param(target1.Id).JsonParam(target1);
+            batch.Sproc(upsertName).Param(target2.Id).JsonParam(target2);
+            batch.Sproc(upsertName).Param(target3.Id).JsonParam(target3);
+            batch.Delete(theMapping.TableName, initialTarget.Id, NpgsqlDbType.Uuid);
+
+            batch.Execute();
+
+            var targets = theSession.Query<Target>().ToArray();
+            targets.Count().ShouldBe(3);
+
+            targets.Any(x => x.Id == target1.Id).ShouldBeTrue();
+            targets.Any(x => x.Id == target2.Id).ShouldBeTrue();
+            targets.Any(x => x.Id == target3.Id).ShouldBeTrue();
+
+            targets.Any(x => x.Id == initialTarget.Id).ShouldBeFalse();
+        }
+    }
+}
