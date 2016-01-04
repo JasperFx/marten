506f39559b5a8c5b558025e3f9f79c1fb5d6a150
jeremydmiller <jeremydmiller@yahoo.com>
2016-01-04 12:30:02 -0600

    Updated Storyteller, added "Edit on Github" link to docs

---
+++ publish-docs.cmd
@@ -0,0 +1,14 @@
+packages\storyteller\tools\ST.exe doc-export c:\code\marten-docs Website --version 0.5.0
+
+cd \code\marten-docs
+
+
+git add --all
+
+
+
+git commit -a -m "Documentation Update"
+
+
+
+git push origin gh-pages
