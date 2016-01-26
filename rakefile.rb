2315c77eccd0dca4f7667b5d6998f64c88259f0e
jeremydmiller <jeremydmiller@yahoo.com>
2016-01-26 09:45:50 -0600

    moved the nuget and binaries to .Net 4.6, bumped version to 0.5.2

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,6 @@
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
-BUILD_VERSION = '0.5.1'
+BUILD_VERSION = '0.5.2'
 CONNECTION = ENV['connection']
 
 tc_build_number = ENV["BUILD_NUMBER"]
