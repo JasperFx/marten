375556a0a62a1750c03318cee4e7eb73e36ccb76
jeremydmiller <jeremydmiller@yahoo.com>
2016-02-19 14:22:54 -0600

    spiking on EventMapping's new functionality, added Stream

--- src/Marten/Events/EventMapping.cs
+++ src/Marten/Events/EventMapping.cs
@@ -1,29 +1,146 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using Baseline;
+using Marten.Linq;
 using Marten.Schema;
+using Marten.Services;
+using Marten.Util;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace Marten.Events
 {
-    public class EventMapping : DocumentMapping
+    public class EventMapping : IDocumentMapping, IDocumentStorage
     {
+
         public static string ToEventTypeName(Type eventType)
         {
             return eventType.Name.SplitPascalCase().ToLower().Replace(" ", "_");
         }
 
-        public EventMapping(StreamMapping parent, Type eventType) : base(eventType)
+        public EventMapping(StreamMapping parent, Type eventType)
         {
+            DocumentType = eventType;
+
             if (!eventType.CanBeCastTo<IEvent>())
                 throw new ArgumentOutOfRangeException(nameof(eventType),
                     $"Only types implementing {typeof (IEvent)} can be accepted");
 
             Stream = parent;
 
-            EventTypeName = ToEventTypeName(eventType);
+            EventTypeName = Alias = ToEventTypeName(eventType);
+
+            IdMember = eventType.GetProperty(nameof(IEvent.Id));
         }
 
         public string EventTypeName { get; set; }
 
         public StreamMapping Stream { get; }
+        public string Alias { get; }
+        public Type DocumentType { get; }
+        public NpgsqlDbType IdType { get; } = NpgsqlDbType.Uuid;
+        public NpgsqlCommand LoaderCommand(object id)
+        {
+            return new NpgsqlCommand($"select d.data, d.id from mt_events as d where id = :id and type = '{Alias}'").With("id", id);
+        }
+
+        public NpgsqlCommand DeleteCommandForId(object id)
+        {
+            throw new NotSupportedException();
+        }
+
+        public NpgsqlCommand DeleteCommandForEntity(object entity)
+        {
+            throw new NotSupportedException();
+        }
+
+        public NpgsqlCommand LoadByArrayCommand<TKey>(TKey[] ids)
+        {
+            return new NpgsqlCommand($"select d.data, d.id from mt_events as d where id = ANY(:ids) and type = '{Alias}'").With("ids", ids);
+        }
+
+        public object Identity(object document)
+        {
+            return document.As<IEvent>().Id;
+        }
+
+        public void RegisterUpdate(UpdateBatch batch, object entity)
+        {
+            // Do nothing
+        }
+
+        public void RegisterUpdate(UpdateBatch batch, object entity, string json)
+        {
+            // Do nothing
+        }
+
+        public void Remove(IIdentityMap map, object entity)
+        {
+            throw new InvalidOperationException("Use IDocumentSession.Events for all persistence of IEvent objects");
+        }
+
+        public void Delete(IIdentityMap map, object id)
+        {
+            throw new InvalidOperationException("Use IDocumentSession.Events for all persistence of IEvent objects");
+        }
+
+        public void Store(IIdentityMap map, object id, object entity)
+        {
+            throw new InvalidOperationException("Use IDocumentSession.Events for all persistence of IEvent objects");
+        }
+
+        public string TableName { get; } = "mt_events";
+        public PropertySearching PropertySearching { get; } = PropertySearching.JSON_Locator_Only;
+        public IIdGeneration IdStrategy { get; } = new GuidIdGeneration();
+        public MemberInfo IdMember { get; }
+
+        public string SelectFields(string tableAlias)
+        {
+            return $"{tableAlias}.id, {tableAlias}.data";
+        }
+
+        public bool ShouldRegenerate(IDocumentSchema schema)
+        {
+            return Stream.ShouldRegenerate(schema);
+        }
+
+        public IField FieldFor(IEnumerable<MemberInfo> members)
+        {
+            return Stream.FieldFor(members);
+        }
+
+        public IWhereFragment FilterDocuments(IWhereFragment query)
+        {
+            return new CompoundWhereFragment("and", DefaultWhereFragment(), query);
+        }
+
+        public IWhereFragment DefaultWhereFragment()
+        {
+            return new WhereFragment($"d.type = '{EventTypeName}'");
+        }
+
+        public IDocumentStorage BuildStorage(IDocumentSchema schema)
+        {
+            return this;
+        }
+
+        public void WriteSchemaObjects(IDocumentSchema schema, StringWriter writer)
+        {
+            Stream.WriteSchemaObjects(schema, writer);
+        }
+
+        public void RemoveSchemaObjects(IManagedConnection connection)
+        {
+            throw new NotSupportedException($"Invalid to remove schema objects for {DocumentType}, Use the parent {Stream.DocumentType} instead");
+        }
+
+        public void DeleteAllDocuments(IConnectionFactory factory)
+        {
+            factory.RunSql($"delete from mt_events where type = '{Alias}'");
+        }
     }
+
+
 }
---
+++ src/Marten/Events/Stream.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Marten.Linq;
+using Marten.Schema;
+using Marten.Services;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace Marten.Events
+{
+    public class Stream
+    {
+        public Stream(Guid id, Type aggregateType)
+        {
+            Id = id;
+            AggregateType = aggregateType;
+        }
+
+        public Guid Id { get; }
+        public Type AggregateType { get; } 
+
+        public readonly IList<IEvent> Events = new List<IEvent>();
+    }
+
+
+}
