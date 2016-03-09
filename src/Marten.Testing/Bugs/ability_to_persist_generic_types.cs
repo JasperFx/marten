a91954816e77c8130bbc5498882e60b90192b642
jokokko <jokokko@gmail.com>
2016-03-09 15:03:01 +0200

    some quick code to enable persistence of (nested) generic classes. #191

---
+++ src/Marten.Testing/Bugs/ability_to_persist_generic_types.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Marten.Services;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class GenericTypeToPersist<T>
+    {
+        public Guid Id = Guid.NewGuid();
+    }
+
+    public class TypeA { }
+    public class TypeB { }
+
+    public class ability_to_persist_generic_types : DocumentSessionFixture<NulloIdentityMap>
+    {
+        [Fact]
+        public void can_persist_and_load_generic_types()
+        {
+            var doc1A = new GenericTypeToPersist<TypeA>();
+            var doc1B = new GenericTypeToPersist<TypeB>();
+
+            theSession.Store(doc1A);
+            theSession.Store(doc1B);
+            theSession.SaveChanges();
+
+            var doc2A = theSession.Load<GenericTypeToPersist<TypeA>>(doc1A.Id);
+            var doc2B = theSession.Load<GenericTypeToPersist<TypeA>>(doc2A.Id);
+            doc2A.ShouldNotBeNull();            
+            doc2B.ShouldNotBeNull();
+        }
+    }
+
+    public class ability_to_persist_nested_generic_types : DocumentSessionFixture<NulloIdentityMap>
+    {
+        [Fact]
+        public void can_persist_and_load_generic_types()
+        {
+            var doc1 = new NestedGenericTypeToPersist<TypeA>();
+            
+            theSession.Store(doc1);            
+            theSession.SaveChanges();
+
+            var doc2 = theSession.Load<NestedGenericTypeToPersist<TypeA>>(doc1.Id);            
+            doc2.ShouldNotBeNull();            
+        }
+
+        public class NestedGenericTypeToPersist<T>
+        {
+            public Guid Id = Guid.NewGuid();
+        }
+    }
+}
