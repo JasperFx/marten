a12da652b16c2b53dacbf322c714f82431054b60
Jeffrey Nevins <jnevins@GCMLP.COM>
2016-03-13 22:07:56 -0400

    Added case insensitive string support

--- src/Marten/Linq/Handlers/StringEndsWith.cs
+++ src/Marten/Linq/Handlers/StringEndsWith.cs
@@ -1,22 +1,32 @@
-using System.Linq;
+using System;
 using System.Linq.Expressions;
-using Baseline;
-using Marten.Schema;
+using System.Reflection;
+using Baseline.Reflection;
 
 namespace Marten.Linq.Handlers
 {
-    public class StringEndsWith : MethodCallParser<string>
+    public class StringEndsWith : StringComparisonParser
     {
-        public StringEndsWith() : base(x => x.EndsWith(null))
+        public StringEndsWith() : base(
+            ReflectionHelper.GetMethod<string>(s => s.EndsWith(null)),
+            ReflectionHelper.GetMethod<string>(s => s.EndsWith(null, StringComparison.CurrentCulture)),
+            ReflectionHelper.GetMethod<string>(s => s.EndsWith(null, true, null)))
         {
         }
 
-        public override IWhereFragment Parse(IDocumentMapping mapping, ISerializer serializer, MethodCallExpression expression)
+        protected override string FormatValue(MethodInfo method, string value)
         {
-            var @object = expression.Object;
-            var locator = mapping.JsonLocator(@object);
-            var value = expression.Arguments.Single().Value().As<string>();
-            return new WhereFragment("{0} like ?".ToFormat(locator), "%" + value);
+            return "%" + value;
+        }
+
+        protected override bool IsCaseInsensitiveComparison(MethodCallExpression expression)
+        {
+            if (AreMethodsEqual(expression.Method, ReflectionHelper.GetMethod<string>(s => s.StartsWith(null, true, null))))
+            {
+                var constant = expression.Arguments[1] as ConstantExpression;
+                if (constant != null && constant.Value is bool) return (bool) constant.Value;
+            }
+            return base.IsCaseInsensitiveComparison(expression);
         }
     }
 }
