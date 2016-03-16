0e4169d24d8b136bc825740843215d216298db6f
jeremydmiller <jeremydmiller@yahoo.com>
2016-03-16 10:05:39 -0500

    little more spiking on IncludeJoin and IncludedSelector

--- src/Marten/Services/Includes/IncludeSpikeClasses.cs
+++ src/Marten/Services/Includes/IncludeSpikeClasses.cs
@@ -22,11 +22,13 @@ namespace Marten.Services.Includes
     public class IncludeJoin<T> : IIncludeJoin where T : class
     {
         public string JoinText { get; }
+        private readonly IDocumentMapping _mapping;
         private readonly Action<T> _callback;
 
-        public IncludeJoin(string joinText, string tableAlias, Action<T> callback)
+        public IncludeJoin(IDocumentMapping mapping, string joinText, string tableAlias, Action<T> callback)
         {
             JoinText = joinText;
+            _mapping = mapping;
             _callback = callback;
 
             TableAlias = tableAlias;
@@ -36,22 +38,23 @@ namespace Marten.Services.Includes
 
         public ISelector<TSearched> WrapSelector<TSearched>(IDocumentSchema schema, ISelector<TSearched> inner)
         {
-            return new IncludeSelector<TSearched, T>(TableAlias, _callback, inner, schema.ResolverFor<T>());
+            return new IncludeSelector<TSearched, T>(TableAlias, _mapping, _callback, inner, schema.ResolverFor<T>());
         }
     }
 
     public class IncludeSelector<TSearched, TIncluded> : ISelector<TSearched> where TIncluded : class
     {
         private readonly string _tableAlias;
+        private readonly IDocumentMapping _includedMapping;
         private readonly Action<TIncluded> _callback;
         private readonly ISelector<TSearched> _inner;
         private readonly IResolver<TIncluded> _resolver;
         private readonly int _startingIndex;
 
-        public IncludeSelector(string tableAlias, Action<TIncluded> callback, ISelector<TSearched> inner,
-            IResolver<TIncluded> resolver)
+        public IncludeSelector(string tableAlias, IDocumentMapping includedMapping, Action<TIncluded> callback, ISelector<TSearched> inner, IResolver<TIncluded> resolver)
         {
             _tableAlias = tableAlias;
+            _includedMapping = includedMapping;
             _callback = callback;
             _inner = inner;
             _resolver = resolver;
@@ -70,12 +73,9 @@ namespace Marten.Services.Includes
         public string[] SelectFields()
         {
             var innerFields = _inner.SelectFields();
+            var outerFields = _includedMapping.SelectFields().Select(x => $"{_tableAlias}.{x}");
 
-            // Have resolver return the fields here.
-
-            return
-                innerFields.Concat(new[]
-                {$"{_tableAlias}.data as {_tableAlias}_data", $"${_tableAlias}.id as {_tableAlias}_id"}).ToArray();
+            return innerFields.Concat(outerFields).ToArray();
         }
     }
 }
