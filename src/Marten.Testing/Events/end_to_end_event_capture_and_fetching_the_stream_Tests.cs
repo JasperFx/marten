cfee61ef3d804808a334fa6ca6d8ed2251a52d67
Tim Cools <tim@soloco.be>
2016-03-16 16:11:32 +0100

    Store of event stream fails with dirty tracking session

--- src/Marten.Testing/Events/end_to_end_event_capture_and_fetching_the_stream_Tests.cs
+++ src/Marten.Testing/Events/end_to_end_event_capture_and_fetching_the_stream_Tests.cs
@@ -7,12 +7,19 @@ namespace Marten.Testing.Events
 {
     public class end_to_end_event_capture_and_fetching_the_stream_Tests
     {
-        [Fact]
-        public void capture_events_to_a_new_stream_and_fetch_the_events_back()
+        public static TheoryData<DocumentTracking> SessionTypes = new TheoryData<DocumentTracking>
+        {
+            DocumentTracking.IdentityOnly,
+            DocumentTracking.DirtyTracking
+        };
+
+        [Theory]
+        [MemberData("SessionTypes")]
+        public void capture_events_to_a_new_stream_and_fetch_the_events_back(DocumentTracking sessionType)
         {
             var store = InitStore();
 
-            using (var session = store.OpenSession())
+            using (var session = store.OpenSession(sessionType))
             {
                 var joined = new MembersJoined { Members = new string[] { "Rand", "Matt", "Perrin", "Thom" } };
                 var departed = new MembersDeparted { Members = new[] { "Thom" } };
@@ -28,12 +35,13 @@ namespace Marten.Testing.Events
             }
         }
 
-        [Fact]
-        public void capture_events_to_a_new_stream_and_fetch_the_events_back_with_stream_id_provided()
+        [Theory]
+        [MemberData("SessionTypes")]
+        public void capture_events_to_a_new_stream_and_fetch_the_events_back_with_stream_id_provided(DocumentTracking sessionType)
         {
             var store = InitStore();
 
-            using (var session = store.OpenSession())
+            using (var session = store.OpenSession(sessionType))
             {
                 var joined = new MembersJoined { Members = new string[] { "Rand", "Matt", "Perrin", "Thom" } };
                 var departed = new MembersDeparted { Members = new[] { "Thom" } };
@@ -50,24 +58,23 @@ namespace Marten.Testing.Events
             }
         }
 
-
-        [Fact]
-        public void capture_events_to_a_non_existing_stream_and_fetch_the_events_back()
+        [Theory]
+        [MemberData("SessionTypes")]
+        public void capture_events_to_a_non_existing_stream_and_fetch_the_events_back(DocumentTracking sessionType)
         {
             var store = InitStore();
 
-            using (var session = store.OpenSession())
+            using (var session = store.OpenSession(sessionType))
             {
                 var joined = new MembersJoined { Members = new string[] { "Rand", "Matt", "Perrin", "Thom" } };
                 var departed = new MembersDeparted { Members = new[] { "Thom" } };
 
                 var id = Guid.NewGuid();
-                session.Events.AppendEvents(id, joined, departed);
+                session.Events.StartStream<Quest>(id, joined);
+                session.Events.AppendEvents(id, departed);
+
                 session.SaveChanges();
 
-                //Throws a   Marten.Testing.Events.end_to_end_event_capture_and_fetching_the_stream_Tests.capture_events_to_a_non_existing_stream_and_fetch_the_events_back [FAIL]
-                //Npgsql.NpgsqlException : 23502: null value in column "type" violates not-null constraint
-                //>>>>  "type" is stream_type in this case.
                 var streamEvents = session.Events.FetchStream<Quest>(id);
 
                 streamEvents.Count().ShouldBe(2);
@@ -76,6 +83,39 @@ namespace Marten.Testing.Events
             }
         }
 
+        [Theory]
+        [MemberData("SessionTypes")]
+        public void capture_events_to_an_existing_stream_and_fetch_the_events_back(DocumentTracking sessionType)
+        {
+            var store = InitStore();
+
+            var id = Guid.NewGuid();
+            var started = new QuestStarted();
+
+            using (var session = store.OpenSession(sessionType))
+            {
+                session.Events.StartStream<Quest>(id, started);
+                session.SaveChanges();
+            }
+
+            using (var session = store.OpenSession(sessionType))
+            {
+                var joined = new MembersJoined { Members = new string[] { "Rand", "Matt", "Perrin", "Thom" } };
+                var departed = new MembersDeparted { Members = new[] { "Thom" } };
+
+                session.Events.AppendEvents(id, joined);
+                session.Events.AppendEvents(id, departed);
+
+                session.SaveChanges();
+
+                var streamEvents = session.Events.FetchStream<Quest>(id);
+
+                streamEvents.Count().ShouldBe(3);
+                streamEvents.ElementAt(0).ShouldBeOfType<QuestStarted>();
+                streamEvents.ElementAt(1).ShouldBeOfType<MembersJoined>();
+                streamEvents.ElementAt(2).ShouldBeOfType<MembersDeparted>();
+            }
+        }
 
         private static DocumentStore InitStore()
         {
