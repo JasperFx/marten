40cf5f48e365c095d8ecf296b78db21ed74b6555
jeremydmiller <jeremydmiller@yahoo.com>
2016-04-01 13:34:05 -0500

    fixing the erroneous Hilo table regeneration. Closes GH-255

--- src/Marten/Schema/Sequences/SequenceFactory.cs
+++ src/Marten/Schema/Sequences/SequenceFactory.cs
@@ -7,25 +7,29 @@ namespace Marten.Schema.Sequences
         private readonly IDocumentSchema _schema;
         private readonly IConnectionFactory _factory;
         private readonly StoreOptions _options;
+        private readonly IMartenLogger _logger;
 
-        public SequenceFactory(IDocumentSchema schema, IConnectionFactory factory, StoreOptions options)
+        public SequenceFactory(IDocumentSchema schema, IConnectionFactory factory, StoreOptions options, IMartenLogger logger)
         {
             _schema = schema;
             _factory = factory;
             _options = options;
+            _logger = logger;
         }
 
         public ISequence Hilo(Type documentType, HiloSettings settings)
         {
-            var qualiffiedTableName = _options.DatabaseSchemaName + ".mt_hilo";
-            if (!_schema.TableExists(qualiffiedTableName))
+            if (!_schema.TableExists(_options.DatabaseSchemaName, "mt_hilo"))
             {
                 if (_options.AutoCreateSchemaObjects == AutoCreate.None)
                 {
                     throw new InvalidOperationException($"Hilo table is missing, but {nameof(StoreOptions.AutoCreateSchemaObjects)} is {_options.AutoCreateSchemaObjects}");
                 }
 
-                _factory.RunSql(SchemaBuilder.GetSqlScript(_options.DatabaseSchemaName, "mt_hilo"));
+                var sqlScript = SchemaBuilder.GetSqlScript(_options.DatabaseSchemaName, "mt_hilo");
+                _logger.SchemaChange(sqlScript);
+
+                _factory.RunSql(sqlScript);
             }
 
             return new HiloSequence(_factory, _options, documentType.Name, settings);
