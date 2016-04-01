2fc6fc5863fdf46769f7cde96220c8f0dd611242
jeremydmiller <jeremydmiller@yahoo.com>
2016-04-01 13:09:24 -0500

    made IUnitOfWork pending change methods smarter about base classes. Closes GH-254

---
+++ src/Marten.Testing/Examples/Setting_Timestamp_on_all_changes_by_base_class_Tests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Baseline;
+using Xunit;
+
+namespace Marten.Testing.Examples
+{
+    public class Setting_Timestamp_on_all_changes_by_base_class_Tests
+    {
+        [Fact]
+        public void demonstration()
+        {
+            using (var store = DocumentStore.For(_ =>
+            {
+                _.Connection(ConnectionSource.ConnectionString);
+                _.AutoCreateSchemaObjects = AutoCreate.All;
+                _.Listeners.Add(new TimestampListener());
+            }))
+            {
+                var doc1s = new Doc1[] {new Doc1(), new Doc1(), };
+                var doc2s = new Doc2[] {new Doc2(), new Doc2(), };
+                var doc3s = new Doc3[] {new Doc3(), new Doc3(), };
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(doc1s);
+                    session.Store(doc2s);
+                    session.Store(doc3s);
+
+                    session.SaveChanges();
+                }
+
+                doc1s.Each(x => x.Timestamp.ShouldNotBeNull());
+                doc2s.Each(x => x.Timestamp.ShouldNotBeNull());
+                doc3s.Each(x => x.Timestamp.ShouldNotBeNull());
+            }
+        }
+    }
+
+    public class TimestampListener : DocumentSessionListenerBase
+    {
+        public override void BeforeSaveChanges(IDocumentSession session)
+        {
+            var entities = session.PendingChanges.AllChangedFor<BaseEntity>();
+            entities.Each(x => x.Timestamp = DateTimeOffset.UtcNow);
+        }
+
+        public override Task BeforeSaveChangesAsync(IDocumentSession session)
+        {
+            return Task.Factory.StartNew(() => BeforeSaveChanges(session));
+        }
+
+    }
+
+    public class Doc1 : BaseEntity
+    {
+        
+    }
+
+    public class Doc2 : BaseEntity
+    {
+        
+    }
+
+    public class Doc3 : BaseEntity
+    {
+        
+    }
+
+    public class BaseEntity
+    {
+        public DateTimeOffset? Timestamp { get; set; }
+        public Guid Id { get; set; }
+    }
+}
