df32a87674a3615d566eb2d2e001e3e730a2e72c
jeremydmiller <jeremydmiller@yahoo.com>
2016-04-05 07:14:04 -0500

    bumped to 0.8.7

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,6 @@
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
-BUILD_VERSION = '0.8.6'
+BUILD_VERSION = '0.8.7'
 CONNECTION = ENV['connection']
 
 tc_build_number = ENV["BUILD_NUMBER"]
