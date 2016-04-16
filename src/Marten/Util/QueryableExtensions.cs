264ddf829ded729380c1f5031f55071570cd8b1f
jeremydmiller <jeremydmiller@yahoo.com>
2016-04-16 11:07:02 -0500

    cleaned out obsolete code in QueryableExtensions

--- src/Marten/Util/QueryableExtensions.cs
+++ src/Marten/Util/QueryableExtensions.cs
@@ -1,30 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using System.Threading.Tasks;
 using Baseline;
 using Marten.Linq;
-using Marten.Linq.QueryHandlers;
-using Marten.Services;
 
 namespace Marten.Util
 {
     public static class QueryableExtensions
     {
-        #region Explain
 
         public static QueryPlan Explain<T>(this IQueryable<T> queryable)
         {
-            var martenQueryable = CastToMartenQueryable(queryable);
-            return martenQueryable.Explain();
+            return queryable.As<IMartenQueryable<T>>().Explain();
         }
 
-        #endregion
 
         #region ToListJson
 
@@ -129,7 +121,6 @@ namespace Marten.Util
 
         #region Count/LongCount/Sum
 
-
         public static Task<int> CountAsync<TSource>(
             this IQueryable<TSource> source,
             CancellationToken token = default(CancellationToken))
@@ -176,8 +167,6 @@ namespace Marten.Util
 
         #region First/FirstOrDefault
 
-        private static readonly MethodInfo _first = GetMethod(nameof(Queryable.First));
-
         public static Task<TSource> FirstAsync<TSource>(
             this IQueryable<TSource> source,
             CancellationToken token = default(CancellationToken))
@@ -204,7 +193,6 @@ namespace Marten.Util
             return source.Select(x => x.Json()).First();
         }
 
-        private static readonly MethodInfo _firstPredicate = GetMethod(nameof(Queryable.First), 1);
 
         public static Task<TSource> FirstAsync<TSource>(
             this IQueryable<TSource> source,
@@ -226,7 +214,6 @@ namespace Marten.Util
             if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
             return source.Where(predicate).Select(x => x.Json()).FirstAsync(token);
-
         }
 
         public static string FirstJson<TSource>(
@@ -237,7 +224,6 @@ namespace Marten.Util
             if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
             return source.Where(predicate).Json().First();
-
         }
 
 
@@ -268,7 +254,6 @@ namespace Marten.Util
             return source.Select(x => x.Json()).FirstOrDefault();
         }
 
-        private static readonly MethodInfo _firstOrDefaultPredicate = GetMethod(nameof(Queryable.FirstOrDefault), 1);
 
         public static Task<TSource> FirstOrDefaultAsync<TSource>(
             this IQueryable<TSource> source,
@@ -306,7 +291,6 @@ namespace Marten.Util
 
         #region Single/SingleOrDefault
 
-
         public static Task<TSource> SingleAsync<TSource>(
             this IQueryable<TSource> source,
             CancellationToken token = default(CancellationToken))
@@ -332,8 +316,6 @@ namespace Marten.Util
 
             return source.Select(x => x.Json()).Single();
         }
-
-        private static readonly MethodInfo _singlePredicate = GetMethod(nameof(Queryable.Single), 1);
 
         public static Task<TSource> SingleAsync<TSource>(
             this IQueryable<TSource> source,
@@ -429,123 +411,7 @@ namespace Marten.Util
 
         #endregion
 
-        #region Last/LastOrDefault
-
-        private static readonly MethodInfo _last = GetMethod(nameof(Queryable.Last));
-
-        public static Task<TSource> LastAsync<TSource>(
-            this IQueryable<TSource> source,
-            CancellationToken token = default(CancellationToken))
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-
-            return ExecuteAsync<TSource, TSource>(_last, source, token);
-        }
-
-        private static readonly MethodInfo _lastOrDefault = GetMethod(nameof(Queryable.LastOrDefault));
-
-        public static Task<TSource> LastOrDefaultAsync<TSource>(
-            this IQueryable<TSource> source,
-            CancellationToken token = default(CancellationToken))
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-
-            return ExecuteAsync<TSource, TSource>(_lastOrDefault, source, token);
-        }
-
-        private static readonly MethodInfo _lastOrDefaultPredicate = GetMethod(nameof(Queryable.LastOrDefault), 1);
-
-        public static Task<TSource> LastOrDefaultAsync<TSource>(
-            this IQueryable<TSource> source,
-            Expression<Func<TSource, bool>> predicate,
-            CancellationToken token = default(CancellationToken))
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            return ExecuteAsync<TSource, TSource>(_lastOrDefaultPredicate, source, predicate, token);
-        }
-
-        #endregion
-
         #region Shared
-
-        private static Task<TResult> ExecuteAsync<TSource, TResult>(
-            MethodInfo operatorMethodInfo,
-            IQueryable<TSource> source,
-            CancellationToken token = default(CancellationToken))
-        {
-            var provider = source.Provider as IMartenQueryProvider;
-            if (provider == null)
-            {
-                throw new InvalidOperationException($"{source.Provider.GetType()} is not IMartenQueryProvider");
-            }
-
-            if (operatorMethodInfo.IsGenericMethod)
-            {
-                operatorMethodInfo = operatorMethodInfo.MakeGenericMethod(typeof (TSource));
-            }
-
-            return provider.ExecuteAsync<TResult>(
-                Expression.Call(null, operatorMethodInfo, source.Expression),
-                token);
-        }
-
-
-        private static Task<TResult> ExecuteAsync<TSource, TResult>(
-            MethodInfo operatorMethodInfo,
-            IQueryable<TSource> source,
-            LambdaExpression expression,
-            CancellationToken token = default(CancellationToken))
-        {
-            return ExecuteAsync<TSource, TResult>(operatorMethodInfo, source, Expression.Quote(expression), token);
-        }
-
-
-
-        private static Task<TResult> ExecuteAsync<TSource, TResult>(
-            MethodInfo operatorMethodInfo,
-            IQueryable<TSource> source,
-            Expression expression,
-            CancellationToken token = default(CancellationToken))
-        {
-            var provider = source.Provider as IMartenQueryProvider;
-            if (provider == null)
-            {
-                throw new InvalidOperationException($"{source.Provider.GetType()} is not IMartenQueryProvider");
-            }
-
-            operatorMethodInfo
-                = operatorMethodInfo.GetGenericArguments().Length == 2
-                    ? operatorMethodInfo.MakeGenericMethod(typeof (TSource), typeof (TResult))
-                    : operatorMethodInfo.MakeGenericMethod(typeof (TSource));
-
-            return provider.ExecuteAsync<TResult>(
-                Expression.Call(
-                    null,
-                    operatorMethodInfo,
-                    new[] {source.Expression, expression}),
-                token);
-        }
-
-        private static MethodInfo GetMethod(
-            string name,
-            int parameterCount = 0,
-            Func<MethodInfo, bool> predicate = null)
-        {
-            return typeof (Queryable)
-                .GetTypeInfo()
-                .GetDeclaredMethods(name)
-                .Single(methodInfo =>
-                {
-                    if (methodInfo.GetParameters().Length != parameterCount + 1)
-                    {
-                        return false;
-                    }
-
-                    return predicate == null || predicate(methodInfo);
-                });
-        }
 
         private static IMartenQueryable<T> CastToMartenQueryable<T>(IQueryable<T> queryable)
         {
