1e2ed9c9ed90e409d193a4a2f546bb4f3be1bee1
jeremydmiller <jeremydmiller@yahoo.com>
2016-04-18 10:39:07 -0500

    bumped publish-docs version to 0.9

--- publish-docs.cmd
+++ publish-docs.cmd
@@ -1,4 +1,4 @@
-packages\storyteller\tools\ST.exe doc-export c:\code\marten-docs ProjectWebsite --version 0.8.0 --project marten
+packages\storyteller\tools\ST.exe doc-export c:\code\marten-docs ProjectWebsite --version 0.9.0 --project marten
 
 cd \code\marten-docs
 
