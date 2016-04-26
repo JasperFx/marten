2c81e4f51524829b75f7c4feef991026f7a95b0f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-04-26 10:57:36 -0500

    bumping to v0.9!

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,6 @@
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
-BUILD_VERSION = '0.8.11'
+BUILD_VERSION = '0.9.0'
 CONNECTION = ENV['connection']
 
 tc_build_number = ENV["BUILD_NUMBER"]
