a25666154f511ad6e590a311e101521646e8e98e
Tim Cools <tim@soloco.be>
2016-04-29 13:25:37 +0200

    fix 339 asyn cache issue (add missing file)

---
+++ src/Marten.Testing/Bugs/Bug_339_async_cache_problem.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Marten.Services;
+using Marten.Testing.Documents;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_339_async_cache_problem : DocumentSessionFixture<NulloIdentityMap>
+    {
+        [Fact]
+        public async Task pending_with_dirty_checks_async()
+        {
+            var user1 = new User();
+
+            using (var session1 = theStore.LightweightSession())
+            {
+                session1.Store(user1);
+                session1.SaveChanges();
+            }
+
+            using (var session2 = theStore.DirtyTrackedSession())
+            {
+                var user12 = await session2.LoadAsync<User>(user1.Id);
+                var breakThings = await session2.LoadAsync<User>(user1.Id);
+            }
+        }
+    }
+}
