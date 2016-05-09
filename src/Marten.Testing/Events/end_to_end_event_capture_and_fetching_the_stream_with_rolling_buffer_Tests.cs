8ca6b76975dc498ced33b2b381c40815fd713d15
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-05-09 09:00:20 -0500

    tests verifying that the rolling buffer table is getting filled on event capture

--- src/Marten.Testing/Events/end_to_end_event_capture_and_fetching_the_stream_with_rolling_buffer_Tests.cs
+++ src/Marten.Testing/Events/end_to_end_event_capture_and_fetching_the_stream_with_rolling_buffer_Tests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using Marten.Util;
 using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
@@ -8,12 +10,6 @@ namespace Marten.Testing.Events
 {
     public class end_to_end_event_capture_and_fetching_the_stream_with_rolling_buffer_Tests
     {
-        public static TheoryData<DocumentTracking> SessionTypes = new TheoryData<DocumentTracking>
-        {
-            DocumentTracking.IdentityOnly,
-            DocumentTracking.DirtyTracking
-        };
-
         private readonly ITestOutputHelper _output;
 
         public end_to_end_event_capture_and_fetching_the_stream_with_rolling_buffer_Tests(ITestOutputHelper output)
@@ -21,24 +17,22 @@ namespace Marten.Testing.Events
             _output = output;
         }
 
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_a_new_stream_and_fetch_the_events_back(DocumentTracking sessionType)
+        [Fact]
+        public void capture_events_to_a_new_stream_and_fetch_the_events_back_in_same_schema()
         {
-            var store = InitStore();
+            var store = InitStore("public");
 
 
-            using (var session = store.OpenSession(sessionType))
+            Guid stream = Guid.NewGuid();
+            using (var session = store.OpenSession(DocumentTracking.IdentityOnly))
             {
-                // SAMPLE: start-stream-with-aggregate-type
                 var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
                 var departed = new MembersDeparted { Members = new[] { "Thom" } };
 
-                var id = session.Events.StartStream<Quest>(joined, departed);
+                session.Events.StartStream<Quest>(stream, joined, departed);
                 session.SaveChanges();
-                // ENDSAMPLE
 
-                var streamEvents = session.Events.FetchStream(id);
+                var streamEvents = session.Events.FetchStream(stream);
 
                 streamEvents.Count().ShouldBe(2);
                 streamEvents.ElementAt(0).Data.ShouldBeOfType<MembersJoined>();
@@ -46,117 +40,51 @@ namespace Marten.Testing.Events
                 streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersDeparted>();
                 streamEvents.ElementAt(1).Version.ShouldBe(2);
             }
+
+            var events = new List<Guid>();
+
+            using (var conn = store.Advanced.OpenConnection())
+            {
+                conn.Execute(cmd =>
+                {
+                    cmd.CommandText = "select event_id, stream_id from public.mt_rolling_buffer where reference_count = 1 and stream_id = :stream";
+                    cmd.With("stream", stream);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var evt = reader.GetGuid(0);
+                            events.Add(evt);
+
+                            reader.GetGuid(1).ShouldBe(stream);
+                        }
+                    }
+                });
+            }
+
+            events.Count.ShouldBe(2);
+
+
         }
 
 
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_a_new_stream_and_fetch_the_events_back_with_stream_id_provided(
-            DocumentTracking sessionType)
-        {
-            var store = InitStore();
-
-            using (var session = store.OpenSession(sessionType))
-            {
-                // SAMPLE: start-stream-with-existing-guid
-                var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
-                var departed = new MembersDeparted { Members = new[] { "Thom" } };
-
-                var id = Guid.NewGuid();
-                session.Events.StartStream<Quest>(id, joined, departed);
-                session.SaveChanges();
-                // ENDSAMPLE
-
-                var streamEvents = session.Events.FetchStream(id);
-
-                streamEvents.Count().ShouldBe(2);
-                streamEvents.ElementAt(0).Data.ShouldBeOfType<MembersJoined>();
-                streamEvents.ElementAt(0).Version.ShouldBe(1);
-                streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersDeparted>();
-                streamEvents.ElementAt(1).Version.ShouldBe(2);
-            }
-        }
-
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_a_non_existing_stream_and_fetch_the_events_back(DocumentTracking sessionType)
-        {
-            var store = InitStore();
-
-            using (var session = store.OpenSession(sessionType))
-            {
-                var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
-                var departed = new MembersDeparted { Members = new[] { "Thom" } };
-
-                var id = Guid.NewGuid();
-                session.Events.StartStream<Quest>(id, joined);
-                session.Events.Append(id, departed);
-
-                session.SaveChanges();
-
-                var streamEvents = session.Events.FetchStream(id);
-
-                streamEvents.Count().ShouldBe(2);
-                streamEvents.ElementAt(0).Data.ShouldBeOfType<MembersJoined>();
-                streamEvents.ElementAt(0).Version.ShouldBe(1);
-                streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersDeparted>();
-                streamEvents.ElementAt(1).Version.ShouldBe(2);
-            }
-        }
-
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_an_existing_stream_and_fetch_the_events_back(DocumentTracking sessionType)
-        {
-            var store = InitStore();
-
-            var id = Guid.NewGuid();
-            var started = new QuestStarted();
-
-            using (var session = store.OpenSession(sessionType))
-            {
-                session.Events.StartStream<Quest>(id, started);
-                session.SaveChanges();
-            }
-
-            using (var session = store.OpenSession(sessionType))
-            {
-                var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
-                var departed = new MembersDeparted { Members = new[] { "Thom" } };
-
-                session.Events.Append(id, joined);
-                session.Events.Append(id, departed);
-
-                session.SaveChanges();
-
-                var streamEvents = session.Events.FetchStream(id);
-
-                streamEvents.Count().ShouldBe(3);
-                streamEvents.ElementAt(0).Data.ShouldBeOfType<QuestStarted>();
-                streamEvents.ElementAt(0).Version.ShouldBe(1);
-                streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersJoined>();
-                streamEvents.ElementAt(1).Version.ShouldBe(2);
-                streamEvents.ElementAt(2).Data.ShouldBeOfType<MembersDeparted>();
-                streamEvents.ElementAt(2).Version.ShouldBe(3);
-            }
-        }
-
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_a_new_stream_and_fetch_the_events_back_in_another_database_schema(
-            DocumentTracking sessionType)
+        [Fact]
+        public void capture_events_to_a_new_stream_and_fetch_the_events_back_in_another_database_schema()
         {
             var store = InitStore("event_store");
 
-            using (var session = store.OpenSession(sessionType))
+
+            Guid stream = Guid.NewGuid();
+            using (var session = store.OpenSession(DocumentTracking.IdentityOnly))
             {
                 var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
                 var departed = new MembersDeparted { Members = new[] { "Thom" } };
 
-                var id = session.Events.StartStream<Quest>(joined, departed);
+                session.Events.StartStream<Quest>(stream, joined, departed);
                 session.SaveChanges();
 
-                var streamEvents = session.Events.FetchStream(id);
+                var streamEvents = session.Events.FetchStream(stream);
 
                 streamEvents.Count().ShouldBe(2);
                 streamEvents.ElementAt(0).Data.ShouldBeOfType<MembersJoined>();
@@ -164,100 +92,32 @@ namespace Marten.Testing.Events
                 streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersDeparted>();
                 streamEvents.ElementAt(1).Version.ShouldBe(2);
             }
-        }
 
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void
-            capture_events_to_a_new_stream_and_fetch_the_events_back_with_stream_id_provided_in_another_database_schema(
-            DocumentTracking sessionType)
-        {
-            var store = InitStore("event_store");
+            var events = new List<Guid>();
 
-            using (var session = store.OpenSession(sessionType))
+            using (var conn = store.Advanced.OpenConnection())
             {
-                var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
-                var departed = new MembersDeparted { Members = new[] { "Thom" } };
+                conn.Execute(cmd =>
+                {
+                    cmd.CommandText = "select event_id, stream_id from event_store.mt_rolling_buffer where reference_count = 1 and stream_id = :stream";
+                    cmd.With("stream", stream);
 
-                var id = Guid.NewGuid();
-                session.Events.StartStream<Quest>(id, joined, departed);
-                session.SaveChanges();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var evt = reader.GetGuid(0);
+                            events.Add(evt);
 
-                var streamEvents = session.Events.FetchStream(id);
-
-                streamEvents.Count().ShouldBe(2);
-                streamEvents.ElementAt(0).Data.ShouldBeOfType<MembersJoined>();
-                streamEvents.ElementAt(0).Version.ShouldBe(1);
-                streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersDeparted>();
-                streamEvents.ElementAt(1).Version.ShouldBe(2);
-            }
-        }
-
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_a_non_existing_stream_and_fetch_the_events_back_in_another_database_schema(
-            DocumentTracking sessionType)
-        {
-            var store = InitStore("event_store");
-
-            using (var session = store.OpenSession(sessionType))
-            {
-                var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
-                var departed = new MembersDeparted { Members = new[] { "Thom" } };
-
-                var id = Guid.NewGuid();
-                session.Events.StartStream<Quest>(id, joined);
-                session.Events.Append(id, departed);
-
-                session.SaveChanges();
-
-                var streamEvents = session.Events.FetchStream(id);
-
-                streamEvents.Count().ShouldBe(2);
-                streamEvents.ElementAt(0).Data.ShouldBeOfType<MembersJoined>();
-                streamEvents.ElementAt(0).Version.ShouldBe(1);
-                streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersDeparted>();
-                streamEvents.ElementAt(1).Version.ShouldBe(2);
-            }
-        }
-
-        [Theory]
-        [MemberData("SessionTypes")]
-        public void capture_events_to_an_existing_stream_and_fetch_the_events_back_in_another_database_schema(
-            DocumentTracking sessionType)
-        {
-            var store = InitStore("event_store");
-
-            var id = Guid.NewGuid();
-            var started = new QuestStarted();
-
-            using (var session = store.OpenSession(sessionType))
-            {
-                session.Events.StartStream<Quest>(id, started);
-                session.SaveChanges();
+                            reader.GetGuid(1).ShouldBe(stream);
+                        }
+                    }
+                });
             }
 
-            using (var session = store.OpenSession(sessionType))
-            {
-                // SAMPLE: append-events
-                var joined = new MembersJoined { Members = new[] { "Rand", "Matt", "Perrin", "Thom" } };
-                var departed = new MembersDeparted { Members = new[] { "Thom" } };
+            events.Count.ShouldBe(2);
 
-                session.Events.Append(id, joined, departed);
 
-                session.SaveChanges();
-                // ENDSAMPLE
-
-                var streamEvents = session.Events.FetchStream(id);
-
-                streamEvents.Count().ShouldBe(3);
-                streamEvents.ElementAt(0).Data.ShouldBeOfType<QuestStarted>();
-                streamEvents.ElementAt(0).Version.ShouldBe(1);
-                streamEvents.ElementAt(1).Data.ShouldBeOfType<MembersJoined>();
-                streamEvents.ElementAt(1).Version.ShouldBe(2);
-                streamEvents.ElementAt(2).Data.ShouldBeOfType<MembersDeparted>();
-                streamEvents.ElementAt(2).Version.ShouldBe(3);
-            }
         }
 
 
@@ -281,7 +141,7 @@ namespace Marten.Testing.Events
                 
             });
 
-            store.Advanced.Clean.CompletelyRemoveAll();
+            store.EventStore.InitializeEventStoreInDatabase();
 
             return store;
         }
