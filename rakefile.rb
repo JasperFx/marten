79b54472760d9ce1d08500848392f8ed9b8a61b0
Corey Kaylor <corey@kaylors.net>
2016-05-16 11:00:04 -0700

    Fixed ci task dependency

--- rakefile.rb
+++ rakefile.rb
@@ -8,7 +8,7 @@ build_revision = tc_build_number || Time.new.strftime('5%H%M')
 build_number = "#{BUILD_VERSION}.#{build_revision}"
 BUILD_NUMBER = build_number 
 
-task :ci => [:connection, :version, :default, :pack]
+task :ci => [:connection, :version, :default, 'paket:pack']
 
 task :default => [:mocha, :test, :storyteller]
 
