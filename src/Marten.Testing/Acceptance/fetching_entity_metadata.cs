9222cd158e01ec70bc96a80b675da4a268f7cbdb
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-05-24 09:03:26 -0500

    can fetch application metadata now from DocumentStore.Advanced.MetadataFor()

---
+++ src/Marten.Testing/Acceptance/fetching_entity_metadata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Acceptance
+{
+    public class fetching_entity_metadata : IntegratedFixture
+    {
+        [Fact]
+        public void total_miss_returns_null()
+        {
+            var shop = new CoffeeShop();
+
+            theStore.Advanced.MetadataFor(shop)
+                .ShouldBeNull();
+        }
+
+        [Fact]
+        public void hit_returns_values()
+        {
+            var shop = new CoffeeShop();
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(shop);
+                session.SaveChanges();
+            }
+
+            var metadata = theStore.Advanced.MetadataFor(shop);
+
+            metadata.ShouldNotBeNull();
+            metadata.CurrentVersion.ShouldNotBe(Guid.Empty);
+            metadata.LastModified.ShouldNotBe(default(DateTime));
+        }
+    }
+}
--- src/Marten/AdvancedOptions.cs
+++ src/Marten/AdvancedOptions.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Baseline;
 using Marten.Linq;
+using Marten.Linq.QueryHandlers;
 using Marten.Schema;
 using Marten.Services;
+using Marten.Util;
+using Npgsql;
 
 namespace Marten
 {
@@ -71,6 +78,83 @@ namespace Marten
         public IList<IDocumentStorage> PrecompileAllStorage()
         {
             return Options.AllDocumentMappings.Select(x => _schema.StorageFor(x.DocumentType)).ToList();
+        }
+
+        /// <summary>
+        /// Fetch the entity version and last modified time from the database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public EntityMetadata MetadataFor<T>(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            var handler = new EntityMetadataQueryHandler(entity, _schema.StorageFor(typeof(T)), _schema.MappingFor(typeof(T)));
+
+            using (var connection = OpenConnection())
+            {
+                return connection.Fetch(handler, null);
+            }
+        }
+
+    }
+
+    public class EntityMetadata
+    {
+        public DateTime LastModified { get; }
+        public Guid CurrentVersion { get; }
+
+        public EntityMetadata(DateTime lastModified, Guid currentVersion)
+        {
+            LastModified = lastModified;
+            CurrentVersion = currentVersion;
+        }
+    }
+
+    public class EntityMetadataQueryHandler : IQueryHandler<EntityMetadata>
+    {
+        private readonly IDocumentStorage _storage;
+        private readonly IDocumentMapping _mapping;
+        private readonly object _id;
+
+        public EntityMetadataQueryHandler(object entity, IDocumentStorage storage, IDocumentMapping mapping)
+        {
+            _id = storage.Identity(entity);
+            _storage = storage;
+            _mapping = mapping;
+        }
+
+        public void ConfigureCommand(NpgsqlCommand command)
+        {
+            var sql =
+                $"select {DocumentMapping.VersionColumn}, {DocumentMapping.LastModifiedColumn} from {_mapping.Table.QualifiedName} where id = :id";
+
+
+            command.AppendQuery(sql);
+
+            command.AddParameter("id", _id);
+        }
+
+        public Type SourceType => _storage.DocumentType;
+        public EntityMetadata Handle(DbDataReader reader, IIdentityMap map)
+        {
+            if (!reader.Read()) return null;
+
+            var version = reader.GetFieldValue<Guid>(0);
+            var timestamp = reader.GetFieldValue<DateTime>(1);
+
+            return new EntityMetadata(timestamp, version);
+        }
+
+        public async Task<EntityMetadata> HandleAsync(DbDataReader reader, IIdentityMap map, CancellationToken token)
+        {
+            var hasAny = await reader.ReadAsync(token).ConfigureAwait(false);
+            if (!hasAny) return null;
+
+            var version = await reader.GetFieldValueAsync<Guid>(0, token).ConfigureAwait(false);
+            var timestamp = await reader.GetFieldValueAsync<DateTime>(1, token).ConfigureAwait(false);
+
+            return new EntityMetadata(timestamp, version);
         }
     }
 }
