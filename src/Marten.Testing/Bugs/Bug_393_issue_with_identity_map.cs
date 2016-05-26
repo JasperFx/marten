97d095ee439a85b5bfa44c8cc39aec2fe1539526
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-05-26 06:36:35 -0500

    git lost a bug test file somehow

---
+++ src/Marten.Testing/Bugs/Bug_393_issue_with_identity_map.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Marten.Schema.Identity;
+using Marten.Services;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_393_issue_with_identity_map : DocumentSessionFixture<IdentityMap>
+    {
+        [Fact]
+        public void load_non_existing_with_a_store_shoudl_return_new_added_document()
+        {
+            var routeId = CombGuidIdGeneration.New();
+
+            using (var session = theStore.OpenSession())
+            {
+                var details = session.Load<RouteDetails>(routeId);
+                details.ShouldBeNull();
+
+                var routeDetails = new RouteDetails { Id = routeId };
+                session.Store(routeDetails);
+
+                details = session.Load<RouteDetails>(routeId); // this was always null
+
+                details.ShouldBeTheSameAs(routeDetails);
+            }
+        }
+    }
+
+    public class Bug_393_issue_with_dirty_tracking_identity_map : DocumentSessionFixture<DirtyTrackingIdentityMap>
+    {
+        [Fact]
+        public void load_non_existing_with_a_store_shoudl_return_new_added_document()
+        {
+            var routeId = CombGuidIdGeneration.New();
+
+            using (var session = theStore.OpenSession())
+            {
+                var details = session.Load<RouteDetails>(routeId);
+                details.ShouldBeNull();
+
+                var routeDetails = new RouteDetails { Id = routeId };
+                session.Store(routeDetails);
+
+                details = session.Load<RouteDetails>(routeId);
+
+                details.ShouldBeTheSameAs(routeDetails);
+            }
+        }
+    }
+
+    public class RouteDetails
+    {
+        public Guid Id { get; set; }
+        public DateTime PlannedDate { get; set; }
+        public DateTime PlannedSince { get; set; }
+        public DateTime DrivingDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
