01b3b362a57b37dce10165aa35ef2202d2a123f6
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-06-01 20:54:06 -0500

    patching API supports Increment()

--- src/Marten.Testing/Acceptance/patching_api.cs
+++ src/Marten.Testing/Acceptance/patching_api.cs
@@ -8,7 +8,7 @@ namespace Marten.Testing.Acceptance
     public class patching_api : DocumentSessionFixture<NulloIdentityMap>
     {
         [Fact]
-        public void set_an_immediate_project_by_id()
+        public void set_an_immediate_property_by_id()
         {
             var target = Target.Random(true);
             target.Number = 5;
@@ -24,6 +24,26 @@ namespace Marten.Testing.Acceptance
             using (var query = theStore.QuerySession())
             {
                 query.Load<Target>(target.Id).Number.ShouldBe(10);
+            }
+        }
+
+        [Fact]
+        public void set_a_deep_property_by_id()
+        {
+            var target = Target.Random(true);
+            target.Inner.Number = 5;
+
+
+            theSession.Store(target);
+            theSession.SaveChanges();
+
+            theSession.Patch<Target>(target.Id).Set(x => x.Inner.Number, 10);
+            theSession.SaveChanges();
+
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Load<Target>(target.Id).Inner.Number.ShouldBe(10);
             }
         }
 
@@ -55,6 +75,97 @@ namespace Marten.Testing.Acceptance
                 query.Load<Target>(target4.Id).Number.ShouldBe(1);
                 query.Load<Target>(target5.Id).Number.ShouldBe(1);
                 query.Load<Target>(target6.Id).Number.ShouldBe(1);
+            }
+        }
+
+        [Fact]
+        public void increment_for_int()
+        {
+            var target = Target.Random();
+            target.Number = 6;
+
+            theSession.Store(target);
+            theSession.SaveChanges();
+
+            theSession.Patch<Target>(target.Id).Increment(x => x.Number);
+            theSession.SaveChanges();
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Load<Target>(target.Id).Number.ShouldBe(7);
+            }
+        }
+
+
+        [Fact]
+        public void increment_for_int_with_explicit_increment()
+        {
+            var target = Target.Random();
+            target.Number = 6;
+
+            theSession.Store(target);
+            theSession.SaveChanges();
+
+            theSession.Patch<Target>(target.Id).Increment(x => x.Number, 3);
+            theSession.SaveChanges();
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Load<Target>(target.Id).Number.ShouldBe(9);
+            }
+        }
+
+        [Fact]
+        public void increment_for_long()
+        {
+            var target = Target.Random();
+            target.Long = 13;
+
+            theSession.Store(target);
+            theSession.SaveChanges();
+
+            theSession.Patch<Target>(target.Id).Increment(x => x.Long);
+            theSession.SaveChanges();
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Load<Target>(target.Id).Long.ShouldBe(14);
+            }
+        }
+
+        [Fact]
+        public void increment_for_double()
+        {
+            var target = Target.Random();
+            target.Double = 11.2;
+
+            theSession.Store(target);
+            theSession.SaveChanges();
+
+            theSession.Patch<Target>(target.Id).Increment(x => x.Double, 2.4);
+            theSession.SaveChanges();
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Load<Target>(target.Id).Double.ShouldBe(13.6);
+            }
+        }
+
+        [Fact]
+        public void increment_for_float()
+        {
+            var target = Target.Random();
+            target.Float = 11.2F;
+
+            theSession.Store(target);
+            theSession.SaveChanges();
+
+            theSession.Patch<Target>(target.Id).Increment(x => x.Float, 2.4F);
+            theSession.SaveChanges();
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Load<Target>(target.Id).Float.ShouldBe(13.6F);
             }
         }
     }
