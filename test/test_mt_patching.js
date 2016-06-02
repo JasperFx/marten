3eaf9b92600fa232ad337396a535641010fc8a28
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-06-01 21:23:20 -0500

    had to take arrow functions out of the mocha tests because our CI server has an old Node

--- test/test_mt_patching.js
+++ test/test_mt_patching.js
@@ -1,8 +1,8 @@
 var Patch = require('./../javascript/mt_patching');
 var expect = require('chai').expect;
 
-describe('Patching API', () => {
-  it('can set the first level prop', () => {
+describe('Patching API', function() {
+  it('can set the first level prop', function() {
     var doc = {name: 'Frodo'};
     
     var patch = {type: 'set', path: 'name', value: 'Bilbo'};
@@ -10,105 +10,105 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({name: 'Bilbo'}); 
   });
   
-  it('can set a deep prop that already exists', () => {
+  it('can set a deep prop that already exists', function() {
      var doc = {member: {name: 'Frodo'}}; 
      var patch = {type: 'set', path: 'member.name', value: 'Bilbo'};
      
      expect(Patch(doc, patch)).to.deep.equal({member: {name: 'Bilbo'}}); 
   });
   
-  it('can set a deep prop does not already exists', () => {
+  it('can set a deep prop does not already exists', function() {
      var doc = {}; 
      var patch = {type: 'set', path: 'member.name', value: 'Bilbo'};
      
      expect(Patch(doc, patch)).to.deep.equal({member: {name: 'Bilbo'}}); 
   });
   
-  it('can set a 3 deep prop that already exists', () => {
+  it('can set a 3 deep prop that already exists', function() {
      var doc = {quest: {member: {name: 'Frodo'}}}; 
      var patch = {type: 'set', path: 'quest.member.name', value: 'Bilbo'};
      
      expect(Patch(doc, patch)).to.deep.equal({quest: {member: {name: 'Bilbo'}}}); 
   });
   
-  it('can set a 3 deep prop that does not yet exists', () => {
+  it('can set a 3 deep prop that does not yet exists', function() {
      var doc = {}; 
      var patch = {type: 'set', path: 'quest.member.name', value: 'Bilbo'};
      
      expect(Patch(doc, patch)).to.deep.equal({quest: {member: {name: 'Bilbo'}}}); 
   });
   
-  it('can increment int first level', () => {
+  it('can increment int first level', function() {
     var doc = {count: 5};
     var patch = {type: 'increment', path: 'count', increment: 1};
     
     expect(Patch(doc, patch)).to.deep.equal({count: 6});
   });
   
-  it('can increment int first level, non default increment', () => {
+  it('can increment int first level, non default increment', function() {
     var doc = {count: 5};
     var patch = {type: 'increment', path: 'count', increment: 3};
     
     expect(Patch(doc, patch)).to.deep.equal({count: 8});
   });
   
-  it('can increment int first level default increment', () => {
+  it('can increment int first level default increment', function() {
     var doc = {count: 5};
     var patch = {type: 'increment', path: 'count'};
     
     expect(Patch(doc, patch)).to.deep.equal({count: 6});
   });
   
-  it('can increment int 2 deep', () => {
+  it('can increment int 2 deep', function() {
     var doc = {summary: {count: 5}};
     var patch = {type: 'increment', path: 'summary.count'};
     
     expect(Patch(doc, patch)).to.deep.equal({summary: {count: 6}});
   });
   
-  it('can increment int 3 deep', () => {
+  it('can increment int 3 deep', function() {
     var doc = {region: {summary: {count: 5}}};
     var patch = {type: 'increment', path: 'region.summary.count'};
     
     expect(Patch(doc, patch)).to.deep.equal({region: {summary: {count: 6}}});
   });
   
-  it('can increment float first level', () => {
+  it('can increment float first level', function() {
     var doc = {count: 5.1};
     var patch = {type: 'increment_float', path: 'count', increment: 1};
     
     expect(Patch(doc, patch)).to.deep.equal({count: 6.1});
   });
   
-  it('can increment float first level, non default increment', () => {
+  it('can increment float first level, non default increment', function() {
     var doc = {count: 5.1};
     var patch = {type: 'increment_float', path: 'count', increment: 3.2};
     
     expect(Patch(doc, patch)).to.deep.equal({count: 8.3});
   });
   
-  it('can increment float first level default increment', () => {
+  it('can increment float first level default increment', function() {
     var doc = {count: 5.1};
     var patch = {type: 'increment_float', path: 'count'};
     
     expect(Patch(doc, patch)).to.deep.equal({count: 6.1});
   });
   
-  it('can increment float 2 deep', () => {
+  it('can increment float 2 deep', function() {
     var doc = {summary: {count: 5.1}};
     var patch = {type: 'increment_float', path: 'summary.count'};
     
     expect(Patch(doc, patch)).to.deep.equal({summary: {count: 6.1}});
   });
   
-  it('can increment float 3 deep', () => {
+  it('can increment float 3 deep', function() {
     var doc = {region: {summary: {count: 5.3}}};
     var patch = {type: 'increment_float', path: 'region.summary.count', increment: 1.1};
     
     expect(Patch(doc, patch)).to.deep.equal({region: {summary: {count: 6.4}}});
   });
   
-  it('can append to a first level array', () => {
+  it('can append to a first level array', function() {
     var doc = {numbers: []};
     
     var patch = {type: 'append', path: 'numbers', value: 5};
@@ -116,7 +116,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({numbers: [5]});
   });
   
-  it('can append to a first level array with existing elements', () => {
+  it('can append to a first level array with existing elements', function() {
     var doc = {numbers: [3, 4]};
     
     var patch = {type: 'append', path: 'numbers', value: 5};
@@ -124,7 +124,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({numbers: [3, 4, 5]});
   });
   
-  it('can append to a first level array when the array does not already exist', () => {
+  it('can append to a first level array when the array does not already exist', function() {
     var doc = {};
     
     var patch = {type: 'append', path: 'numbers', value: 5};
@@ -132,7 +132,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({numbers: [5]});
   });
   
-  it('can append to a 2nd level array when the array does not already exist', () => {
+  it('can append to a 2nd level array when the array does not already exist', function() {
     var doc = {};
     
     var patch = {type: 'append', path: 'region.numbers', value: 5};
@@ -140,7 +140,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({region: {numbers: [5]}});
   });
   
-  it('can append to a 3rd level array when the array does not already exist', () => {
+  it('can append to a 3rd level array when the array does not already exist', function() {
     var doc = {division: {region: {numbers: [3, 4]}}};
     
     var patch = {type: 'append', path: 'division.region.numbers', value: 5};
@@ -148,7 +148,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({division: {region: {numbers: [3, 4, 5]}}});
   });
   
-  it('can rename a prop shallow', () => {
+  it('can rename a prop shallow', function() {
     var doc = {number: 1};
     
     var patch = {type: 'rename', path: 'number', to: 'value'};
@@ -156,7 +156,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({value: 1});
   });
   
-  it('can rename a prop 2 deep', () => {
+  it('can rename a prop 2 deep', function() {
     var doc = {region: {number: 1}};
     
     var patch = {type: 'rename', path: 'region.number', to: 'value'};
@@ -164,7 +164,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({region: {value: 1}});
   });
   
-  it('can rename a prop 3 deep', () => {
+  it('can rename a prop 3 deep', function() {
     var doc = {country: {region: {number: 1}}};
     
     var patch = {type: 'rename', path: 'country.region.number', to: 'value'};
@@ -172,7 +172,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({country: {region: {value: 1}}});
   });
   
-  it('can insert into a child collection with default', () => {
+  it('can insert into a child collection with default', function() {
     var doc = {numbers: [3, 4]};
     
     var patch = {type: 'insert', path: 'numbers', value: 5};
@@ -180,7 +180,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({numbers: [5, 3, 4]});
   });
   
-  it('can insert into a child collection with designated index', () => {
+  it('can insert into a child collection with designated index', function() {
     var doc = {numbers: [3, 4]};
     
     var patch = {type: 'insert', path: 'numbers', value: 5, index: 1};
@@ -188,7 +188,7 @@ describe('Patching API', () => {
     expect(Patch(doc, patch)).to.deep.equal({numbers: [3, 5, 4]});
   });
   
-  it('can insert into a 2 deep child collection with designated index', () => {
+  it('can insert into a 2 deep child collection with designated index', function() {
     var doc = {region: {numbers: [3, 4]}};
     
     var patch = {type: 'insert', path: 'region.numbers', value: 5, index: 1};
