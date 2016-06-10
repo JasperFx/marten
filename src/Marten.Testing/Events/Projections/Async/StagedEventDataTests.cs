5f362b4150e1072304c67ae93875f9b889ef9aac
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-06-10 16:44:50 -0500

    little bit of work on event projection progression logging and reading

---
+++ src/Marten.Testing/Events/Projections/Async/StagedEventDataTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.Tasks;
+using Marten.Events;
+using Marten.Events.Projections.Async;
+using Marten.Util;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Events.Projections.Async
+{
+    public class StagedEventDataTests : IntegratedFixture
+    {
+        public StagedEventDataTests()
+        {
+            theStore.Schema.EnsureStorageExists(typeof(EventStream));
+        }
+
+        [Fact]
+        public async Task can_get_last_event_progression_on_initial_check()
+        {
+            var factory = new ConnectionSource();
+
+            var data = new StagedEventData(factory, new EventGraph(new StoreOptions()), new JilSerializer());
+
+            var lastEncountered = await data.LastEventProgression("something");
+
+            lastEncountered.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task can_get_last_event_progression_from_existing_data()
+        {
+            var factory = new ConnectionSource();
+
+            using (var conn = factory.Create())
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.Sql("insert into mt_event_progression (name, last_seq_id) values ('something', 121)");
+
+                    await cmd.ExecuteNonQueryAsync();
+
+                }
+            }
+
+            using (var data = new StagedEventData(factory, new EventGraph(new StoreOptions()), new JilSerializer()))
+            {
+                var lastEncountered = await data.LastEventProgression("something");
+
+                lastEncountered.ShouldBe(121);
+            }
+
+        }
+
+        [Fact]
+        public async Task can_register_progress_initial()
+        {
+            using (var data = new StagedEventData(new ConnectionSource(), new EventGraph(new StoreOptions()), new JilSerializer()))
+            {
+                await data.RegisterProgress("something", 111);
+
+                var lastEncountered = await data.LastEventProgression("something");
+
+                lastEncountered.ShouldBe(111);
+            }
+        }
+
+        [Fact]
+        public async Task can_register_subsequent_progress()
+        {
+            using (var data = new StagedEventData(new ConnectionSource(), new EventGraph(new StoreOptions()), new JilSerializer()))
+            {
+                await data.RegisterProgress("something", 111);
+                await data.RegisterProgress("something", 211);
+                await data.RegisterProgress("else", 333);
+
+                var lastEncountered = await data.LastEventProgression("something");
+
+                lastEncountered.ShouldBe(211);
+            }
+        }
+    }
+}
---
+++ src/Marten/Events/Projections/Async/AsyncDaemonClasses.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Marten.Schema;
+using Marten.Services;
+using Marten.Util;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace Marten.Events.Projections.Async
+{
+    public interface IStagedEventData
+    {
+        Task<long> LastEventProgression(string name);
+
+        Task RegisterProgress(string name, long lastEncountered);
+
+        Task<IEnumerable<IEvent>> FetchPage(string name, long lastEncountered, int pageSize, string[] eventTypeNames);
+    }
+
+    public class StagedEventData : IStagedEventData, IDisposable
+    {
+        private readonly EventGraph _events;
+        private readonly ISerializer _serializer;
+
+        public StagedEventData(IConnectionFactory factory, EventGraph events, ISerializer serializer)
+        {
+            _events = events;
+            _serializer = serializer;
+            _conn = factory.Create();
+
+            _conn.Open();
+
+            _sproc = new FunctionName(events.DatabaseSchemaName, "mt_mark_event_progression");
+        }
+
+        public readonly CancellationToken Cancellation = new CancellationToken();
+        private readonly NpgsqlConnection _conn;
+        private readonly FunctionName _sproc;
+
+        public async Task<long> LastEventProgression(string name)
+        {
+            var sql = $"select last_seq_id from {_events.DatabaseSchemaName}.mt_event_progression where name = :name";
+            var cmd = _conn.CreateCommand().Sql(sql).With("name", name);
+            using (var reader = await cmd.ExecuteReaderAsync(Cancellation).ConfigureAwait(false))
+            {
+                var hasAny = await reader.ReadAsync(Cancellation).ConfigureAwait(false);
+
+                if (!hasAny) return 0;
+
+                return await reader.GetFieldValueAsync<long>(0, Cancellation).ConfigureAwait(false);
+            }
+        }
+
+        public async Task RegisterProgress(string name, long lastEncountered)
+        {
+            var cmd = _conn.CreateCommand()
+                .CallsSproc(_sproc)
+                .With("name", name, NpgsqlDbType.Varchar)
+                .With("last_encountered", lastEncountered);
+
+
+            await cmd.ExecuteNonQueryAsync(Cancellation).ConfigureAwait(false);
+        }
+
+        public Task<IEnumerable<IEvent>> FetchPage(string name, long lastEncountered, int pageSize, string[] eventTypeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _conn.Close();
+            _conn.Dispose();
+        }
+    }
+}
