1b071f2208d6ece62b13c0a9ae3165bd3284c785
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-06-14 08:28:15 -0500

    bumping to 0.9.8, Closes GH-381

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,6 @@
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
-BUILD_VERSION = '0.9.7'
+BUILD_VERSION = '0.9.8'
 CONNECTION = ENV['connection']
 
 tc_build_number = ENV["BUILD_NUMBER"]
