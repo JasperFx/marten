6566300c3afcc3729c280ff858d4b36e384aaf17
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-06-17 15:12:38 -0500

    Docs on the optimistic concurrency

--- documentation/documentation/documents/advanced/optimistic_concurrency.md
+++ documentation/documentation/documents/advanced/optimistic_concurrency.md
@@ -1,5 +1,43 @@
 <!--Title:Optimistic Concurrency-->
 
-**This is done, docs are coming.** Support [offline optimistic locking](https://github.com/JasperFx/Marten/issues/35) by versioning against documents.
+Recent versions of Marten (&gt;0.9.5) have a new feature that allows you to enforce offline optimistic concurrency checks against documents that you are attempting to persist. You would use this feature if you're concerned about a document in your current session having been modified by another session since you originally loaded the document.
 
-TODO(write some content!)
+I first learned about this concept from Martin Fowler's [PEAA book](http://martinfowler.com/eaaCatalog/). From [Fowler's definition](http://martinfowler.com/eaaCatalog/optimisticOfflineLock.html), offline optimistic concurrency:
+
+<blockquote><i>Prevents conflicts between concurrent business transactions by detecting a conflict and rolling back the transaction.</i></blockquote>
+
+In Marten's case, you have to explicitly opt into optimistic versioning for each document type. You can do that with either an attribute on your document type like so:
+
+<[sample:UseOptimisticConcurrencyAttribute]>
+
+Or by using Marten's configuration API to do it programmatically:
+
+<[sample:configuring-optimistic-concurrency]>
+
+Once optimistic concurrency is turned on for the CoffeeShop document type, a session will now only be able to update a document if the document has been unchanged in the database since it was initially loaded.
+
+To demonstrate the failure case, consider the following  acceptance test from Marten's codebase:
+
+<[sample:update_with_stale_version_standard]>
+
+Marten is throwing an AggregateException for the entire batch of changes being persisted from SaveChanges()/SaveChangesAsync() after rolling back the current database transaction. The individual ConcurrencyException's inside of the aggregated exception expose information about the actual document type and identity that failed.
+
+
+## How it works
+
+The optimistic concurrency checks work by adding a new argument to the _upsert_ function for a document type to represent the 
+current version that was loaded by the current session. If that argument matches the value in the database column, everything works
+as normal. If the current version does not match the existing version in the database, the upsert function returns some data
+that tells the Marten client that there was a version mismatch. After Marten submits all the document changes in a call to
+`SaveChanges()/SaveChangesAsync()`, it checks for concurrency violations across all documents and throws a single `AggregatedException`
+with all the detected violations.
+
+
+## Storing a Document with a Given Version
+
+To designate the version of the document you're trying to store, you can use the `IDocumentSession.Store(doc, version)` method
+shown below:
+
+<[sample:store_with_the_right_version]>
+
+This method might come in handy if you detect that a document in your current session has been changed by another session.
--- src/Marten.Testing/Acceptance/optimistic_concurrency.cs
+++ src/Marten.Testing/Acceptance/optimistic_concurrency.cs
@@ -12,6 +12,18 @@ namespace Marten.Testing.Acceptance
     public class optimistic_concurrency : IntegratedFixture
     {
 
+
+        public void example_configuration()
+        {
+            // SAMPLE: configuring-optimistic-concurrency
+    var store = DocumentStore.For(_ =>
+    {
+        // Adds optimistic concurrency checking to Issue
+        _.Schema.For<Issue>().UseOptimisticConcurrency(true);
+    });
+            // ENDSAMPLE
+        }
+
         [Fact]
         public void can_generate_the_upsert_smoke_test_with_95_style()
         {
@@ -105,51 +117,54 @@ namespace Marten.Testing.Acceptance
             }
         }
 
-        [Fact]
-        public void update_with_stale_version_standard()
+
+        // SAMPLE: update_with_stale_version_standard
+    [Fact]
+    public void update_with_stale_version_standard()
+    {
+        var doc1 = new CoffeeShop();
+        using (var session = theStore.OpenSession())
         {
-            var doc1 = new CoffeeShop();
-            using (var session = theStore.OpenSession())
-            {
-                session.Store(doc1);
-                session.SaveChanges();
-            }
-
-            var session1 = theStore.DirtyTrackedSession();
-            var session2 = theStore.DirtyTrackedSession();
-
-            var session1Copy = session1.Load<CoffeeShop>(doc1.Id);
-            var session2Copy = session2.Load<CoffeeShop>(doc1.Id);
-
-            try
-            {
-                session1Copy.Name = "Mozart's";
-                session2Copy.Name = "Dominican Joe's";
-
-                // Should go through just fine
-                session2.SaveChanges();
-
-
-                var ex = Exception<AggregateException>.ShouldBeThrownBy(() =>
-                {
-                    session1.SaveChanges();
-                });
-
-                var concurrency = ex.InnerExceptions.OfType<ConcurrencyException>().Single();
-                concurrency.Message.ShouldBe($"Optimistic concurrency check failed for {typeof(CoffeeShop).FullName} #{doc1.Id}");
-            }
-            finally
-            {
-                session1.Dispose();
-                session2.Dispose();
-            }
-
-            using (var query = theStore.QuerySession())
-            {
-                query.Load<CoffeeShop>(doc1.Id).Name.ShouldBe("Dominican Joe's");
-            }
-
+            session.Store(doc1);
+            session.SaveChanges();
         }
+
+        var session1 = theStore.DirtyTrackedSession();
+        var session2 = theStore.DirtyTrackedSession();
+
+        var session1Copy = session1.Load<CoffeeShop>(doc1.Id);
+        var session2Copy = session2.Load<CoffeeShop>(doc1.Id);
+
+        try
+        {
+            session1Copy.Name = "Mozart's";
+            session2Copy.Name = "Dominican Joe's";
+
+            // Should go through just fine
+            session2.SaveChanges();
+
+
+            var ex = Exception<AggregateException>.ShouldBeThrownBy(() =>
+            {
+                session1.SaveChanges();
+            });
+
+            var concurrency = ex.InnerExceptions.OfType<ConcurrencyException>().Single();
+            concurrency.Message.ShouldBe($"Optimistic concurrency check failed for {typeof(CoffeeShop).FullName} #{doc1.Id}");
+        }
+        finally
+        {
+            session1.Dispose();
+            session2.Dispose();
+        }
+
+        using (var query = theStore.QuerySession())
+        {
+            query.Load<CoffeeShop>(doc1.Id).Name.ShouldBe("Dominican Joe's");
+        }
+
+    }
+        // ENDSAMPLE
 
 
         [Fact]
@@ -397,32 +412,34 @@ namespace Marten.Testing.Acceptance
             }
         }
 
-        [Fact]
-        public void store_with_the_right_version()
+        // SAMPLE: store_with_the_right_version
+    [Fact]
+    public void store_with_the_right_version()
+    {
+        var doc1 = new CoffeeShop();
+        using (var session = theStore.OpenSession())
         {
-            var doc1 = new CoffeeShop();
-            using (var session = theStore.OpenSession())
-            {
-                session.Store(doc1);
-                session.SaveChanges();
-            }
-
-            var metadata = theStore.Advanced.MetadataFor(doc1);
-
-            using (var session = theStore.OpenSession())
-            {
-                doc1.Name = "Mozart's";
-                session.Store(doc1, metadata.CurrentVersion);
-
-                session.SaveChanges();
-            }
-
-            using (var query = theStore.QuerySession())
-            {
-                query.Load<CoffeeShop>(doc1.Id).Name
-                    .ShouldBe("Mozart's");
-            }
+            session.Store(doc1);
+            session.SaveChanges();
         }
+
+        var metadata = theStore.Advanced.MetadataFor(doc1);
+
+        using (var session = theStore.OpenSession())
+        {
+            doc1.Name = "Mozart's";
+            session.Store(doc1, metadata.CurrentVersion);
+
+            session.SaveChanges();
+        }
+
+        using (var query = theStore.QuerySession())
+        {
+            query.Load<CoffeeShop>(doc1.Id).Name
+                .ShouldBe("Mozart's");
+        }
+    }
+        // ENDSAMPLE
 
         [Fact]
         public async Task store_with_the_right_version_async()
@@ -512,10 +529,12 @@ namespace Marten.Testing.Acceptance
         public Guid Id { get; set; } = Guid.NewGuid();
     }
 
+    // SAMPLE: UseOptimisticConcurrencyAttribute
     [UseOptimisticConcurrency]
     public class CoffeeShop : Shop
     {
         // Guess where I'm at as I code this?
         public string Name { get; set; } = "Starbucks";
     }
+    // ENDSAMPLE
 }
