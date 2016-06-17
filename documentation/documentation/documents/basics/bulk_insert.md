d73b4e09557d5872353effe7e47ee6ace4fc5828
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-06-17 10:57:23 -0500

    Updated docs on bulk inserts

--- documentation/documentation/documents/basics/bulk_insert.md
+++ documentation/documentation/documents/basics/bulk_insert.md
@@ -6,3 +6,28 @@ Marten supports [Postgresql's COPY](http://www.postgresql.org/docs/9.4/static/sq
 <[sample:using_bulk_insert]>
 
 The bulk insert is done with a single transaction. For really large document collections, you may need to page the calls to `IDocumentStore.BulkInsert()`.
+
+**By default, bulk insert will fail if there are any duplicate id's between the documents being inserted and the existing database data.**
+
+If you want to use the bulk insert feature, but you know that you could have duplicate documents, you can use one of the two following optional modes:
+
+## Ignore Duplicate Values
+
+In this case, you only want to insert brand new documents and just throwaway any potential changes to existing documents.
+
+<[sample:bulk_insert_with_IgnoreDuplicates]>
+
+Internally, Marten creates a temporary table matching the targeted document table and inserts the new values into that table. After writing those documents, Marten issues
+an INSERT command to copy rows from the temporary table to the real table, filtering out any matches in existing id's.
+
+## Overwrite Duplicates
+
+In the second case, you want to use the bulk insert to write a batch of documents and overwrite any existing data with matching id's:
+
+<[sample:bulk_insert_with_OverwriteExisting]>
+
+Internally, Marten creates a temporary table matching the targeted document table and inserts the new values into that table. After writing those documents, Marten issues
+an INSERT command to copy rows from the temporary table to the real table, filtering out any matches in existing id's, then a second UPDATE command to overwrite data from 
+the matching id's.
+
+
--- src/Marten.Testing/bulk_loading_Tests.cs
+++ src/Marten.Testing/bulk_loading_Tests.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using System.Linq;
-using Marten.Schema.Identity;
+﻿using System.Linq;
 using Marten.Services;
 using Marten.Testing.Fixtures;
 using Shouldly;
@@ -11,104 +9,6 @@ namespace Marten.Testing
     public class bulk_loading_Tests : DocumentSessionFixture<NulloIdentityMap>
     {
         [Fact]
-        public void load_with_small_batch()
-        {
-            // SAMPLE: using_bulk_insert
-            // This is just creating some randomized
-            // document data
-            var data = Target.GenerateRandomData(100).ToArray();
-
-            // Load all of these into a Marten-ized database
-            theStore.BulkInsert(data);
-
-            // And just checking that the data is actually there;)
-            theSession.Query<Target>().Count().ShouldBe(data.Length);
-            // ENDSAMPLE
-
-
-            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
-
-        }
-
-        [Fact]
-        public void load_with_small_batch_and_duplicated_fields()
-        {
-            StoreOptions(_ =>
-            {
-                _.Schema.For<Target>().Searchable(x => x.String);
-            });
-
-            var data = Target.GenerateRandomData(100).ToArray();
-
-            theStore.BulkInsert(data);
-
-            theSession.Query<Target>().Count().ShouldBe(data.Length);
-
-            theSession.Query<Target>().Where(x => x.String == data[0].String).Any()
-                .ShouldBeTrue();
-        }
-
-        [Fact]
-        public void load_with_small_batch_and_duplicated_data_field()
-        {
-            StoreOptions(_ =>
-            {
-                _.Schema.For<Target>().Searchable(x => x.Date);
-            });
-
-            var data = Target.GenerateRandomData(100).ToArray();
-
-            theStore.BulkInsert(data);
-
-            theSession.Query<Target>().Count().ShouldBe(data.Length);
-
-            theSession.Query<Target>().Where(x => x.Date == data[0].Date).Any()
-                .ShouldBeTrue();
-        }
-
-
-        [Fact]
-        public void load_with_multiple_batches()
-        {
-            var data = Target.GenerateRandomData(100).ToArray();
-
-            theStore.BulkInsert(data, batchSize:15);
-
-            theSession.Query<Target>().Count().ShouldBe(data.Length);
-
-            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
-        }
-
-        [Fact]
-        public void load_with_small_batch_and_ignore_duplicates_smoke_test()
-        {
-            var data = Target.GenerateRandomData(100).ToArray();
-
-            theStore.BulkInsert(data, mode: BulkInsertMode.IgnoreDuplicates);
-
-            theSession.Query<Target>().Count().ShouldBe(data.Length);
-
-
-            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
-
-        }
-
-        [Fact]
-        public void load_with_small_batch_and_overwrites_smoke_test()
-        {
-            var data = Target.GenerateRandomData(100).ToArray();
-
-            theStore.BulkInsert(data, mode: BulkInsertMode.OverwriteExisting);
-
-            theSession.Query<Target>().Count().ShouldBe(data.Length);
-
-
-            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
-
-        }
-
-
-        [Fact]
         public void load_with_ignore_duplicates()
         {
             var data1 = Target.GenerateRandomData(100).ToArray();
@@ -118,25 +18,36 @@ namespace Marten.Testing
             var data2 = Target.GenerateRandomData(50).ToArray();
 
             // Rigging up data2 so 5 of its values would be getting lost
-            for (int i = 0; i < 5; i++)
+            for (var i = 0; i < 5; i++)
             {
                 data2[i].Id = data1[i].Id;
                 data2[i].Number = -1;
             }
 
-            theStore.BulkInsert(data2, mode: BulkInsertMode.IgnoreDuplicates);
+            theStore.BulkInsert(data2, BulkInsertMode.IgnoreDuplicates);
 
             using (var session = theStore.QuerySession())
             {
                 session.Query<Target>().Count().ShouldBe(data1.Length + data2.Length - 5);
 
-                for (int i = 0; i < 5; i++)
+                for (var i = 0; i < 5; i++)
                 {
                     session.Load<Target>(data1[i].Id).Number.ShouldBeGreaterThanOrEqualTo(0);
                 }
             }
+        }
 
 
+        [Fact]
+        public void load_with_multiple_batches()
+        {
+            var data = Target.GenerateRandomData(100).ToArray();
+
+            theStore.BulkInsert(data, batchSize: 15);
+
+            theSession.Query<Target>().Count().ShouldBe(data.Length);
+
+            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
         }
 
         [Fact]
@@ -149,26 +60,103 @@ namespace Marten.Testing
             var data2 = Target.GenerateRandomData(50).ToArray();
 
             // Rigging up data2 so 5 of its values would be getting lost
-            for (int i = 0; i < 5; i++)
+            for (var i = 0; i < 5; i++)
             {
                 data2[i].Id = data1[i].Id;
                 data2[i].Number = -1;
             }
 
-            theStore.BulkInsert(data2, mode: BulkInsertMode.OverwriteExisting);
+            theStore.BulkInsert(data2, BulkInsertMode.OverwriteExisting);
 
             using (var session = theStore.QuerySession())
             {
                 session.Query<Target>().Count().ShouldBe(data1.Length + data2.Length - 5);
 
                 // Values were overwritten
-                for (int i = 0; i < 5; i++)
+                for (var i = 0; i < 5; i++)
                 {
                     session.Load<Target>(data1[i].Id).Number.ShouldBe(-1);
                 }
             }
         }
 
+        [Fact]
+        public void load_with_small_batch()
+        {
+            // SAMPLE: using_bulk_insert
+            // This is just creating some randomized
+            // document data
+            var data = Target.GenerateRandomData(100).ToArray();
 
+            // Load all of these into a Marten-ized database
+            theStore.BulkInsert(data, batchSize: 500);
+
+            // And just checking that the data is actually there;)
+            theSession.Query<Target>().Count().ShouldBe(data.Length);
+            // ENDSAMPLE
+
+
+            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void load_with_small_batch_and_duplicated_data_field()
+        {
+            StoreOptions(_ => { _.Schema.For<Target>().Searchable(x => x.Date); });
+
+            var data = Target.GenerateRandomData(100).ToArray();
+
+            theStore.BulkInsert(data);
+
+            theSession.Query<Target>().Count().ShouldBe(data.Length);
+
+            theSession.Query<Target>().Where(x => x.Date == data[0].Date).Any()
+                .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void load_with_small_batch_and_duplicated_fields()
+        {
+            StoreOptions(_ => { _.Schema.For<Target>().Searchable(x => x.String); });
+
+            var data = Target.GenerateRandomData(100).ToArray();
+
+            theStore.BulkInsert(data);
+
+            theSession.Query<Target>().Count().ShouldBe(data.Length);
+
+            theSession.Query<Target>().Where(x => x.String == data[0].String).Any()
+                .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void load_with_small_batch_and_ignore_duplicates_smoke_test()
+        {
+            // SAMPLE: bulk_insert_with_IgnoreDuplicates
+    var data = Target.GenerateRandomData(100).ToArray();
+
+    theStore.BulkInsert(data, BulkInsertMode.IgnoreDuplicates);
+            // ENDSAMPLE
+
+            theSession.Query<Target>().Count().ShouldBe(data.Length);
+
+
+            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void load_with_small_batch_and_overwrites_smoke_test()
+        {
+            // SAMPLE: bulk_insert_with_OverwriteExisting
+    var data = Target.GenerateRandomData(100).ToArray();
+
+    theStore.BulkInsert(data, BulkInsertMode.OverwriteExisting);
+            // ENDSAMPLE
+
+            theSession.Query<Target>().Count().ShouldBe(data.Length);
+
+
+            theSession.Load<Target>(data[0].Id).ShouldNotBeNull();
+        }
     }
 }
