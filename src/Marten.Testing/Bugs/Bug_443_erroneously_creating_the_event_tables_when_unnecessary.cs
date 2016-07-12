2cfd87f8ffa69f865c46b294e2e5d006db1a6c3e
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-07-12 17:09:55 -0500

    tests to verify the event store table bug. Closes GH-443

---
+++ src/Marten.Testing/Bugs/Bug_443_erroneously_creating_the_event_tables_when_unnecessary.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Baseline;
+using Marten.Testing.Documents;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_443_erroneously_creating_the_event_tables_when_unnecessary : IntegratedFixture
+    {
+        [Fact]
+        public void event_table_should_not_be_there_if_unused()
+        {
+            theStore.Schema.EnsureStorageExists(typeof(User));
+
+            theStore.Schema.ApplyAllConfiguredChangesToDatabase();
+
+            var tables = theStore.Schema.DbObjects.SchemaTables();
+
+            tables.Any(x => x.Name == "mt_events").ShouldBeFalse();
+            tables.Any(x => x.Name == "mt_streams").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void event_tables_are_not_part_of_the_ddl()
+        {
+            var ddl = theStore.Schema.ToDDL();
+
+            ddl.ShouldNotContain("mt_events");
+            ddl.ShouldNotContain("mt_streams");
+        }
+
+        [Fact]
+        public void not_part_of_the_patch()
+        {
+            var patch = theStore.Schema.ToPatch();
+
+            patch.UpdateDDL.ShouldNotContain("mt_events");
+            patch.UpdateDDL.ShouldNotContain("mt_streams");
+        }
+
+        [Fact]
+        public void not_part_of_the_by_type_dump()
+        {
+            var fileSystem = new FileSystem();
+            fileSystem.CleanDirectory("sql");
+            
+            theStore.Schema.WriteDDLByType("sql");
+
+            fileSystem.FindFiles("sql", FileSet.Shallow("*.sql"))
+                .Any(x => x.EndsWith("mt_streams.sql"))
+                .ShouldBeFalse();
+
+            
+        }
+    }
+}
