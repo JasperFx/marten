d605e40eb601ca2833c9681861a9b756afd08cdc
Corey Kaylor <corey@kaylors.net>
2016-08-25 15:28:10 -0700

    Removing restore workaround

--- rakefile.rb
+++ rakefile.rb
@@ -134,7 +134,7 @@ namespace :nuget do
 
   desc 'Restores nuget packages with paket'
   task :restore => [:bootstrap] do
-    sh 'dotnet restore --disable-parallel'
+    sh 'dotnet restore'
     #sh '.paket/paket.exe restore'
   end
 
