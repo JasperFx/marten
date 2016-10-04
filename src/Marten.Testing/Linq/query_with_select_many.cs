87977dd4ac3f36442585997859c8cb9e887156d0
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-10-04 13:41:54 -0500

    WIP on SelectMany

---
+++ src/Marten.Testing/Linq/query_with_select_many.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Linq
+{
+    public class query_with_select_many : IntegratedFixture
+    {
+        [Fact]
+        public void can_do_simple_select_many_against_simple_array()
+        {
+            var product1 = new Product {Tags = new [] {"a", "b", "c"}};
+            var product2 = new Product {Tags = new [] {"b", "c", "d"}};
+            var product3 = new Product {Tags = new [] {"d", "e", "f"}};
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(product1, product2, product3);
+                session.SaveChanges();
+            }
+
+            using (var query = theStore.QuerySession())
+            {
+                var names = query.Query<Product>().SelectMany(x => x.Tags).ToList();
+
+                names.Count.ShouldBe(6);
+            }
+        }
+    }
+
+    public class Product
+    {
+        public Guid Id;
+        public string[] Tags { get; set; }
+
+    }
+}
