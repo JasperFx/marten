838012574dc94f5587d58eb7f26d1046ef4969b2
Jonathan Mezach <jonathanm@infosupport.com>
2016-10-05 13:30:23 +0200

    Initial stab at supporting Apply methods with Event<T>

--- src/Marten.Testing/Events/Projections/AggregationStepTests.cs
+++ src/Marten.Testing/Events/Projections/AggregationStepTests.cs
@@ -6,6 +6,7 @@ using Baseline;
 using Baseline.Reflection;
 using Marten.Events.Projections;
 using Xunit;
+using Marten.Events;
 
 namespace Marten.Testing.Events.Projections
 {
@@ -24,9 +25,29 @@ namespace Marten.Testing.Events.Projections
             var party = new QuestParty();
             var joinedEvent = new MembersJoined {Members = new []{"Wolverine", "Cyclops", "Nightcrawler"}};
 
-            joinedStep.Apply(party, joinedEvent);
+            joinedStep.Apply(party, new Event<MembersJoined>(joinedEvent));
 
             party.Members.ShouldHaveTheSameElementsAs(joinedEvent.Members);
+        }
+
+        [Fact]
+        public void can_build_aggregation_step_for_an_event_apply_method()
+        {
+            Expression<Action<QuestParty, MembersDeparted>> apply = (p, j) => p.Apply(new Event<MembersDeparted>(j));
+
+
+            var departed = ReflectionHelper.GetMethod<QuestParty>(x => x.Apply(new Event<MembersDeparted>(new MembersDeparted())));
+
+            var departedStep = new EventAggregationStep<QuestParty, MembersDeparted>(departed);
+
+            var party = new QuestParty();
+            var joinedEvent = new MembersJoined { Members = new[] { "Wolverine", "Cyclops", "Nightcrawler" } };
+            var departedEvent = new MembersDeparted { Members = new[] { "Nightcrawler" } };
+
+            party.Apply(joinedEvent);
+            departedStep.Apply(party, new Event<MembersDeparted>(departedEvent));
+
+            party.Members.ShouldHaveTheSameElementsAs(joinedEvent.Members.Take(2));
         }
     }
 
