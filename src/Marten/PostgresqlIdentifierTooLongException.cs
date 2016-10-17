7d52976839861a2dab7775d8856380c18f0a0dc7
James Hopper <jimgolfgti@gmail.com>
2016-10-17 22:23:08 +0100

    Checks for null, empty, space

--- src/Marten/PostgresqlIdentifierTooLongException.cs
+++ src/Marten/PostgresqlIdentifierTooLongException.cs
@@ -7,7 +7,8 @@ namespace Marten
         public int Length { get; set; }
         public string Name { get; set; }
 
-        public PostgresqlIdentifierTooLongException(int length, string name) : base($"Database identifier {name} would be truncated. The {nameof(StoreOptions)}{nameof(StoreOptions.NameDataLength)} property is currently {length}. You may want to change this value with a corresponding change to Postgresql's NAMEDATALEN")
+        public PostgresqlIdentifierTooLongException(int length, string name) 
+            : base($"Database identifier {name} would be truncated. The {nameof(StoreOptions)}{nameof(StoreOptions.NameDataLength)} property is currently {length}. You may want to change this value with a corresponding change to Postgresql's NAMEDATALEN")
         {
             Length = length;
             Name = name;
