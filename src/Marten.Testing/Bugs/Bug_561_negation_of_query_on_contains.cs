c8e3a23e9b01473676762bfed58147bd82e6681f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-10-20 14:03:00 -0500

    better Linq support for !Contains(element) type queries. Closes GH-561

---
+++ src/Marten.Testing/Bugs/Bug_561_negation_of_query_on_contains.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Marten.Services;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_561_negation_of_query_on_contains : DocumentSessionFixture<NulloIdentityMap>
+    {
+        public Bug_561_negation_of_query_on_contains()
+        {
+            var doc1 = new DocWithArrays { Strings = new string[] { "a", "b", "c" } };
+            var doc2 = new DocWithArrays { Strings = new string[] { "c", "d", "e" } };
+            var doc3 = new DocWithArrays { Strings = new string[] { "d", "e", "f" } };
+            var doc4 = new DocWithArrays { Strings = new string[] { "g", "h", "i" } };
+
+            theSession.Store(doc1, doc2, doc3, doc4);
+
+            theSession.SaveChanges();
+        }
+
+        [Fact]
+        public void negated_contains()
+        {
+            theSession.Query<DocWithArrays>().Count(x => !x.Strings.Contains("c"))
+                .ShouldBe(2);
+
+        }
+
+        [Fact]
+        public void NotContainsInExpressionThrowsNotSupportedException()
+        {
+            theSession.Query<DocWithArrays>().Count(x => x.Strings.Contains("d") && !x.Strings.Contains("c"))
+                .ShouldBe(1);
+        }
+
+        [Fact]
+        public void ExpressionWithNotContainsReturnsCorrectResults()
+        {
+            theSession.Query<DocWithArrays>().Count(x => x.Strings.Contains("d") && !x.Strings.Contains("c"))
+                .ShouldBe(1);
+
+        }
+    }
+
+    public class DocWithArrays
+    {
+        public Guid Id;
+
+        public int[] Numbers;
+
+        public string[] Strings;
+
+        public DateTime[] Dates;
+    }
+}
---
+++ src/Marten/Linq/NotVisitor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using Baseline;
+using Marten.Schema;
+using Remotion.Linq.Parsing;
+
+namespace Marten.Linq
+{
+    public class NotVisitor : RelinqExpressionVisitor
+    {
+        private readonly MartenExpressionParser.WhereClauseVisitor _parent;
+        private readonly IQueryableDocument _mapping;
+        private readonly Action<IWhereFragment> _callback;
+
+        public NotVisitor(MartenExpressionParser.WhereClauseVisitor parent, IQueryableDocument mapping, Action<IWhereFragment> callback)
+        {
+            _parent = parent;
+            _mapping = mapping;
+            _callback = callback;
+        }
+
+        protected override Expression VisitMember(MemberExpression expression)
+        {
+            if (expression.Type == typeof (bool))
+            {
+                var locator = _mapping.JsonLocator(expression);
+                var @where = new WhereFragment($"{locator} = False");
+                _callback(@where);
+            }
+
+            return base.VisitMember(expression);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression expression)
+        {
+            if (expression.Type == typeof (bool) && expression.NodeType == ExpressionType.NotEqual)
+            {
+                var binaryExpression = expression.As<BinaryExpression>();
+                var locator = _mapping.JsonLocator(binaryExpression.Left);
+                if (binaryExpression.Right.NodeType == ExpressionType.Constant &&
+                    binaryExpression.Right.As<ConstantExpression>().Value == null)
+                {
+                    var @where = new WhereFragment($"({locator}) IS NULL");
+                    _callback(@where);
+                }
+            }
+
+            return base.VisitBinary(expression);
+        }
+    }
+}
