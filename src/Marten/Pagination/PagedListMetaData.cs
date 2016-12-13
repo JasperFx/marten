ba137868f5d66f42170717b7a39dfb4fa669be21
mysticmind <babu.annamalai@gmail.com>
2016-12-14 01:03:23 +0530

    1)Changed pagination implementation with a new PagedList and PagedListMetaData classes 2)PagedQueryableExtensions with PagedQuery and PagedQueryAsync which returns PagedList 3)Removed PagedList* methods from QueryableExtensions 4)Added/updated unit tests

---
+++ src/Marten/Pagination/PagedListMetaData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marten.Pagination
+{
+    public class PagedListMetaData
+    {
+        public PagedListMetaData(int pageNumber, int pageSize, int totalItemCount)
+        {
+            TotalItemCount = totalItemCount;
+            this.PageSize = pageSize;
+            PageNumber = pageNumber;
+            PageCount = TotalItemCount > 0
+                            ? (int)Math.Ceiling(TotalItemCount / (double)PageSize)
+                            : 0;
+            HasPreviousPage = PageNumber > 1;
+            HasNextPage = PageNumber < PageCount;
+            IsFirstPage = PageNumber == 1;
+            IsLastPage = PageNumber >= PageCount;
+            FirstItemOnPage = (PageNumber - 1) * PageSize + 1;
+            var numberOfLastItemOnPage = FirstItemOnPage + PageSize - 1;
+            LastItemOnPage = numberOfLastItemOnPage > TotalItemCount
+                                ? TotalItemCount
+                                : numberOfLastItemOnPage;
+        }
+
+        public int PageNumber { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public int PageCount { get; private set; }
+
+        public int TotalItemCount { get; private set; }
+
+        public bool HasPreviousPage { get; private set; }
+        
+        public bool HasNextPage { get; private set; }
+
+        public bool IsFirstPage { get; private set; }
+
+        public bool IsLastPage { get; private set; }
+
+        public int FirstItemOnPage { get; private set; }
+
+        public int LastItemOnPage { get; private set; }
+    }
+}
---
+++ src/Marten/Pagination/PagedQueryableExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Marten.Linq;
+using Baseline;
+
+namespace Marten.Pagination
+{
+    public static class PagedQueryableExtensions
+    {
+        public static PagedList<T> PagedQuery<T>(this IQueryable<T> queryable, int pageNumber, int pageSize, Func<IQueryable<T>, IQueryable<T>> order = null)
+        {
+            if (pageNumber < 1)
+                throw new ArgumentOutOfRangeException($"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
+            if (pageSize < 1)
+                throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+
+            var totalItemCount = queryable.Count();
+
+
+            if (order != null)
+            {
+                queryable = order(queryable);
+            }
+
+            var skipCount = pageNumber > 0 ? (pageNumber - 1) * pageSize : 0;
+
+            IEnumerable<T> items = null;
+
+            if (totalItemCount > 0)
+            {
+                items = queryable.Skip(skipCount).Take(pageSize).AsEnumerable();
+            }
+
+            return new PagedList<T>(pageNumber, pageSize, totalItemCount, items);
+        }
+
+        public static async Task<PagedList<T>> PagedQueryAsync<T>(
+            this IQueryable<T> queryable,
+            int pageNumber,
+            int pageSize,
+            Func<IQueryable<T>, IQueryable<T>> order = null,
+            CancellationToken token = default(CancellationToken))
+        {
+            if (pageNumber < 1)
+                throw new ArgumentOutOfRangeException($"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
+            if (pageSize < 1)
+                throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+
+            int totalItemCount = await queryable.CountAsync().ConfigureAwait(false);
+
+            if (order != null)
+            {
+                queryable = order(queryable);
+            }
+
+            var skipCount = pageNumber > 0 ? (pageNumber - 1) * pageSize : 0;
+
+            IEnumerable<T> items = null;
+
+            if (totalItemCount > 0)
+            {
+                items = await queryable.Skip(skipCount).Take(pageSize).As<IMartenQueryable>().ToListAsync<T>(token);
+            }
+
+            return new PagedList<T>(pageNumber, pageSize, totalItemCount, items);
+        }
+    }
+}
