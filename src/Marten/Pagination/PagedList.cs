e5b34de696a0eb83b1cfb6ec55ed49f2996e7d26
mysticmind <babu.annamalai@gmail.com>
2016-12-15 17:27:12 +0530

    1)Applied a combination of R# and style cop to fix code styling and 2)Added code comments and documentation

--- src/Marten/Pagination/PagedList.cs
+++ src/Marten/Pagination/PagedList.cs
@@ -1,43 +1,81 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Marten.Pagination
 {
+    /// <summary>
+    /// Class to return The paged list from a paged query.
+    /// </summary>
+    /// <typeparam name="T">Document Type</typeparam>
     public class PagedList<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Field to hold the paged query result
+        /// </summary>
         private readonly List<T> _subset = new List<T>();
+
+        /// <summary>
+        /// Field to hold page meta data
+        /// </summary>
         private PagedListMetaData _metaData;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PagedList{T}" /> class.
+        /// </summary>
+        /// <param name="pageNumber">Current page number</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="totalItemCount">Total count of all records</param>
+        /// <param name="items">Paged query result</param>
         public PagedList(int pageNumber, int pageSize, int totalItemCount, IEnumerable<T> items)
         {
             this._metaData = new PagedListMetaData(pageNumber, pageSize, totalItemCount);
 
             if (items != null)
+            {
                 this._subset.AddRange(items);
+            }
         }
 
+        /// <summary>
+        /// Return the paged query result
+        /// </summary>
+        /// <param name="index">Index to fetch item from paged query result</param>
+        /// <returns>/returns item from paged query result</returns>
         public T this[int index]
         {
             get { return this._subset[index]; }
         }
+
+        /// <summary>
+        /// Return the number of records in the paged query result
+        /// </summary>
         public int Count
         {
-            get { return this._subset.Count(); }
+            get { return this._subset.Count(); }   
         }
 
+        /// <summary>
+        /// Return page meta data
+        /// </summary>
         public PagedListMetaData MetaData
         {
-            get { return this._metaData;  }
+            get { return this._metaData; }
         }
 
+        /// <summary>
+        /// Generic Enumerator
+        /// </summary>
+        /// <returns>Generic Enumerator of paged query result</returns>
         public IEnumerator<T> GetEnumerator()
         {
             return ((IEnumerable<T>)this._subset).GetEnumerator();
         }
 
+        /// <summary>
+        /// Enumerator
+        /// </summary>
+        /// <returns>Enumerator of paged query result</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return ((IEnumerable<T>)this._subset).GetEnumerator();
--- src/Marten/Pagination/PagedQueryableExtensions.cs
+++ src/Marten/Pagination/PagedQueryableExtensions.cs
@@ -8,63 +8,112 @@ using Baseline;
 
 namespace Marten.Pagination
 {
+    /// <summary>
+    /// Extension methods on <see cref="IQueryable{T}"/> for performing paged queries
+    /// </summary>
     public static class PagedQueryableExtensions
     {
-        public static PagedList<T> PagedQuery<T>(this IQueryable<T> queryable, int pageNumber, int pageSize, Func<IQueryable<T>, IQueryable<T>> order = null)
+        /// <summary>
+        /// Extension method to return a paged results
+        /// </summary>
+        /// <typeparam name="T">Document Type</typeparam>
+        /// <param name="queryable">Extension point on <see cref="IQueryable{T}"/></param>
+        /// <param name="pageNumber">Page number</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="order">Order clause</param>
+        /// <returns>return paged result</returns>
+        public static PagedList<T> PagedQuery<T>(
+            this IQueryable<T> queryable, 
+            int pageNumber, 
+            int pageSize, 
+            Func<IQueryable<T>, IQueryable<T>> order = null)
         {
+            // throw an argument exception if page number is less than one
             if (pageNumber < 1)
+            {
                 throw new ArgumentOutOfRangeException($"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
-            if (pageSize < 1)
-                throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+            }
 
+            // throw argument exception if page size is less than one
+            if (pageSize < 1)
+            {
+                throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+            }
+
+            // get the total records which will help to compute the number of pages
             var totalItemCount = queryable.Count();
 
-
+            // apply order if passed
             if (order != null)
             {
                 queryable = order(queryable);
             }
 
+            // compute the number of items to be skipped
             var skipCount = pageNumber > 0 ? (pageNumber - 1) * pageSize : 0;
 
             IEnumerable<T> items = null;
 
+            // get paged result
             if (totalItemCount > 0)
             {
                 items = queryable.Skip(skipCount).Take(pageSize).AsEnumerable();
             }
 
+            // return paged list
             return new PagedList<T>(pageNumber, pageSize, totalItemCount, items);
         }
 
+        /// <summary>
+        /// Async Extension method to return a paged results
+        /// </summary>
+        /// <typeparam name="T">Document Type</typeparam>
+        /// <param name="queryable">Extension point on <see cref="IQueryable{T}"/></param>
+        /// <param name="pageNumber">Page number</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="order">Order clause</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>return paged result</returns>
         public static async Task<PagedList<T>> PagedQueryAsync<T>(
-            this IQueryable<T> queryable,
-            int pageNumber,
-            int pageSize,
-            Func<IQueryable<T>, IQueryable<T>> order = null,
+            this IQueryable<T> queryable, 
+            int pageNumber, 
+            int pageSize, 
+            Func<IQueryable<T>, IQueryable<T>> order = null, 
             CancellationToken token = default(CancellationToken))
         {
+            // throw an argument exception if page number is less than one
             if (pageNumber < 1)
+            {
                 throw new ArgumentOutOfRangeException($"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
+            }
+
+            // throw argument exception if page size is less than one
             if (pageSize < 1)
+            {
                 throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+            }
 
-            int totalItemCount = await queryable.CountAsync().ConfigureAwait(false);
+            // get the total records which will help to compute the number of pages    
+            var totalItemCount = await queryable.CountAsync(token).ConfigureAwait(false);
 
+            // apply order if passed
             if (order != null)
             {
                 queryable = order(queryable);
             }
 
+            // compute the number of items to be skipped
             var skipCount = pageNumber > 0 ? (pageNumber - 1) * pageSize : 0;
 
             IEnumerable<T> items = null;
 
+            // get paged result
             if (totalItemCount > 0)
             {
                 items = await queryable.Skip(skipCount).Take(pageSize).As<IMartenQueryable>().ToListAsync<T>(token);
             }
 
+            // return paged list
             return new PagedList<T>(pageNumber, pageSize, totalItemCount, items);
         }
     }
