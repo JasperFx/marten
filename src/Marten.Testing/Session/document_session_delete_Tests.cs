a085eb4f388b247246330f869fb8a88b13d61649
Eric Green <edgeric@gmail.com>
2017-01-11 11:33:36 -0600

    failing test for #654

---
+++ src/Marten.Testing/Session/document_session_delete_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Marten.Services;
+using Marten.Testing.Documents;
+using Xunit;
+
+namespace Marten.Testing.Session
+{
+    public class document_session_delete_Tests : DocumentSessionFixture<IdentityMap>
+    {
+        [Fact]
+        public void upsert_then_delete_should_delete()
+        {
+            var issue = new Issue { Id = Guid.NewGuid() };
+
+            theSession.Store(issue);
+            theSession.Delete<Issue>(issue.Id);
+            theSession.SaveChanges();
+
+            var loadedIssue = theSession.Load<Issue>(issue.Id);
+            loadedIssue.ShouldBeNull();
+        }
+    }
+}
