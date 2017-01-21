cad8a1520be65371460a0c89472218d78f01860e
James Hopper <jimgolfgti@gmail.com>
2017-01-21 22:47:32 +0000

    Adds DeletedSince + DeletedBefore extensions

---
+++ src/Marten.Testing/Linq/SoftDeletes/DeletedSinceParserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Marten.Linq.SoftDeletes;
+using Marten.Schema;
+using Marten.Services;
+using Npgsql;
+using Xunit;
+
+namespace Marten.Testing.Linq.SoftDeletes
+{
+    public class DeletedSinceParserTests
+    {
+        private readonly DocumentMapping _mapping;
+        private readonly MethodCallExpression _expression;
+        private readonly DeletedSinceParser _parser;
+
+        public DeletedSinceParserTests()
+        {
+            _mapping = new DocumentMapping(typeof(object), new StoreOptions()) {DeleteStyle = DeleteStyle.SoftDelete};
+            _expression = Expression.Call(typeof(SoftDeletedExtensions).GetMethod(nameof(SoftDeletedExtensions.DeletedSince)),
+                Expression.Parameter(typeof(object)),
+                Expression.Constant(DateTimeOffset.UtcNow));
+            _parser = new DeletedSinceParser();
+        }
+
+        [Fact]
+        public void WhereFragmentContainsExpectedExpression()
+        {
+            var result = _parser.Parse(_mapping, new JsonNetSerializer(), _expression);
+
+            result.ToSql(new NpgsqlCommand()).ShouldContain("d.mt_deleted and d.mt_deleted_at >");
+        }
+
+        [Fact]
+        public void ThrowsIfDocumentMappingNotSoftDeleted()
+        {
+            _mapping.DeleteStyle = DeleteStyle.Remove;
+
+            Exception<NotSupportedException>.ShouldBeThrownBy(() => _parser.Parse(_mapping, new JsonNetSerializer(), _expression));
+        }
+    }
+}
--- src/Marten/Linq/SoftDeletes/SoftDeletedExtensions.cs
+++ src/Marten/Linq/SoftDeletes/SoftDeletedExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.CompilerServices;
+
 namespace Marten.Linq.SoftDeletes
 {
     public static class SoftDeletedExtensions
@@ -22,6 +25,28 @@ namespace Marten.Linq.SoftDeletes
         /// <param name="doc"></param>
         /// <returns></returns>
         public static bool IsDeleted(this object doc)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// The search results should include documents deleted since given time (&gt;)
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static bool DeletedSince(this object doc, DateTimeOffset time)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// The search results should include documents deleted before given time (&lt;)
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static bool DeletedBefore(this object doc, DateTimeOffset time)
         {
             return true;
         }
