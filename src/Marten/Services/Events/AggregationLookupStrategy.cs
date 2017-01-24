eb3f7dcf54d8f8e07c8680709d6c15b1c5c920e8
migajek <migajek@gmail.com>
2017-01-24 10:13:07 +0100

    refactored AggregationLookupStrategy to reduce magic factor and clean-up the code

--- src/Marten/Services/Events/AggregationLookupStrategy.cs
+++ src/Marten/Services/Events/AggregationLookupStrategy.cs
@@ -1,37 +1,14 @@
-﻿namespace Marten.Services.Events
+﻿using Baseline;
+using Marten.Events.Projections;
+
+namespace Marten.Services.Events
 {
-    public sealed class AggregationLookupStrategy
+    public static class AggregationLookupStrategy
     {
-        public readonly short Value;
+        public static IAggregatorLookup UsePublicApply
+            => new AggregatorLookup(type => typeof(Aggregator<>).CloseAndBuildAs<IAggregator>(type));
 
-        private bool Equals(AggregationLookupStrategy other)
-        {
-            return Value == other.Value;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            return obj is AggregationLookupStrategy && Equals((AggregationLookupStrategy) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
-        }
-
-        public static readonly AggregationLookupStrategy UsePublicApply = new AggregationLookupStrategy(0);
-        public static readonly AggregationLookupStrategy UsePrivateApply = new AggregationLookupStrategy(1);
-
-        public static implicit operator short(AggregationLookupStrategy item)
-        {
-            return item.Value;
-        }
-
-        private AggregationLookupStrategy(short value)
-        {
-            Value = value;
-        }
+        public static IAggregatorLookup UsePrivateApply
+            => new AggregatorLookup(type => typeof(AggregatorApplyPrivate<>).CloseAndBuildAs<IAggregator>(type));
     }
 }
