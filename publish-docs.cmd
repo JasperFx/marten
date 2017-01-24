402048de1a9c2a5fc1504e203f22cbe8a41482d6
mysticmind <babu.annamalai@gmail.com>
2017-01-24 11:09:04 +0530

    Includes a copy step for missing embed.js from ST tools to documentation\content

--- publish-docs.cmd
+++ publish-docs.cmd
@@ -1,3 +1,5 @@
+copy packages\storyteller\tools\embed.js documentation\content /Y
+
 packages\storyteller\tools\ST.exe doc-export c:\code\marten-docs ProjectWebsite --version 1.0 --project marten
 
 cd \code\marten-docs
