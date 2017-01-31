fcdc9ae5225878c3c7196b90853ef5e37cd2e20f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-01-31 06:34:00 -0600

    first cut of using StringBuilder's internally

--- src/Marten/Linq/IWhereFragment.cs
+++ src/Marten/Linq/IWhereFragment.cs
@@ -4,6 +4,7 @@ namespace Marten.Linq
 {
     public interface IWhereFragment
     {
+        // TODO -- have this take in a StringBuilder? Have to be in 2.0
         string ToSql(NpgsqlCommand command);
         bool Contains(string sqlText);
     }
