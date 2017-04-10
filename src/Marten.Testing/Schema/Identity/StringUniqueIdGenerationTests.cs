62a09674918b6056d25c2ca761f0bb87fdf40b3f
Marco Dissel <marco.dissel@gmail.com>
2017-04-10 20:11:00 +0200

    Added string unique ID Generator

---
+++ src/Marten.Testing/Schema/Identity/StringUniqueIdGenerationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Marten.Schema.Identity;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Schema.Identity
+{
+    public class StringUniqueIdGeneratorTests
+    {
+        [Fact]
+        public void do_nothing_with_an_existing_value()
+        {
+			string existing = "someValue";
+
+            var generator = new StringUniqueIdGeneration();
+
+            bool assigned = true;
+
+            generator.Assign(existing, out assigned).ShouldBe(existing);
+
+            assigned.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void assign_a_new_value_with_an_empty()
+        {
+			string existing = string.Empty;
+            var generator = new StringUniqueIdGeneration();
+            bool assigned = false;
+            generator.Assign(existing, out assigned)
+                .ShouldNotBeNullOrEmpty();
+
+            assigned.ShouldBeTrue();
+        }
+
+		[Fact]
+		public void assign_a_new_value_with_an_null() {
+			string existing = null;
+			var generator = new StringUniqueIdGeneration();
+			bool assigned = false;
+			generator.Assign(existing, out assigned)
+					.ShouldNotBeNullOrEmpty();
+
+			assigned.ShouldBeTrue();
+		}
+	}
+}
---
+++ src/Marten/Schema/Identity/StringUniqueIdGeneration.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Baseline;
+
+namespace Marten.Schema.Identity
+{
+    /// <summary>
+    /// StringUniqueIdGeneration Generation. More info https://firebase.googleblog.com/2015/02/the-2120-ways-to-ensure-unique_68.html
+		/// Copied from Gist https://gist.github.com/kiliman/ca1d9f4135078a6b24c5005113bbeea4
+    /// </summary>
+    public class StringUniqueIdGeneration : IIdGeneration, IIdGenerator<string>
+    {
+        public IEnumerable<Type> KeyTypes { get; } = new[] {typeof(string)};
+
+
+        public IIdGenerator<T> Build<T>(IDocumentSchema schema)
+        {
+            return this.As<IIdGenerator<T>>();
+        }
+
+        public string Assign(string existing, out bool assigned)
+        {
+            if (existing.IsEmpty())
+            {
+                assigned = true;
+								return FirebasePushIDGenerator.GeneratePushID();
+            }
+            assigned = false;
+            return existing;
+        }
+    }
+
+public class FirebasePushIDGenerator
+{
+    /**
+     * Fancy ID generator that creates 20-character string identifiers with the following properties:
+     *
+     * 1. They're based on timestamp so that they sort *after* any existing ids.
+     * 2. They contain 72-bits of random data after the timestamp so that IDs won't collide with other clients' IDs.
+     * 3. They sort *lexicographically* (so the timestamp is converted to characters that will sort properly).
+     * 4. They're monotonically increasing.  Even if you generate more than one in the same timestamp, the
+     *    latter ones will sort after the former ones.  We do this by using the previous random bits
+     *    but "incrementing" them by 1 (only in the case of a timestamp collision).
+     */
+    // Modeled after base64 web-safe chars, but ordered by ASCII.
+    const string PUSH_CHARS = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+
+    // Timestamp of last push, used to prevent local collisions if you push twice in one ms.
+    private static long lastPushTime = 0;
+
+    // We generate 72-bits of randomness which get turned into 12 characters and appended to the
+    // timestamp to prevent collisions with other clients.  We store the last characters we
+    // generated because in the event of a collision, we'll use those same characters except
+    // "incremented" by one.
+    private static char[] lastRandChars = new char[12];
+
+    // Random number generator
+    private static Random rng = new Random();
+    
+    public static string GeneratePushID()
+    {
+        var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        Console.WriteLine("now=" + now);
+
+        var duplicateTime = (now == lastPushTime);
+        lastPushTime = now;
+
+        var timeStampChars = new char[8];
+        for (var i = 7; i >= 0; i--)
+        {
+            timeStampChars[i] = PUSH_CHARS[(int)(now % 64)];
+            now = now >> 6;
+        }
+        if (now != 0) throw new Exception("We should have converted the entire timestamp.");
+
+        var id = string.Join(string.Empty, timeStampChars);
+
+        if (!duplicateTime)
+        {
+            for (var i = 0; i < 12; i++)
+            {
+                lastRandChars[i] = (char)rng.Next(0, 63);
+            }
+        }
+        else
+        {
+            // If the timestamp hasn't changed since last push, use the same random number, except incremented by 1.
+            for (var i = 11; i >= 0 && lastRandChars[i] == 63; i--)
+            {
+                lastRandChars[i] = (char)0;
+            }
+            lastRandChars[11]++;
+        }
+        for (var i = 0; i < 12; i++)
+        {
+            id += PUSH_CHARS[lastRandChars[i]];
+        }
+        if (id.Length != 20) throw new Exception("Length should be 20.");
+
+        return id;
+    }
+
+    public static long ConvertPushID(string id)
+    {
+        var timestamp = 0L;
+        for (var i = 0; i < 8; i++)
+        {
+            var n = PUSH_CHARS.IndexOf(id[i]);
+            timestamp = (timestamp << 6) + n;
+        }
+        return timestamp;
+    }
+}
+}
