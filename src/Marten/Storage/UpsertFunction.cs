3e5cafb4165330b2fab6afec30a9c560583dd13d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-04-12 09:54:10 -0500

    pulled a superclass out of UpsertFunction for the other pieces

--- src/Marten/Storage/UpsertFunction.cs
+++ src/Marten/Storage/UpsertFunction.cs
@@ -1,28 +1,25 @@
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.IO;
 using System.Linq;
 using Baseline;
 using Marten.Schema;
 using Marten.Schema.Arguments;
 using Marten.Util;
-using Npgsql;
 
 namespace Marten.Storage
 {
-    public class UpsertFunction : ISchemaObject
+    public class UpsertFunction : Function
     {
         private readonly string _primaryKeyConstraintName;
         private readonly DbObjectName _tableName;
 
         public readonly IList<UpsertArgument> Arguments = new List<UpsertArgument>();
 
-        public UpsertFunction(DocumentMapping mapping)
+        public UpsertFunction(DocumentMapping mapping) : base(mapping.UpsertFunction)
         {
             if (mapping == null) throw new ArgumentNullException(nameof(mapping));
 
-            Identifier = mapping.UpsertFunction;
             _tableName = mapping.Table;
             _primaryKeyConstraintName = "pk_" + mapping.Table.Name;
                 
@@ -57,7 +54,7 @@ namespace Marten.Storage
 
         }
 
-        public void Write(DdlRules rules, StringWriter writer)
+        public override void Write(DdlRules rules, StringWriter writer)
         {
             var ordered = OrderedArguments();
 
@@ -105,115 +102,11 @@ $function$;
         }
 
 
-        public FunctionBody ToBody(DdlRules rules)
-        {
-            var dropSql = toDropSql();
-
-            var writer = new StringWriter();
-            Write(rules, writer);
-
-            return new FunctionBody(Identifier, new string[] {dropSql}, writer.ToString());
-        }
-
-        private string toDropSql()
+        protected override string toDropSql()
         {
             var argList = OrderedArguments().Select(x => x.PostgresType).Join(", ");
             var dropSql = $"drop function if exists {Identifier.QualifiedName}({argList});";
             return dropSql;
-        }
-
-        public void WriteDropStatement(DdlRules rules, StringWriter writer)
-        {
-            var dropSql = toDropSql();
-            writer.WriteLine(dropSql);
-        }
-
-        public DbObjectName Identifier { get; }
-
-        public void ConfigureQueryCommand(CommandBuilder builder)
-        {
-            var schemaParam = builder.AddParameter(Identifier.Schema).ParameterName;
-            var nameParam = builder.AddParameter(Identifier.Name).ParameterName;
-
-            builder.Append($@"
-SELECT pg_get_functiondef(pg_proc.oid) 
-FROM pg_proc JOIN pg_namespace as ns ON pg_proc.pronamespace = ns.oid WHERE ns.nspname = :{schemaParam} and proname = :{nameParam};
-
-SELECT format('DROP FUNCTION %s.%s(%s);'
-             ,n.nspname
-             ,p.proname
-             ,pg_get_function_identity_arguments(p.oid))
-FROM   pg_proc p
-LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace 
-WHERE  p.proname = :{nameParam}
-AND    n.nspname = :{schemaParam};
-");
-        }
-
-        private FunctionDelta fetchDelta(DbDataReader reader, DdlRules rules)
-        {
-            if (!reader.Read()) return null;
-
-            var upsertDefinition = reader.GetString(0);
-
-            reader.NextResult();
-            var drops = new List<string>();
-            while (reader.Read())
-            {
-                drops.Add(reader.GetString(0));
-            }
-
-            if (upsertDefinition == null) return null;
-
-            var actualBody = new FunctionBody(Identifier, drops.ToArray(), upsertDefinition);
-
-            var expectedBody = ToBody(rules);
-
-            return new FunctionDelta(expectedBody, actualBody);
-        }
-
-        public FunctionDelta FetchDelta(NpgsqlConnection conn, DdlRules rules)
-        {
-            var cmd = conn.CreateCommand();
-            var builder = new CommandBuilder(cmd);
-
-            ConfigureQueryCommand(builder);
-
-            cmd.CommandText = builder.ToString();
-
-            using (var reader = cmd.ExecuteReader())
-            {
-                return fetchDelta(reader, rules);
-            }
-        }
-
-        public SchemaPatchDifference CreatePatch(DbDataReader reader, SchemaPatch patch, AutoCreate autoCreate)
-        {
-            var diff = fetchDelta(reader, patch.Rules);
-            if (diff == null)
-            {
-                Write(patch.Rules, patch.UpWriter);
-                WriteDropStatement(patch.Rules, patch.DownWriter);
-
-                return SchemaPatchDifference.Create;
-            }
-
-            if (diff.AllNew)
-            {
-                Write(patch.Rules, patch.UpWriter);
-                WriteDropStatement(patch.Rules, patch.DownWriter);
-
-                return SchemaPatchDifference.Create;
-            }
-
-            if (diff.HasChanged)
-            {
-                diff.WritePatch(patch);
-
-                return SchemaPatchDifference.Update;
-            }
-
-            return SchemaPatchDifference.None;
         }
     }
 }
