9c38dbce3419be7b8f34ea8ded4b8455cd343352
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-04-20 09:42:27 -0500

    checkpoint: fixed a test about creating fk's

--- src/Marten.Testing/Bugs/Bug_553_ForeignKey_attribute_is_not_creating_an_index.cs
+++ src/Marten.Testing/Bugs/Bug_553_ForeignKey_attribute_is_not_creating_an_index.cs
@@ -1,32 +1,21 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
 using Marten.Schema;
 using Marten.Testing.Documents;
-using Shouldly;
 using Xunit;
 
 namespace Marten.Testing.Bugs
 {
     public class Bug_553_ForeignKey_attribute_is_not_creating_an_index : IntegratedFixture
     {
-
         [Fact]
         public void should_create_an_index_for_the_fk()
         {
             theStore.DefaultTenant.EnsureStorageExists(typeof(DocWithFK));
 
-            var mapping = theStore.DefaultTenant.MappingFor(typeof(DocWithFK));
-            throw new NotImplementedException("Need another way to do this");
-//            var objects = theStore.Schema.DbObjects.FindSchemaObjects((DocumentMapping) mapping);
-//
-//            objects.ActualIndices.Keys.Single()
-//                .ShouldBe("mt_doc_docwithfk_idx_user_id");
 
-
+            var table = theStore.Schema.DbObjects.ExistingTableFor(typeof(DocWithFK));
+            table.ActualIndices.ContainsKey("mt_doc_docwithfk_idx_user_id");
         }
-
-
     }
 
     public class DocWithFK
