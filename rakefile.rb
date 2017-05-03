edb3d0125dd95473682280de5851caeb1b59d427
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-05-03 08:48:56 -0500

    bumping to 1.5.1

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ require 'json'
 
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
-BUILD_VERSION = '1.5.0';
+BUILD_VERSION = '1.5.1'
 CONNECTION = ENV['connection']
 
 tc_build_number = ENV["BUILD_NUMBER"]
