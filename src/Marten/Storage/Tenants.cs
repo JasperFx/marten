8e93bdba32b4ab21866c09daad2293ee5d116295
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-05-04 11:44:16 -0500

    little bit of spiking on tenants

---
+++ src/Marten/Storage/Tenants.cs
@@ -0,0 +1,33 @@
+﻿namespace Marten.Storage
+{
+    public class Tenants
+    {
+        public const string Default = "*DEFAULT*";
+    }
+
+    public interface ITenantStrategy
+    {
+        string[] AllKnownTenants();
+        ITenant Create(string tenantId, AutoCreate autoCreate);
+    }
+
+    public class SingleTenant : ITenantStrategy
+    {
+        private readonly string _connectionString;
+
+        public SingleTenant(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public string[] AllKnownTenants()
+        {
+            return new[] {Tenants.Default};
+        }
+
+        public ITenant Create(string tenantId, AutoCreate autoCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
