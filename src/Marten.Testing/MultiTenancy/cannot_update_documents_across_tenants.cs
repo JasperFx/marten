433c9abe6a2a00b555e7c129253c60a07c404d3d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-05-19 15:27:49 -0500

    proving that you can't update other tenant's data

---
+++ src/Marten.Testing/MultiTenancy/cannot_update_documents_across_tenants.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Marten.Testing.Examples;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.MultiTenancy
+{
+    public class cannot_update_documents_across_tenants : IntegratedFixture
+    {
+        [Fact]
+        public void will_not_cross_the_streams()
+        {
+            StoreOptions(_ =>
+            {
+                _.Connection(ConnectionSource.ConnectionString)
+                    .MultiTenanted();
+            });
+
+            var user = new User {UserName = "Me"};
+            user.Id = Guid.NewGuid();
+
+            using (var red = theStore.OpenSession("Red"))
+            {
+                red.Store(user);
+                red.SaveChanges();
+            }
+
+            using (var green = theStore.OpenSession("Green"))
+            {
+                var greenUser = new User
+                {
+                    UserName = "You",
+                    Id = user.Id
+                };
+
+                // Nothing should happen here
+                green.Store(greenUser);
+                green.SaveChanges();
+            }
+
+            // Still got the original data
+            using (var query = theStore.QuerySession("Red"))
+            {
+                query.Load<User>(user.Id).UserName.ShouldBe("Me");
+            }
+        }
+    }
+}
