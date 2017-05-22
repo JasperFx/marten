b2a33ea8af0677016bbb31a08897a4079f8af054
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-05-21 19:21:52 -0500

    finished w/ converting the basic build script to VS2017

--- src/Marten.Storyteller/Program.cs
+++ src/Marten.Storyteller/Program.cs
@@ -20,7 +20,7 @@ namespace Marten.Storyteller
             {
                 //var results = runner.Run("Multi Tenancy / Deleting by Id");
                 //var results = runner.Run("Multi Tenancy / Loading Documents by Id");
-                var results = runner.Run("Multi Tenancy / Querying via Compiled Queries");
+                var results = runner.Run("Event Store / Async Daemon / Rebuild Projection");
                 Console.WriteLine(results.Counts);
 
                 //results = runner.Run("Event Store / Async Daemon / Rebuild Projection");
