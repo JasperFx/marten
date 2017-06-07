32f0408e62ddb1741dbbcd6e89d3782d217184ae
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-06-07 16:28:28 -0500

    Linq support for EqualsIgnoreCase(). Closes GH-748

---
+++ src/Marten.Testing/Linq/EqualsIgnoreCase_filtering.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Baseline;
+using Marten.Testing.Documents;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Linq
+{
+    public class EqualsIgnoreCase_filtering : IntegratedFixture
+    {
+        [Fact]
+        public void can_search_case_insensitive()
+        {
+            var user1 = new User{UserName = "Abc"};
+            var user2 = new User{UserName = "DeF"};
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(user1, user2);
+                session.SaveChanges();
+            }
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Query<User>().Single(x => x.UserName.EqualsIgnoreCase("abc")).Id.ShouldBe(user1.Id);
+                query.Query<User>().Single(x => x.UserName.EqualsIgnoreCase("aBc")).Id.ShouldBe(user1.Id);
+                query.Query<User>().Single(x => x.UserName.EqualsIgnoreCase("def")).Id.ShouldBe(user2.Id);
+
+                query.Query<User>().Any(x => x.UserName.EqualsIgnoreCase("abcd")).ShouldBeFalse();
+            }
+        }
+    }
+}
