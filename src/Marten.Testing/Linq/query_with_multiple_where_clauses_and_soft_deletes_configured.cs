488e26511b4d69541a8d8e3af9e8cd81009fb73e
Jeff Doolittle <jeffdoolittle@gmail.com>
2017-08-10 07:18:57 -0700

    failing test demonstrating GH-844

---
+++ src/Marten.Testing/Linq/query_with_multiple_where_clauses_and_soft_deletes_configured.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marten.Testing.Linq
+{
+    public class query_with_multiple_where_clauses_and_soft_deletes_configured : IntegratedFixture
+    {
+        [Fact]
+        public async Task TestMultipleWhereClausesWithSoftDeleteConfigured()
+        {
+            StoreOptions(_ => _.Schema.For<SoftDeletedItem>().SoftDeleted());
+
+            var item1 = new SoftDeletedItem { Number = 1, Name = "Jim Bob" };
+            var item2 = new SoftDeletedItem { Number = 2, Name = "Joe Bill" };
+            var item3 = new SoftDeletedItem { Number = 1, Name = "Jim Beam" };
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(item1, item2, item3);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.QuerySession())
+            {
+                var query = session.Query<SoftDeletedItem>()
+                    .Where(x => x.Number == 1)
+                    .Where(x => x.Name.StartsWith("Jim"));
+
+                var result = await query.ToListAsync();
+                Assert.Equal(2, result.Count);
+            }
+        }
+
+        [Fact]
+        public async Task TestMultipleWhereClausesWithoutSoftDeleteConfigured()
+        {
+            StoreOptions(_ => _.Schema.For<SoftDeletedItem>());
+
+            var item1 = new SoftDeletedItem { Number = 1, Name = "Jim Bob" };
+            var item2 = new SoftDeletedItem { Number = 2, Name = "Joe Bill" };
+            var item3 = new SoftDeletedItem { Number = 1, Name = "Jim Beam" };
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(item1, item2, item3);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.QuerySession())
+            {
+                var query = session.Query<SoftDeletedItem>()
+                    .Where(x => x.Number == 1)
+                    .Where(x => x.Name.StartsWith("Jim"));
+
+                var result = await query.ToListAsync();
+                Assert.Equal(2, result.Count);
+            }
+        }
+    }
+
+    public class SoftDeletedItem
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+    }
+
+}
