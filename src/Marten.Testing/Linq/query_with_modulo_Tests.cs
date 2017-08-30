4a9e83d74ea341babfedb5b72a9acb6bbc656280
James Hopper <jimgolfgti@gmail.com>
2017-08-30 11:26:46 +0100

    Fixes ordering of operands

--- src/Marten.Testing/Linq/query_with_modulo_Tests.cs
+++ src/Marten.Testing/Linq/query_with_modulo_Tests.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using Marten.Services;
 using Xunit;
 
@@ -20,10 +20,27 @@ namespace Marten.Testing.Linq
 
             theSession.SaveChanges();
 
-            theSession.Query<Target>().Where(x => x.Number % 2 == 0 && x.Color == Colors.Blue).ToArray()
+            theSession.Query<Target>().Where(x => x.Number % 2 == 0 && x.Color < Colors.Green).ToArray()
                 .Select(x => x.Number)
                 .ShouldHaveTheSameElementsAs(2, 4);
         }
         // ENDSAMPLE
+
+        [Fact]
+        public void use_modulo_operands_reversed()
+        {
+            theSession.Store(new Target { Color = Colors.Blue, Number = 1 });
+            theSession.Store(new Target { Color = Colors.Blue, Number = 2 });
+            theSession.Store(new Target { Color = Colors.Blue, Number = 3 });
+            theSession.Store(new Target { Color = Colors.Blue, Number = 4 });
+            theSession.Store(new Target { Color = Colors.Blue, Number = 5 });
+            theSession.Store(new Target { Color = Colors.Green, Number = 6 });
+
+            theSession.SaveChanges();
+
+            theSession.Query<Target>().Where(x => 0 == x.Number % 2 && Colors.Green > x.Color).ToArray()
+                .Select(x => x.Number)
+                .ShouldHaveTheSameElementsAs(2, 4);
+        }
     }
 }
