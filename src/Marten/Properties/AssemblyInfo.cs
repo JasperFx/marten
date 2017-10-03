404c95b9ee41bbcf92e1db18f356b0216c4af231
jeremydmiller <jeremydmiller@yahoo.com>
2017-10-02 21:46:07 -0500

    still fighting the build

--- src/Marten/Properties/AssemblyInfo.cs
+++ src/Marten/Properties/AssemblyInfo.cs
@@ -2,7 +2,4 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Marten")]
+
