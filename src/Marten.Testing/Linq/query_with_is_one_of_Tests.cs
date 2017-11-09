ea6047ddbda57d3fba2b9a69d561128e1b30c7e9
mysticmind <babu.annamalai@gmail.com>
2017-11-09 15:34:27 +0530

    Fix for IsOneOf with negation GH-898
    - Add logic to handle combination of not operator with IsOneOf
    - Add a unit test for ISOneOf with negation

--- src/Marten.Testing/Linq/query_with_is_one_of_Tests.cs
+++ src/Marten.Testing/Linq/query_with_is_one_of_Tests.cs
@@ -33,6 +33,27 @@ namespace Marten.Testing.Linq
                 .ShouldHaveTheSameElementsAs(expected);
         }
 
+        [Fact]
+        public void can_query_against_integers_with_not_operator()
+        {
+            var targets = Target.GenerateRandomData(100).ToArray();
+            theStore.BulkInsert(targets);
+
+            var validNumbers = targets.Select(x => x.Number).Distinct().Take(3).ToArray();
+
+            var found = theSession.Query<Target>().Where(x => !x.Number.IsOneOf(validNumbers)).ToArray();
+
+            var expected = targets
+                .Where(x => !validNumbers
+                .Contains(x.Number))
+                .OrderBy(x => x.Id)
+                .Select(x => x.Id)
+                .ToArray();
+
+            found.OrderBy(x => x.Id).Select(x => x.Id)
+                .ShouldHaveTheSameElementsAs(expected);
+        }
+
         public void is_one_of_example()
         {
             // SAMPLE: is_one_of
--- src/Marten/Linq/WhereClauseVisitor.cs
+++ src/Marten/Linq/WhereClauseVisitor.cs
@@ -85,7 +85,7 @@ namespace Marten.Linq
                 switch (node.NodeType)
                 {
                     case ExpressionType.Not:
-                        if (node.Operand is SubQueryExpression)
+                        if (node.Operand is SubQueryExpression || node.Operand is MethodCallExpression)
                         {
                             var nested = new WhereClauseVisitor(_parent, _mapping);
                             nested.Visit(node.Operand);
