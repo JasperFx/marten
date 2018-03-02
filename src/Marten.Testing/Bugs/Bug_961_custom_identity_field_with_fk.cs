9eb2b7e4c50a4c0c1da2fcb68ed6ae49bda565a0
jeremydmiller <jeremydmiller@yahoo.com>
2018-03-02 13:22:54 -0600

    using id field for all foreign keys. Closes GH-961

---
+++ src/Marten.Testing/Bugs/Bug_961_custom_identity_field_with_fk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_961_custom_identity_field_with_fk : IntegratedFixture
+    {
+        public class FkTarget
+        {
+            public Guid SomeId { get; set; }
+        }
+
+        public class Document
+        {
+            public int Id { get; set; }
+            public Guid TargetId { get; set; }
+        }
+
+        [Fact]
+        public void can_build_the_fk_correctly()
+        {
+            StoreOptions(_ =>
+            {
+                _.Schema.For<FkTarget>().Identity(x => x.SomeId);
+
+                _.Schema.For<Document>().ForeignKey<FkTarget>(a => a.TargetId);
+            });
+
+            theStore.Schema.ApplyAllConfiguredChangesToDatabase();
+        }
+    }
+}
