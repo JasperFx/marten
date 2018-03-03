7dde17fef71e9c36ac4a0f1598403571944a12d1
cocowalla <800977+cocowalla@users.noreply.github.com>
2018-03-03 22:35:09 +0000

    FIx for #980: allow BulkInsert for sub-classes

---
+++ src/Marten.Testing/Bugs/Bug_980_bulk_insert_with_subclass.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Shouldly;
+using Xunit;
+using Marten.Testing.Documents;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_980_bulk_insert_with_subclass : IntegratedFixture
+    {
+        [Fact]
+        public void can_do_a_bulk_insert()
+        {
+            StoreOptions(_ =>
+            {
+                _.Schema.For<User>()
+                    .AddSubClass<AdminUser>()
+                    .AddSubClass<SuperUser>();
+            });
+
+            var users = new List<User>
+            {
+                new AdminUser { UserName = "foo" },
+                new SuperUser { UserName = "bar" },
+                new SuperUser { UserName = "myergen" }
+            };
+
+            Should.NotThrow(() => theStore.BulkInsertDocuments(users));
+
+            using (var query = theStore.LightweightSession())
+            {
+                query.Query<AdminUser>().Count().ShouldBe(1);
+                query.Query<SuperUser>().Count().ShouldBe(2);
+            }
+        }
+    }
+}
