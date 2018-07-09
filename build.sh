9bb00166022988ee26c23a08f09fde02c653369f
mysticmind <babu.annamalai@gmail.com>
2018-07-09 10:37:15 +0530

    Update travis ci build file to remove the restore step targetting netstandard1.3

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ev
 
-dotnet restore ./src/Marten.sln --runtime netstandard1.3
 dotnet build ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp2.0 --configuration Release
 npm run test
 dotnet test ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp2.0 --configuration Release 
