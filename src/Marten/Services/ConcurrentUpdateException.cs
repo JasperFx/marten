6ddab5650f1f87f64c8674e39d15d91a3ca4dcf4
Michael Wolfenden <michael-wolfenden@users.noreply.github.com>
2018-07-20 17:45:33 +1000

    Fix spelling mistake, 'collission' => 'collision'

--- src/Marten/Services/ConcurrentUpdateException.cs
+++ src/Marten/Services/ConcurrentUpdateException.cs
@@ -4,7 +4,7 @@ namespace Marten.Services
 {
     public class ConcurrentUpdateException : Exception
     {
-        public ConcurrentUpdateException(Exception innerException) : base("Write collission detected while commiting the transaction.", innerException)
+        public ConcurrentUpdateException(Exception innerException) : base("Write collision detected while commiting the transaction.", innerException)
         {
         }
     }
