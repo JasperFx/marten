419675e81a14d49a68183e488910d32ed4803622
mysticmind <babu.annamalai@gmail.com>
2018-09-12 00:12:27 +0530

    Refactor and simplify the PagedList implementation
    - Refactor and simplify the PagedList implementation to use
    QueryStatistics to use single db query GH-930
    - BREAKING CHANGE: AsPagedList and AsPagedListAsync IQueryable<T> is
    changed to IMartenQueryable
    - BREAKING CHANGE: some of the properties in IPagedList<T> has change been
    changed from int to long
    - Update unit tests

--- src/Marten/Pagination/PagedListQueryableExtensions.cs
+++ src/Marten/Pagination/PagedListQueryableExtensions.cs
@@ -1,11 +1,12 @@
-﻿using System.Linq;
+﻿using Marten.Linq;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Marten.Pagination
 {
     /// <summary>
-    /// Extension methods on <see cref="IQueryable{T}"/> for performing paged queries
+    /// Extension methods on <see cref="IMartenQueryable{T}"/> for performing paged queries
     /// </summary>
     public static class PagedListQueryableExtensions
     {
@@ -13,36 +14,36 @@ namespace Marten.Pagination
         /// Extension method to return a paged results
         /// </summary>
         /// <typeparam name="T">Document Type</typeparam>
-        /// <param name="queryable">Extension point on <see cref="IQueryable{T}"/></param>
+        /// <param name="queryable">Extension point on <see cref="IMartenQueryable{T}"/></param>
         /// <param name="pageNumber">one based page number</param>
         /// <param name="pageSize">Page size</param>
         /// <returns>return paged result</returns>
         public static IPagedList<T> AsPagedList<T>(
-            this IQueryable<T> queryable, 
+            this IMartenQueryable<T> queryable, 
             int pageNumber, 
             int pageSize)
         {
             // return paged list
-            return new PagedList<T>(queryable, pageNumber, pageSize);
+            return PagedList<T>.CreateAsync(queryable, pageNumber, pageSize).GetAwaiter().GetResult();
         }
 
         /// <summary>
         /// Async Extension method to return a paged results
         /// </summary>
         /// <typeparam name="T">Document Type</typeparam>
-        /// <param name="queryable">Extension point on <see cref="IQueryable{T}"/></param>
+        /// <param name="queryable">Extension point on <see cref="IMartenQueryable{T}"/></param>
         /// <param name="pageNumber">One based page number</param>
         /// <param name="pageSize">Page size</param>
         /// <param name="token">Cancellation token</param>
         /// <returns>return paged result</returns>
         public static async Task<IPagedList<T>> AsPagedListAsync<T>(
-            this IQueryable<T> queryable, 
+            this IMartenQueryable<T> queryable, 
             int pageNumber, 
             int pageSize, 
             CancellationToken token = default(CancellationToken))
         {
             // return paged list
-            return await AsyncPagedList<T>.CreateAsync(queryable, pageNumber, pageSize).ConfigureAwait(false);
+            return await PagedList<T>.CreateAsync(queryable, pageNumber, pageSize).ConfigureAwait(false);
         }
     }
 }
