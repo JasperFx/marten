ba6aa5d042b48813421f2e88092f69764a77dcb4
mysticmind <babu.annamalai@gmail.com>
2018-09-14 17:30:40 +0530

    Fix TravisCI build file to update framework to netcoreapp2.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
 
-dotnet build ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp2.0 --configuration Release
+dotnet build ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp2.1 --configuration Release
 npm run test
-dotnet test ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp2.0 --configuration Release 
+dotnet test ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp2.1 --configuration Release
