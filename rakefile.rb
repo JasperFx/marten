556d5e4b2a05c3762ffa979fc2ff66ebc531c6c8
mysticmind <babu.annamalai@gmail.com>
2018-09-26 22:10:49 +0530

    Add Bullseye based .NET Core build tool
    - Add Bullseye based .NET Core build utility project
    - Move docs.csproj to tools\stdocs
    - Update StoryTeller to use v5.2.0
    - Update rake tasks for stdocs to point to the new path for docs.csproj
    - Update rake tasks for storyteller and open_st to use StoryTeller
    v5.2.0
    - Add helper scripts build.cmd, build.ps1 and build.sh to use the
    new .Net Core based build tool

--- rakefile.rb
+++ rakefile.rb
@@ -82,21 +82,23 @@ end
 desc "Run the storyteller specifications"
 task :storyteller => [:compile] do
 	Dir.chdir("src/Marten.Storyteller") do
-	  system "dotnet storyteller run -r artifacts --culture en-US"
+	  syetem "dotnet run --framework netcoreapp2.1 --culture en-US"
 	end
 end
 
 desc "Run the storyteller specifications"
 task :open_st => [:compile] do
 	Dir.chdir("src/Marten.Storyteller") do
-	  system "dotnet storyteller open --culture en-US"
+	  system "dotnet storyteller open --framework netcoreapp2.1"
 	end
 end
 
 "Launches the documentation project in editable mode"
 task :docs do
-	sh "dotnet restore"
-	sh "dotnet stdocs run -v #{BUILD_VERSION}"
+	Dir.chdir("tools/stdocs") do
+	  sh "dotnet restore"
+	  sh "dotnet stdocs run -d ../../documentation -v #{BUILD_VERSION}"
+	end
 end
 
 "Exports the documentation to jasperfx.github.io/marten - requires Git access to that repo though!"
