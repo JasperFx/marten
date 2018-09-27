032e15d256a3194e49e61677b211b4e8565af4b8
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-09-27 18:25:24 +0200

    Added missing assignment to EventStreamUnexpectedMaxEventIdException

--- src/Marten/Services/EventStreamUnexpectedMaxEventIdException.cs
+++ src/Marten/Services/EventStreamUnexpectedMaxEventIdException.cs
@@ -10,6 +10,8 @@ namespace Marten.Services
 
         public EventStreamUnexpectedMaxEventIdException(object id, Type aggregateType, int expected, int actual) : base($"Unexpected MAX(id) for event stream, expected {expected} but got {actual}")
         {
+            Id = id;
+            AggregateType = aggregateType;
         }
     }
 }
