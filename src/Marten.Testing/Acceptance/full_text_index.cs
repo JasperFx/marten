8f63b0a2e446d2c3be8a1ab6a3e0b8aa115a8a10
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-01-27 12:20:45 +0100

    Fixed failing test

--- src/Marten.Testing/Acceptance/full_text_index.cs
+++ src/Marten.Testing/Acceptance/full_text_index.cs
@@ -212,7 +212,7 @@ namespace Marten.Testing.Acceptance
 
                 var somebody = session.Query<BlogPost>()
                     .Where(blogPost => blogPost.Category == "Travel")
-                    .Search("Lorem ipsum")
+                    .Search("Lorem")
                     .ToList();
             }
 
