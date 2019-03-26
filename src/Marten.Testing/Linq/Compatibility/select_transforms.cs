a892220b7699de0ec36a95af8d51fb2430a3d9d5
jeremydmiller <jeremydmiller@yahoo.com>
2019-03-26 13:02:09 -0500

    Proof of concept for testing Select() transformations

---
+++ src/Marten.Testing/Linq/Compatibility/select_transforms.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Marten.Testing.Linq.Compatibility.Support;
+using Xunit;
+
+namespace Marten.Testing.Linq.Compatibility
+{
+    public class select_transforms : LinqTestContext<DefaultQueryFixture, select_transforms>
+    {
+        public select_transforms(DefaultQueryFixture fixture) : base(fixture)
+        {
+        }
+
+        static select_transforms()
+        {
+            selectInOrder(docs => docs.OrderBy(x => x.Id).Take(10).Select(x => new Person{Name = x.String, Number = x.Number}));
+        }
+        
+        [Theory]
+        [MemberData(nameof(GetDescriptions))]
+        public Task run_query(string description)
+        {
+            return assertTestCase(description, Fixture.Store);
+        }
+    }
+
+    public class Person
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+    }
+}
