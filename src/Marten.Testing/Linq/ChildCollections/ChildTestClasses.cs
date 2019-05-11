454af14e6ab40e5b92f5f06161ac1b1445b4fe5e
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-05-11 13:17:49 +0200

    Added tests for GH-1193

---
+++ src/Marten.Testing/Linq/ChildCollections/ChildTestClasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Marten.Testing.Linq.ChildCollections
+{
+    public class Root
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ChildLevel1> ChildsLevel1 { get; set; }
+    }
+
+    public class ChildLevel1
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ChildLevel2> ChildsLevel2 { get; set; }
+    }
+
+    public class ChildLevel2
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
