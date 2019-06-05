3c56ac6970f86a73284800ba9acfe332e1632efb
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-06-06 00:03:28 +0200

    Update BulkLoading.cs

--- src/MartenBenchmarks/BulkLoading.cs
+++ src/MartenBenchmarks/BulkLoading.cs
@@ -5,6 +5,7 @@ using Marten.Testing;
 namespace MartenBenchmarks
 {
     [SimpleJob(warmupCount: 2)]
+    [MemoryDiagnoser]
     public class BulkLoading
     {
         public static Target[] Docs = Target.GenerateRandomData(1000).ToArray();
