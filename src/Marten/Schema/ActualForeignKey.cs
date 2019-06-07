e480f0a2d711efc559bc2d9649f3f4f02af2744f
Mark Warpool <codinggorilla@outlook.com>
2019-06-07 15:55:21 -0500

    Adding logic to the Table and TableDelta to detect changes in foreign keys and apply the required changes and rollbacks

---
+++ src/Marten/Schema/ActualForeignKey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Marten.Schema
+{
+    public class ActualForeignKey
+    {
+        public DbObjectName Table { get; }
+        public string Name { get; }
+        public string DDL { get; }
+
+        public bool DoesCascadeDeletes()
+        {
+            // NOTE: Use .IndexOf() so it's not effected by whitespace
+            return DDL.IndexOf("on delete cascade", StringComparison.OrdinalIgnoreCase) != -1;
+        }
+
+        public ActualForeignKey(DbObjectName table, string name, string ddl)
+        {
+            Table = table;
+            Name = name;
+            DDL = ddl;
+        }
+    }
+}
