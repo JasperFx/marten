6ed7d835581c6ef2e66df29cbdc9b0e0fbf42ba4
jokokko <jokokko@gmail.com>
2019-06-20 13:10:58 +0300

    Adapt tests to removal of SkippableFact

--- src/Marten.Testing/Exceptions/known_exception_causes_dueto_pg9.cs
+++ src/Marten.Testing/Exceptions/known_exception_causes_dueto_pg9.cs
@@ -1,31 +1,16 @@
-using System;
 using System.Linq;
 using Marten.Exceptions;
-using Marten.Testing.Acceptance;
 using Marten.Testing.Documents;
+using Marten.Testing.Harness;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Marten.Testing.Exceptions
 {
     public class known_exception_causes_dueto_pg9: IntegratedFixture
     {
-        private readonly bool _hasRequiredMaximumPgVersion;
-        private readonly string _skipReason;
-
-        public known_exception_causes_dueto_pg9()
-        {
-            var requiredMaximumPgVersion = Version.Parse("10.0");
-            _hasRequiredMaximumPgVersion =
-                theStore.Diagnostics.GetPostgresVersion().CompareTo(requiredMaximumPgVersion) < 0;
-            _skipReason = $"Test skipped, maximum Postgres version required is {requiredMaximumPgVersion}";
-        }
-
-        [SkippableFact]
+        [PgVersionTargetedFact(MaximumVersion = "10.0")]
         public void can_map_jsonb_FTS_not_supported()
         {
-            Skip.IfNot(_hasRequiredMaximumPgVersion, _skipReason);
-
             var e = Assert.Throws<MartenCommandNotSupportedException>(() =>
             {
                 using (var session = theStore.OpenSession())
@@ -37,10 +22,10 @@ namespace Marten.Testing.Exceptions
             Assert.Contains(KnownExceptionCause.ToTsvectorOnJsonb.Description, e.Message);
         }
 
-        [SkippableFact]
+        [PgVersionTargetedFact(MaximumVersion = "10.0")]
         public void can_totsvector_other_than_jsonb_without_FTS_exception()
         {
-            var e = Assert.Throws<MartenCommandException>(() =>
+            Assert.Throws<MartenCommandException>(() =>
             {
                 using (var session = theStore.OpenSession())
                 {
