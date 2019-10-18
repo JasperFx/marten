f800fe74042ba70f4403f9424a27864be6dc5dc0
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-10-18 12:05:39 +0200

    Added tests for inline and custom projections with base classes

---
+++ src/Marten.Testing/Events/Projections/inline_aggregation_with_base_view_class.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Marten.Services;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Events.Projections
+{
+    public class inline_aggregation_with_base_view_class: DocumentSessionFixture<NulloIdentityMap>
+    {
+        private readonly MonsterSlayed slayed1 = new MonsterSlayed { Name = "Troll" };
+        private readonly MonsterSlayed slayed2 = new MonsterSlayed { Name = "Dragon" };
+        private readonly Guid streamId;
+
+        public inline_aggregation_with_base_view_class()
+        {
+            StoreOptions(_ =>
+            {
+                _.AutoCreateSchemaObjects = AutoCreate.All;
+                _.Events.InlineProjections.AggregateStreamsWith<QuestMonstersWithBaseClass>();
+                _.Events.InlineProjections.AggregateStreamsWith<QuestMonstersWithBaseClassAndIdOverloaded>();
+                _.Events.InlineProjections.AggregateStreamsWith<QuestMonstersWithBaseClassAndIdOverloadedWithNew>();
+            });
+
+            streamId = theSession.Events
+                .StartStream<QuestMonstersWithBaseClass>(slayed1, slayed2).Id;
+
+            theSession.SaveChanges();
+        }
+
+        [Fact]
+        public void run_inline_aggregation_with_base_class()
+        {
+            VerifyInlineProjection<QuestMonstersWithBaseClass>();
+        }
+
+        [Fact]
+        public void run_inline_aggregation_with_base_class_and_id_overloaded()
+        {
+            VerifyInlineProjection<QuestMonstersWithBaseClassAndIdOverloaded>();
+        }
+
+        [Fact]
+        public void run_inline_aggregation_with_base_class_and_id_overloaded_with_new()
+        {
+            VerifyInlineProjection<QuestMonstersWithBaseClassAndIdOverloadedWithNew>();
+        }
+
+        private void VerifyInlineProjection<T>() where T : IMonstersView
+        {
+            var loadedView = theSession.Load<T>(streamId);
+
+            loadedView.Id.ShouldBe(streamId);
+            loadedView.Monsters.ShouldHaveTheSameElementsAs("Troll", "Dragon");
+
+            var queriedView = theSession.Query<T>()
+                .Single(x => x.Id == streamId);
+
+            queriedView.Id.ShouldBe(streamId);
+            queriedView.Monsters.ShouldHaveTheSameElementsAs("Troll", "Dragon");
+        }
+    }
+}
---
+++ src/Marten.Testing/Events/Projections/view_projection_for_view_with_base_class.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Linq;
+using Marten.Events.Projections;
+using Marten.Services;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Events.Projections
+{
+    public class view_projection_for_view_with_base_class: DocumentSessionFixture<NulloIdentityMap>
+    {
+        private readonly MonsterSlayed slayed1 = new MonsterSlayed { Name = "Troll" };
+        private readonly MonsterSlayed slayed2 = new MonsterSlayed { Name = "Dragon" };
+        private readonly Guid streamId;
+
+        public view_projection_for_view_with_base_class()
+        {
+            StoreOptions(_ =>
+            {
+                _.AutoCreateSchemaObjects = AutoCreate.All;
+                _.Events.InlineProjections.Add<ViewProjectionForViewWithBaseClass>();
+                _.Events.InlineProjections.Add<ViewProjectionForViewWithBaseClassAndIdOverloaded>();
+                _.Events.InlineProjections.Add<ViewProjectionForViewWithBaseClassAndIdOverloadedWithNew>();
+            });
+
+            streamId = theSession.Events
+                .StartStream<QuestMonstersWithBaseClass>(slayed1, slayed2).Id;
+
+            theSession.SaveChanges();
+        }
+
+        [Fact]
+        public void run_view_projection_with_base_class()
+        {
+            VerifyInlineProjection<QuestMonstersWithBaseClass>();
+        }
+
+        [Fact]
+        public void run_view_projection_with_base_class_and_id_overloaded()
+        {
+            VerifyInlineProjection<QuestMonstersWithBaseClassAndIdOverloaded>();
+        }
+
+        [Fact]
+        public void run_view_projection_with_base_class_and_id_overloaded_with_new()
+        {
+            VerifyInlineProjection<QuestMonstersWithBaseClassAndIdOverloadedWithNew>();
+        }
+
+        private void VerifyInlineProjection<T>() where T : IMonstersView
+        {
+            var loadedView = theSession.Load<T>(streamId);
+
+            loadedView.Id.ShouldBe(streamId);
+            loadedView.Monsters.ShouldHaveTheSameElementsAs("Troll", "Dragon");
+
+            var queriedView = theSession.Query<T>()
+                .Single(x => x.Id == streamId);
+
+            queriedView.Id.ShouldBe(streamId);
+            queriedView.Monsters.ShouldHaveTheSameElementsAs("Troll", "Dragon");
+        }
+
+        public class ViewProjectionForViewWithBaseClass: ViewProjection<QuestMonstersWithBaseClass, Guid>
+        {
+            public ViewProjectionForViewWithBaseClass()
+            {
+                ProjectEvent<MonsterSlayed>(Project);
+            }
+
+            private void Project(QuestMonstersWithBaseClass view, MonsterSlayed @event)
+            {
+                view.Apply(@event);
+            }
+        }
+
+        public class ViewProjectionForViewWithBaseClassAndIdOverloaded: ViewProjection<QuestMonstersWithBaseClassAndIdOverloaded, Guid>
+        {
+            public ViewProjectionForViewWithBaseClassAndIdOverloaded()
+            {
+                ProjectEvent<MonsterSlayed>(Project);
+            }
+
+            private void Project(QuestMonstersWithBaseClassAndIdOverloaded view, MonsterSlayed @event)
+            {
+                view.Apply(@event);
+            }
+        }
+
+        public class ViewProjectionForViewWithBaseClassAndIdOverloadedWithNew: ViewProjection<QuestMonstersWithBaseClassAndIdOverloadedWithNew, Guid>
+        {
+            public ViewProjectionForViewWithBaseClassAndIdOverloadedWithNew()
+            {
+                ProjectEvent<MonsterSlayed>(Project);
+            }
+
+            private void Project(QuestMonstersWithBaseClassAndIdOverloadedWithNew view, MonsterSlayed @event)
+            {
+                view.Apply(@event);
+            }
+        }
+    }
+}
