cc2db5ce5c5b8ad575553e66604179e52a8f8050
jokokko <jokokko@gmail.com>
2020-01-23 15:44:02 +0200

    An example scenario on storing & querying non-uniform JSON records via the help of `dynamic`. Nothing elegant, but still better than no docs at all.

---
+++ documentation/documentation/scenarios/dynamicdata.md
@@ -0,0 +1,17 @@
+<!--Title: Storing & reading back non-uniform JSON documents via dynamic -->
+
+This scenario demonstrates how to store and query non-uniform JSON documents via the help of [`dynamic`](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/using-type-dynamic).
+
+## Scenario
+
+Let us assume we have a document with non-uniform JSON records, presenting temperature sensor data whereby individual records are identified either via the field _detector_ or _sensor_.
+
+<[sample:sample-scenarios-dynamic-records]>
+
+To store and later read back these records, we create a wrapper type with a `dynamic` property to present our record.
+
+<[sample:sample-scenarios-dynamic-type]>
+
+We then read and serialize our records into our newly introduced intermediate type and persist an array of its instances via `BulkInsert`. Lastly, we read back the records, via the non-generic `Query` extension method, passing in predicates that take into account the non-uniform fields of the source documents. After reading back the data for _sensor-1_, we calculate the average of its recorded temperatures:
+
+<[sample:sample-scenarios-dynamic-insertandquery]>
---
+++ src/Marten.Testing/Scenarios/persist_and_query_via_dynamic.cs
@@ -0,0 +1,77 @@
+using System.IO;
+using System.Linq;
+using Marten.Services;
+using Xunit;
+
+namespace Marten.Testing.Scenarios
+{
+    public class persist_and_query_via_dynamic: DocumentSessionFixture<NulloIdentityMap>
+    {
+        // SAMPLE: sample-scenarios-dynamic-type
+        class TemperatureData
+        {
+            public int Id { get; set; }
+            public dynamic Values { get; set; }
+        }
+        // ENDSAMPLE
+
+        [Fact]
+        public void CanPersistAndQueryDynamic()
+        {
+            // SAMPLE: sample-scenarios-dynamic-records
+            // Our documents with non-uniform structure
+            var jsonRecords = @"
+[{
+        'sensor': 'aisle-1',
+        'timestamp': '2020-01-21 11:19:19.283',
+        'temperature': 21.2
+    }, {
+        'sensor': 'aisle-2',
+        'timestamp': '2020-01-21 11:18:19.220',
+        'temperature': 21.6
+    }, {
+        'sensor': 'aisle-1',
+        'timestamp': '2020-01-21 11:17:19.190',
+        'temperature': 21.6
+    }, {
+        'detector': 'aisle-1',
+        'timestamp': '2020-01-21 11:16:19.100',
+        'temperature': 20.9
+    }, {
+        'sensor': 'aisle-3',
+        'timestamp': '2020-01-21 11:15:19.037',
+        'temperature': 21.7,
+    }, {
+        'detector': 'aisle-1',
+        'timestamp': '2020-01-21 11:14:19.100',
+        'temperature': -1.0
+    }
+]";
+            // ENDSAMPLE
+
+            // SAMPLE: sample-scenarios-dynamic-insertandquery
+            using (var reader = new StringReader(jsonRecords))
+            {
+                // Deserialize our document
+                var records = theStore.Serializer.FromJson<dynamic[]>(reader);
+                var docs = records.Select(x => new TemperatureData { Values = x }).ToArray();
+
+                // Persist our records
+                theStore.BulkInsertDocuments(docs);
+
+                using (var session = theStore.OpenSession())
+                {
+                    // Read back the data for "aisle-1"
+                    dynamic[] tempsFromDb = session.Query(typeof(TemperatureData),
+                        "where data->'Values'->>'detector' = :sensor OR data->'Values'->>'sensor' = :sensor", new { sensor = "aisle-1" }).ToArray();
+
+                    var temperatures = tempsFromDb.Select(x => (decimal)x.Values.temperature);
+
+                    Assert.Equal(15.675m, temperatures.Average());
+                    Assert.Equal(4, tempsFromDb.Length);
+                }
+            }
+            // ENDSAMPLE
+        }
+    }
+}
