542e955a1255171b8883886acebf799de9ff6b59
Miguel <242977+Miggleness@users.noreply.github.com>
2020-02-01 07:56:50 +0800

    Updated test
    Increased MaxLo in test for 1404 yet again so that we aren't getting the next hi too aggressively to the point the tests fail

--- src/Marten.Testing/Schema/Identity/Sequences/Bug_1404_Hilo_concurrent_update_failure.cs
+++ src/Marten.Testing/Schema/Identity/Sequences/Bug_1404_Hilo_concurrent_update_failure.cs
@@ -51,7 +51,7 @@ namespace Marten.Testing.Schema.Identity.Sequences
         {
             return DocumentStore.For(_ =>
             {
-                _.HiloSequenceDefaults.MaxLo = 3;
+                _.HiloSequenceDefaults.MaxLo = 5;
                 _.Connection(ConnectionSource.ConnectionString);
                 _.AutoCreateSchemaObjects = AutoCreate.All;
             });
