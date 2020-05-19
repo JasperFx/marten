e230f24f4a0fc09f7267c6e8441d632a893755d7
Oskar Dudycz <oskar.dudycz@gmail.com>
2020-05-19 22:12:10 +0200

    #1389 - Added support for hierarchical documents in custom SQL query

--- src/Marten.Schema.Testing/Hierarchies/end_to_end_document_hierarchy_with_interface_tests.cs
+++ src/Marten.Schema.Testing/Hierarchies/end_to_end_document_hierarchy_with_interface_tests.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Linq;
 using Marten.Services;
 using Newtonsoft.Json;
+using Shouldly;
 using Xunit;
 
 namespace Marten.Schema.Testing.Hierarchies
 {
-    public class end_to_end_document_hierarchy_with_interface_tests : IntegrationContextWithIdentityMap<NulloIdentityMap>
+    public class end_to_end_document_hierarchy_with_interface_tests: IntegrationContextWithIdentityMap<NulloIdentityMap>
     {
         public end_to_end_document_hierarchy_with_interface_tests()
         {
@@ -15,46 +17,81 @@ namespace Marten.Schema.Testing.Hierarchies
                     _.Schema.For<IPolicy>()
                         .Identity(x => x.VersionId)
                         .AddSubClassHierarchy();
+
+                    _.Schema.For<IPolicy>().GinIndexJsonData();
                 });
         }
 
         [Fact]
         public void persists_subclass()
         {
-            var policy = new LinuxPolicy { Name = Guid.NewGuid().ToString() };
+            var policy = new LinuxPolicy {Name = Guid.NewGuid().ToString()};
             using (var session = theStore.LightweightSession())
             {
                 session.Store(policy);
                 session.SaveChanges();
             }
         }
+
+
+        [Fact]
+        public void query_for_only_a_subclass_with_string_where_clause()
+        {
+            var policy = new LinuxPolicy {VersionId = Guid.NewGuid(), Name = Guid.NewGuid().ToString()};
+            using (var session = theStore.LightweightSession())
+            {
+                session.Store(policy);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.LightweightSession())
+            {
+                session.Query<IPolicy>($"Where id = \'{policy.VersionId}\'").Single()
+                    .VersionId.ShouldBe(policy.VersionId);
+            }
+        }
+
+
+        [Fact]
+        public void query_for_only_a_subclass_with_where_clause()
+        {
+            var policy = new LinuxPolicy {VersionId = Guid.NewGuid(), Name = Guid.NewGuid().ToString()};
+            using (var session = theStore.LightweightSession())
+            {
+                session.Store(policy);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.LightweightSession())
+            {
+                session.Query<IPolicy>().Single(p => p.VersionId == policy.VersionId)
+                    .VersionId.ShouldBe(policy.VersionId);
+            }
+        }
     }
 
-    public abstract class BasePolicy : IPolicy
+    public abstract class BasePolicy: IPolicy
     {
         public Guid VersionId { get; set; } = Guid.NewGuid();
         public Guid DocumentId { get; set; } = Guid.NewGuid();
 
-        [JsonIgnore]
-        public abstract PolicyType Type { get; protected set; }
+        [JsonIgnore] public abstract PolicyType Type { get; protected set; }
 
         public string Name { get; set; }
     }
 
-    public abstract class OsPolicy : BasePolicy
+    public abstract class OsPolicy: BasePolicy
     {
     }
 
-    public class LinuxPolicy : OsPolicy
+    public class LinuxPolicy: OsPolicy
     {
-        [JsonIgnore]
-        public override PolicyType Type { get; protected set; } = PolicyType.Linux;
+        [JsonIgnore] public override PolicyType Type { get; protected set; } = PolicyType.Linux;
     }
 
-    public class WindowsPolicy : OsPolicy
+    public class WindowsPolicy: OsPolicy
     {
-        [JsonIgnore]
-        public override PolicyType Type { get; protected set; } = PolicyType.Windows;
+        [JsonIgnore] public override PolicyType Type { get; protected set; } = PolicyType.Windows;
     }
 
     public interface IVersioned
@@ -66,7 +103,7 @@ namespace Marten.Schema.Testing.Hierarchies
         Guid DocumentId { get; set; }
     }
 
-    public interface IPolicy : IVersioned
+    public interface IPolicy: IVersioned
     {
         PolicyType Type { get; }
         string Name { get; set; }
@@ -75,6 +112,6 @@ namespace Marten.Schema.Testing.Hierarchies
     public enum PolicyType
     {
         Windows = 1,
-        Linux = 2,
+        Linux = 2
     }
 }
--- src/Marten/Linq/DeserializeSelector.cs
+++ src/Marten/Linq/DeserializeSelector.cs
@@ -11,28 +11,27 @@ namespace Marten.Linq
     {
         private readonly ISerializer _serializer;
 
-        public DeserializeSelector(ISerializer serializer) : base("data")
+        public DeserializeSelector(ISerializer serializer): base("data")
         {
             _serializer = serializer;
         }
 
-        public DeserializeSelector(ISerializer serializer, params string[] selectFields) : base(selectFields)
+        public DeserializeSelector(ISerializer serializer, params string[] selectFields): base(selectFields)
         {
             _serializer = serializer;
         }
 
         public T Resolve(DbDataReader reader, IIdentityMap map, QueryStatistics stats)
         {
-            if (!typeof(T).IsSimple())
-            {
-                var json = reader.As<DbDataReader>().GetTextReader(0);
-                return _serializer.FromJson<T>(json);
-            }
+            if (typeof(T).IsSimple())
+                return reader.GetFieldValue<T>(0);
 
-            return reader.GetFieldValue<T>(0);
+            var json = reader.As<DbDataReader>().GetTextReader(0);
+            return _serializer.FromJson<T>(json);
         }
 
-        public async Task<T> ResolveAsync(DbDataReader reader, IIdentityMap map, QueryStatistics stats, CancellationToken token)
+        public async Task<T> ResolveAsync(DbDataReader reader, IIdentityMap map, QueryStatistics stats,
+            CancellationToken token)
         {
             if (!typeof(T).IsSimple())
             {
