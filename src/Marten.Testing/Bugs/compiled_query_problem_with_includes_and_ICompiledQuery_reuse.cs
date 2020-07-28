c1380e3bac456bb432aa3c64bb84fc6bac4c0e03
jeremydmiller <jeremydmiller@yahoo.com>
2020-07-28 06:29:39 -0500

    Brought in an old bug report that appears to be fixed now

--- src/Marten.Testing/Bugs/compiled_query_problem_with_includes_and_ICompiledQuery_reuse.cs
+++ src/Marten.Testing/Bugs/compiled_query_problem_with_includes_and_ICompiledQuery_reuse.cs
@@ -1,27 +1,28 @@
-﻿using Marten.Linq;
-using Marten.Services;
-using Marten.Services.Includes;
-using Marten.Testing.Documents;
-using Shouldly;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Marten.Linq;
+using Marten.Testing.Documents;
+using Marten.Testing.Harness;
+using Shouldly;
 using Xunit;
 
 namespace Marten.Testing.Bugs
 {
-    public class compiled_query_problem_with_includes_and_ICompiledQuery_reuse : DocumentSessionFixture<IdentityMap>
+    public class compiled_query_problem_with_includes_and_ICompiledQuery_reuse: IntegrationContext
     {
-        public class IssueWithUsers : ICompiledListQuery<Issue>
+        public compiled_query_problem_with_includes_and_ICompiledQuery_reuse(DefaultStoreFixture fixture): base(fixture)
         {
-            public List<User> Users { get; set; }
-            // Can also work like that:
-            //public List<User> Users => new List<User>();
+        }
 
-            public Expression<Func<IQueryable<Issue>, IEnumerable<Issue>>> QueryIs()
+        public class IssueWithUsers: ICompiledListQuery<Issue>
+        {
+            public List<User> Users { get; } = new List<User>();
+
+            public Expression<Func<IMartenQueryable<Issue>, IEnumerable<Issue>>> QueryIs()
             {
-                return query => query.Include<Issue, IssueWithUsers>(x => x.AssigneeId, x => x.Users, JoinType.Inner);
+                return query => query.Include(x => x.AssigneeId, Users);
             }
         }
 
@@ -31,9 +32,9 @@ namespace Marten.Testing.Bugs
             var user1 = new User();
             var user2 = new User();
 
-            var issue1 = new Issue { AssigneeId = user1.Id, Title = "Garage Door is busted" };
-            var issue2 = new Issue { AssigneeId = user2.Id, Title = "Garage Door is busted" };
-            var issue3 = new Issue { AssigneeId = user2.Id, Title = "Garage Door is busted" };
+            var issue1 = new Issue {AssigneeId = user1.Id, Title = "Garage Door is busted"};
+            var issue2 = new Issue {AssigneeId = user2.Id, Title = "Garage Door is busted"};
+            var issue3 = new Issue {AssigneeId = user2.Id, Title = "Garage Door is busted"};
 
             theSession.Store(user1, user2);
             theSession.Store(issue1, issue2, issue3);
@@ -48,8 +49,8 @@ namespace Marten.Testing.Bugs
                 query.Users.Count.ShouldBe(2);
                 issues.Count().ShouldBe(3);
 
-                query.Users.Any(x => x.Id == user1.Id);
-                query.Users.Any(x => x.Id == user2.Id);
+                query.Users.Any(x => x.Id == user1.Id).ShouldBeTrue();
+                query.Users.Any(x => x.Id == user2.Id).ShouldBeTrue();
             }
 
             // Issue second query
