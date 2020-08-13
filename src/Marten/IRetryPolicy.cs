34373df42143a1478337eb10f42aa1bb7f9160f8
Marko Lahma <marko.lahma@gmail.com>
2020-08-13 22:02:17 +0300

    introduce DefaultRetryPolicy

--- src/Marten/IRetryPolicy.cs
+++ src/Marten/IRetryPolicy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace Marten
 {
@@ -64,6 +65,109 @@ namespace Marten
         public Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> operation, CancellationToken cancellationToken)
         {
             return operation();
+        }
+    }
+
+    /// <summary>
+    /// Default retry policy, which accounts for <see cref="NpgsqlException.IsTransient"/>.
+    /// </summary>
+    /// <remarks>
+    /// Based on example https://martendb.io/documentation/documents/advanced/retrypolicy/ by Joona-Pekka Kokko.
+    /// </remarks>
+    public sealed class DefaultRetryPolicy: IRetryPolicy
+    {
+        private readonly int _maxTries;
+        private readonly Func<Exception, bool> _filter;
+
+        private static readonly Func<Exception, bool> DefaultFilter = x => x is NpgsqlException npgsqlException && npgsqlException.IsTransient;
+
+        private DefaultRetryPolicy(int maxTries, Func<Exception, bool> filter)
+        {
+            _maxTries = maxTries;
+            _filter = filter;
+        }
+
+        /// <summary>
+        /// Initializes a retry policy that will retry once after failure.
+        /// </summary>
+        /// <param name="filter">Optional filter, default to checking for <see cref="NpgsqlException.IsTransient"/></param>
+        /// <returns>The configured retry policy.</returns>
+        public static IRetryPolicy Once(Func<Exception, bool> filter)
+        {
+            return new DefaultRetryPolicy(2, filter ?? DefaultFilter);
+        }
+
+        /// <summary>
+        /// Initializes a retry policy that will retry twice after failure.
+        /// </summary>
+        /// <param name="filter">Optional filter, default to checking for <see cref="NpgsqlException.IsTransient"/></param>
+        /// <returns>The configured retry policy.</returns>
+        public static IRetryPolicy Twice(Func<Exception, bool> filter = null)
+        {
+            return new DefaultRetryPolicy(3, filter ?? DefaultFilter);
+        }
+
+        /// <summary>
+        /// Initializes a retry policy that will retry given amount of times after failure.
+        /// </summary>
+        /// <param name="filter">Optional filter, default to checking for <see cref="NpgsqlException.IsTransient"/></param>
+        /// <returns>The configured retry policy.</returns>
+        public static IRetryPolicy Times(int times, Func<Exception, bool> filter = null)
+        {
+            return new DefaultRetryPolicy(times + 1, filter ?? DefaultFilter);
+        }
+
+        void IRetryPolicy.Execute(Action operation)
+        {
+            Try(() =>
+            {
+                operation();
+                return Task.CompletedTask;
+            }, CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        TResult IRetryPolicy.Execute<TResult>(Func<TResult> operation)
+        {
+            return Try(() => Task.FromResult(operation()), CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        Task IRetryPolicy.ExecuteAsync(Func<Task> operation, CancellationToken cancellationToken)
+        {
+            return Try(operation, cancellationToken);
+        }
+
+        Task<TResult> IRetryPolicy.ExecuteAsync<TResult>(Func<Task<TResult>> operation, CancellationToken cancellationToken)
+        {
+            return Try(operation, cancellationToken);
+        }
+
+        private async Task Try(Func<Task> operation, CancellationToken token)
+        {
+            for (var tries = 0;; token.ThrowIfCancellationRequested())
+            {
+                try
+                {
+                    await operation().ConfigureAwait(false);
+                    return;
+                }
+                catch (Exception e) when (++tries < _maxTries && _filter(e))
+                {
+                }
+            }
+        }
+
+        private async Task<T> Try<T>(Func<Task<T>> operation, CancellationToken token)
+        {
+            for (var tries = 0;; token.ThrowIfCancellationRequested())
+            {
+                try
+                {
+                    return await operation().ConfigureAwait(false);
+                }
+                catch (Exception e) when (++tries < _maxTries && _filter(e))
+                {
+                }
+            }
         }
     }
 }
