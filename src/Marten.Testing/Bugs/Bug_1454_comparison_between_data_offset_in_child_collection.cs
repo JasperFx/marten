25309cb928e9930930839431bacb04c4ddcc4ee2
jeremydmiller <jeremydmiller@yahoo.com>
2020-09-13 14:57:00 -0500

    Validating the usage of DateTime/DateTimeOffset comparisons within SelectMany() conditions. Closes GH-1454

---
+++ src/Marten.Testing/Bugs/Bug_1454_comparison_between_data_offset_in_child_collection.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using Marten.Testing.Documents;
+using Marten.Testing.Harness;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_1454_comparison_between_data_offset_in_child_collection : IntegrationContext
+    {
+        public Bug_1454_comparison_between_data_offset_in_child_collection(DefaultStoreFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public void can_query_against_DateTime()
+        {
+            theSession.Query<Target>().SelectMany(x => x.Children)
+                .Where(x => x.Date == DateTime.Today.AddDays(-3))
+                .ToList().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void can_query_against_DateTimeOffset()
+        {
+            theSession.Query<Target>().SelectMany(x => x.Children)
+                .Where(x => x.DateOffset == DateTimeOffset.Now)
+                .ToList().ShouldNotBeNull();
+        }
+    }
+}
