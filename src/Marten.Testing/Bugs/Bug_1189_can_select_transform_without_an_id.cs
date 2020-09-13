bb54ff99cf78db4acba4b87534a640969a7062be
jeremydmiller <jeremydmiller@yahoo.com>
2020-09-13 15:02:47 -0500

    Verifying the Select() to a new type does not require an Id. Closes GH-1189

---
+++ src/Marten.Testing/Bugs/Bug_1189_can_select_transform_without_an_id.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using Marten.Testing.Documents;
+using Marten.Testing.Harness;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_1189_can_select_transform_without_an_id : IntegrationContext
+    {
+        public Bug_1189_can_select_transform_without_an_id(DefaultStoreFixture fixture) : base(fixture)
+        {
+        }
+
+        public class TargetView
+        {
+            public Colors Color { get; set; }
+            public int Number { get; set; }
+        }
+
+        [Fact]
+        public void can_select()
+        {
+            var targets = Target.GenerateRandomData(100).ToArray();
+            theStore.BulkInsert(targets);
+
+            var view = theSession.Query<Target>().Select(x => new TargetView {Color = x.Color, Number = x.Number})
+                .FirstOrDefault();
+
+            view.ShouldNotBeNull();
+
+
+
+        }
+    }
+}
