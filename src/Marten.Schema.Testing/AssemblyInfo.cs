65da433a528a9d9399fafcc385fa39835b8c8ca4
mysticmind <babu.annamalai@gmail.com>
2020-12-04 08:11:15 +0530

    Enable test parallelization by default

--- src/Marten.Schema.Testing/AssemblyInfo.cs
+++ src/Marten.Schema.Testing/AssemblyInfo.cs
@@ -1,6 +1,2 @@
 using Xunit;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
+[assembly: CollectionBehavior(DisableTestParallelization = false)]
