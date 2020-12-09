c054188aa27568d747e264b962e96a9c8d63f2e8
jeremydmiller <jeremydmiller@yahoo.com>
2020-12-09 13:06:09 -0600

    Getting the MethodCollection ready for a mix of real methods and Lambdas

--- src/Marten/Events/V4Concept/CodeGeneration/MethodCollection.cs
+++ src/Marten/Events/V4Concept/CodeGeneration/MethodCollection.cs
@@ -4,26 +4,60 @@ using System.Linq;
 using System.Reflection;
 using LamarCodeGeneration;
 using LamarCodeGeneration.Frames;
+using LamarCodeGeneration.Model;
 using Marten.Schema;
 
 namespace Marten.Events.V4Concept.CodeGeneration
 {
+    internal class MethodSlot
+    {
+        public Setter Setter { get; }
+        public MethodInfo Method { get; }
+
+        public MethodSlot(MethodInfo method)
+        {
+            Method = method;
+            EventType = method.GetEventType();
+        }
+
+        public Type EventType { get; }
+
+        public MethodSlot(Setter setter, MethodInfo method, Type eventType)
+        {
+            Setter = setter;
+            Method = method;
+            EventType = eventType;
+        }
+    }
+
     internal abstract class MethodCollection
     {
+        private int _lambdaNumber = 0;
+
+
         public Type ProjectionType { get; }
 
         protected MethodCollection(string methodName, Type projectionType)
         {
             ProjectionType = projectionType;
 
-            Methods = projectionType.GetMethods().Where(x => x.Name == methodName).ToArray();
-            IsAsync = Methods.Any(x => x.IsAsync());
+            Methods = projectionType.GetMethods().Where(x => x.Name == methodName).Select(x => new MethodSlot(x)).ToList();
+            IsAsync = Methods.Any(x => x.Method.IsAsync());
+            LambdaName = methodName;
+        }
+
+        public string LambdaName { get; protected set; }
+
+        public void AddLambda(object lambda)
+        {
+            var name = LambdaName + (++_lambdaNumber).ToString();
+            var method = lambda.GetType().GetMethod("Invoke");
         }
 
         public abstract IEventHandlingFrame CreateAggregationHandler(Type aggregateType,
             DocumentMapping aggregateMapping, MethodInfo method);
 
-        public MethodInfo[] Methods { get; }
+        public List<MethodSlot> Methods { get; }
 
         public bool IsAsync { get;}
 
@@ -41,9 +75,9 @@ namespace Marten.Events.V4Concept.CodeGeneration
 
             foreach (var collection in collections)
             {
-                foreach (var methodInfo in collection.Methods)
+                foreach (var slot in collection.Methods)
                 {
-                    var frame = collection.CreateAggregationHandler(aggregateType, mapping, methodInfo);
+                    var frame = collection.CreateAggregationHandler(aggregateType, mapping, slot.Method);
                     if (byType.TryGetValue(frame.EventType, out var container))
                     {
                         container.Add((Frame) frame);
