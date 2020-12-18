e40806b3d0444f4b5df57411bbca8b0515bcfb83
jeremydmiller <jeremydmiller@yahoo.com>
2020-12-18 10:25:39 -0600

    WIP: ripped out DocumentProjection

--- src/Marten/Events/Projections/IDocumentsProjection.cs
+++ src/Marten/Events/Projections/IDocumentsProjection.cs
@@ -12,24 +12,4 @@ namespace Marten.Events.Projections
         Type[] Produces { get; }
     }
 
-    [Obsolete("This will be eliminated in V4 and replaced w/ the new ViewProjection")]
-    public abstract class DocumentsProjection: IDocumentsProjection
-    {
-        public abstract Type[] Consumes { get; }
-        public abstract Type[] Produces { get; }
-
-        public abstract AsyncOptions AsyncOptions { get; }
-
-        public abstract void Apply(IDocumentSession session, EventPage page);
-
-        public abstract Task ApplyAsync(IDocumentSession session, EventPage page, CancellationToken token);
-
-        public void EnsureStorageExists(ITenant tenant)
-        {
-            foreach (var type in Produces)
-            {
-                tenant.EnsureStorageExists(type);
-            }
-        }
-    }
 }
