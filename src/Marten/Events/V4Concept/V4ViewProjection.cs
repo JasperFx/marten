e42f8b239fe3ec4e5963508905cece5cfd9463ad
jeremydmiller <jeremydmiller@yahoo.com>
2020-12-18 09:22:33 -0600

    Getting ready for the V4 projection switcheroo!

--- src/Marten/Events/V4Concept/V4ViewProjection.cs
+++ src/Marten/Events/V4Concept/V4ViewProjection.cs
@@ -1,53 +1,28 @@
 using System;
-using System.Reflection;
-using System.Threading.Tasks;
-using LamarCodeGeneration;
-using Marten.Events.V4Concept.CodeGeneration;
-using Marten.Schema;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Marten.Events.V4Concept.Aggregation;
 
 namespace Marten.Events.V4Concept
 {
     /// <summary>
-    /// This would be a helper for the open ended, ViewProjection
+    /// Project a single document view across events that may span across
+    /// event streams in a user-defined grouping
     /// </summary>
-    internal class ProjectMethodCollection: MethodCollection
+    /// <typeparam name="TDoc"></typeparam>
+    /// <typeparam name="TId"></typeparam>
+    public abstract class V4ViewProjection<TDoc, TId> : V4AggregateProjection<TDoc>
     {
-        public static readonly string MethodName = "Process";
-
-        public ProjectMethodCollection(Type projectionType, Type aggregateType) : base(MethodName, projectionType, aggregateType)
-        {
-        }
-
-        public override IEventHandlingFrame CreateEventTypeHandler(Type aggregateType, DocumentMapping aggregateMapping,
-            MethodSlot slot)
+        // TODO -- the split logic will be different. This'll need to override that a bit
+        public void Identity<TEvent>(Expression<Func<TEvent, TId>> expression)
         {
             throw new NotImplementedException();
         }
+
+        public void FanOut<TEvent, TChild>(Expression<Func<TEvent, IEnumerable<TChild>>> expression)
+        {
+
+            throw new NotImplementedException();
+        }
     }
-
-    // This would be the basis for the newly improved V4 version of ViewProjection
-    public abstract class ViewProjection
-    {
-        private readonly ProjectMethodCollection _projectMethods;
-        private string _projectionName; // More on this later
-
-        public ViewProjection()
-        {
-            _projectMethods = new ProjectMethodCollection(GetType(), null);
-        }
-
-        [IgnoreProjectionMethod]
-        public void Project<TEvent>(Action<TEvent, IDocumentOperations> project)
-        {
-            _projectMethods.AddLambda(project, typeof(TEvent));
-        }
-
-        [IgnoreProjectionMethod]
-        public void ProjectAsync<TEvent>(Func<TEvent, IDocumentOperations, Task> project)
-        {
-            _projectMethods.AddLambda(project, typeof(TEvent));
-        }
-
-    }
-
 }
