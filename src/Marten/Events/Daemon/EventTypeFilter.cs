9addbd923764e2f775ec9cd3c07cb82feb02f260
jeremydmiller <jeremydmiller@yahoo.com>
2021-01-14 14:49:32 -0600

    New EventTypeFilter for the async daemon

---
+++ src/Marten/Events/Daemon/EventTypeFilter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Marten.Linq.SqlGeneration;
+using Marten.Util;
+using NpgsqlTypes;
+
+namespace Marten.Events.Daemon
+{
+    internal class EventTypeFilter: ISqlFragment
+    {
+        private readonly string[] _typeNames;
+
+        public EventTypeFilter(EventGraph graph, Type[] eventTypes)
+        {
+            _typeNames = eventTypes.Select(x => graph.EventMappingFor((Type) x).Alias).ToArray();
+        }
+
+        public void Apply(CommandBuilder builder)
+        {
+            var parameters = builder.AppendWithParameters("d.type = ANY(?)");
+            parameters[0].NpgsqlDbType = NpgsqlDbType.Varchar | NpgsqlDbType.Array;
+            parameters[0].Value = _typeNames;
+        }
+
+        public bool Contains(string sqlText)
+        {
+            return false;
+        }
+    }
+}
