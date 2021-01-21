0a97796f0781c7d7d2617fa2e617ca96261b55bd
jeremydmiller <jeremydmiller@yahoo.com>
2021-01-21 15:51:32 -0600

    First end to end test of an EventProjection running asynchronously

---
+++ src/Marten.Testing/Events/Daemon/event_projections_end_to_end.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Baseline.Dates;
+using Marten.Events;
+using Marten.Events.Daemon;
+using Marten.Events.Projections;
+using Marten.Testing.Events.Daemon.TestingSupport;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+using Shouldly;
+
+namespace Marten.Testing.Events.Daemon
+{
+    public class event_projections_end_to_end : DaemonContext
+    {
+        private readonly ITestOutputHelper _output;
+
+        public event_projections_end_to_end(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task run_simultaneously()
+        {
+            StoreOptions(x => x.Events.Projections.Async(new DistanceProjection()));
+
+            NumberOfStreams = 10;
+
+            var logger = new TestLogger<IProjection>(_output);
+            var agent = new NodeAgent(theStore, logger);
+
+            await agent.StartAll();
+
+            var waiter = agent.Tracker.WaitForShardState(new ShardState("Distance", NumberOfEvents), 15.Seconds());
+
+            await PublishSingleThreaded();
+
+
+            await waiter;
+
+            var distances = await theSession.Query<Distance>().ToListAsync();
+
+            var events = (await theSession.Events.QueryAllRawEvents().ToListAsync());
+            var travels = events.OfType<Event<Travel>>().ToDictionary(x => x.Id);
+
+            foreach (var distance in distances)
+            {
+
+                if (travels.TryGetValue(distance.Id, out var travel))
+                {
+                    distance.Day.ShouldBe(travel.Data.Day);
+                    distance.Total.ShouldBe(travel.Data.TotalDistance());
+                }
+                else
+                {
+                    travel.ShouldNotBeNull();
+                }
+
+                logger.LogDebug("Compared distance " + distance);
+            }
+        }
+    }
+
+    public class Distance
+    {
+        public Guid Id { get; set; }
+        public double Total { get; set; }
+        public int Day { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Total)}: {Total}, {nameof(Day)}: {Day}";
+        }
+    }
+
+    public class DistanceProjection: EventProjection
+    {
+        public DistanceProjection()
+        {
+            ProjectionName = "Distance";
+        }
+
+        public Distance Create(Event<Travel> travel)
+        {
+            return new Distance {Id = travel.Id, Day = travel.Data.Day, Total = travel.Data.TotalDistance()};
+        }
+    }
+}
