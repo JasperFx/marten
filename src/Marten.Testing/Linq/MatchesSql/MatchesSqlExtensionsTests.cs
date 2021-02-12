bfb85ba2a6257bb8de20c86a0d075de978dc653b
karelg <karel.golberg@agileworks.eu>
2021-02-12 17:12:02 +0200

    Add unittests to Linq extensions

---
+++ src/Marten.Testing/Linq/MatchesSql/MatchesSqlExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Marten.Linq;
+using Marten.Linq.MatchesSql;
+using Marten.Testing.CoreFunctionality;
+using Marten.Util;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Linq.MatchesSql
+{
+    public class MatchesSqlExtensionsTests
+    {
+        [Fact]
+        public void Throws_NotSupportedException_when_called_directly()
+        {
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(
+                () => new object().MatchesSql("d.data ->> 'UserName' = ? or d.data ->> 'UserName' = ?", "baz", "jack"));
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(
+                () => new object().MatchesSql(new WhereFragment("d.data ->> 'UserName' != ?", "baz")));
+        }
+
+    }
+}
---
+++ src/Marten.Testing/LinqExtensionsTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Remotion.Linq.Parsing.ExpressionVisitors.MemberBindings;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing
+{
+    public class LinqExtensionsTests
+    {
+        [Fact]
+        public void IsOneOf_shows_if_value_is_contained_in_the_specified_collection()
+        {
+            1.IsOneOf(1, 2).ShouldBeTrue();
+            1.IsOneOf(2, 1).ShouldBeTrue();
+            1.IsOneOf(2, 3).ShouldBeFalse();
+            1.IsOneOf(new List<int> {1, 2}).ShouldBeTrue();
+            1.IsOneOf(new List<int> {2, 1}).ShouldBeTrue();
+            1.IsOneOf(new List<int> {2, 3}).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void In_shows_if_value_is_contained_in_the_specified_collection()
+        {
+            1.In(1, 2).ShouldBeTrue();
+            1.In(2, 1).ShouldBeTrue();
+            1.In(2, 3).ShouldBeFalse();
+            1.In(new List<int> {1, 2}).ShouldBeTrue();
+            1.In(new List<int> {2, 1}).ShouldBeTrue();
+            1.In(new List<int> {2, 3}).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void IsSupersetOf_shows_if_collection_is_superset_of_the_specified_collection()
+        {
+            new[] {1, 2, 3}.IsSupersetOf(1, 2).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSupersetOf(2, 3).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSupersetOf(1, 2, 3).ShouldBeTrue();
+            new[] {1, 2}.IsSupersetOf(1, 2, 3).ShouldBeFalse();
+            new[] {2, 3}.IsSupersetOf(1, 2, 3).ShouldBeFalse();
+            new[] {1, 2, 3}.IsSupersetOf(new List<int> {1, 2}).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSupersetOf(new List<int> {2, 3}).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSupersetOf(new List<int> {1, 2, 3}).ShouldBeTrue();
+            new[] {1, 2}.IsSupersetOf(new List<int> {1, 2, 3}).ShouldBeFalse();
+            new[] {2, 3}.IsSupersetOf(new List<int> {1, 2, 3}).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void IsSubsetOf_shows_if_collection_is_subset_of_the_specified_collection()
+        {
+            new[] {1, 2}.IsSubsetOf(1, 2, 3).ShouldBeTrue();
+            new[] {2, 3}.IsSubsetOf(1, 2, 3).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSubsetOf(1, 2, 3).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSubsetOf(1, 2).ShouldBeFalse();
+            new[] {1, 2, 3}.IsSubsetOf(2, 3).ShouldBeFalse();
+            new[] {1, 2}.IsSubsetOf(new List<int> {1, 2, 3}).ShouldBeTrue();
+            new[] {2, 3}.IsSubsetOf(new List<int> {1, 2, 3}).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSubsetOf(new List<int> {1, 2, 3}).ShouldBeTrue();
+            new[] {1, 2, 3}.IsSubsetOf(new List<int> {1, 2}).ShouldBeFalse();
+            new[] {1, 2, 3}.IsSubsetOf(new List<int> {2, 3}).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void IsEmpty_shows_if_collection_is_null_or_empty()
+        {
+            ((IEnumerable<object>)null).IsEmpty().ShouldBeTrue();
+            Enumerable.Empty<object>().IsEmpty().ShouldBeTrue();
+            Enumerable.Repeat(new object(), 1).IsEmpty().ShouldBeFalse();
+            ((string)(null)).IsEmpty().ShouldBeTrue();
+            "".IsEmpty().ShouldBeTrue();
+            " ".IsEmpty().ShouldBeFalse();
+            "a".IsEmpty().ShouldBeFalse();
+        }
+
+        [Fact]
+        public void TenantIsOneOf_throws_NotSupportedException_when_called_directly()
+        {
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().TenantIsOneOf("a", "b"));
+        }
+
+        [Fact]
+        public void Search_throws_NotSupportedException_when_called_directly()
+        {
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().Search("search term"));
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().Search("search term", "reg conf"));
+        }
+
+        [Fact]
+        public void PlainTextSearch_throws_NotSupportedException_when_called_directly()
+        {
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().PlainTextSearch("search term"));
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().PlainTextSearch("search term", "reg conf"));
+        }
+
+        [Fact]
+        public void PhraseSearch_throws_NotSupportedException_when_called_directly()
+        {
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().PhraseSearch("search term"));
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().PhraseSearch("search term", "reg conf"));
+        }
+
+        [Fact]
+        public void WebStyleSearch_throws_NotSupportedException_when_called_directly()
+        {
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().WebStyleSearch("search term"));
+            ShouldThrowExtensions.ShouldThrow<NotSupportedException>(() => new object().WebStyleSearch("search term", "reg conf"));
+        }
+    }
+}
