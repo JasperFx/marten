2994f389f273fe7335dbff0a78e3f2654fd17309
jeremydmiller <jeremydmiller@yahoo.com>
2021-02-26 12:01:00 -0600

    Updated the command line support to the latest Oakton

--- src/AspNetCoreWithMarten/Program.cs
+++ src/AspNetCoreWithMarten/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Oakton.AspNetCore;
+using Oakton;
 
 namespace AspNetCoreWithMarten
 {
--- src/Marten.CommandLine/MartenInput.cs
+++ src/Marten.CommandLine/MartenInput.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using Oakton;
-using Oakton.AspNetCore;
 
 [assembly: OaktonCommandAssembly]
 
