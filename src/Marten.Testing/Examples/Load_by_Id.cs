0bd196ed19486ab0b2bfe78a794300100089e85d
Jedidja Bourgeois <me@jedidja.ca>
2021-03-04 08:04:21 -0500

    Update loading document documentation with possible null return value and async sample.

--- src/Marten.Testing/Examples/Load_by_Id.cs
+++ src/Marten.Testing/Examples/Load_by_Id.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Marten.Testing.Documents;
 
 namespace Marten.Testing.Examples
@@ -6,7 +8,7 @@ namespace Marten.Testing.Examples
     public class Load_by_Id
     {
         // SAMPLE: load_by_id
-        public void load_by_id(IDocumentSession session)
+        public void LoadById(IDocumentSession session)
         {
             var userId = Guid.NewGuid();
 
@@ -19,7 +21,7 @@ namespace Marten.Testing.Examples
             // Another overload for documents identified by strings
             var doc2 = session.Load<StringDoc>("Hank");
 
-            // Load multiple documents by a group of id's
+            // Load multiple documents by a group of ids
             var users = session.LoadMany<User>(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
 
             var ids = new Guid[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
@@ -27,7 +29,30 @@ namespace Marten.Testing.Examples
             // If you already have an array of id values
             var users2 = session.LoadMany<User>(ids);
         }
+        // ENDSAMPLE
 
+        // SAMPLE: async_load_by_id
+        public async Task LoadByIdAsync(IQuerySession session, CancellationToken token = default (CancellationToken))
+        {
+            var userId = Guid.NewGuid();
+
+            // Load a single document identified by a Guid
+            var user = await session.LoadAsync<User>(userId, token);
+
+            // There's an overload of Load for integers and longs
+            var doc = await session.LoadAsync<IntDoc>(15, token);
+
+            // Another overload for documents identified by strings
+            var doc2 = await session.LoadAsync<StringDoc>("Hank", token);
+
+            // Load multiple documents by a group of ids
+            var users = await session.LoadManyAsync<User>(token, Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+            var ids = new Guid[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+
+            // If you already have an array of id values
+            var users2 = await session.LoadManyAsync<User>(token, ids);
+        }
         // ENDSAMPLE
     }
 }
