f6adb1212f9ae6dfba735973042bf22cceb02df3
jeremydmiller <jeremydmiller@yahoo.com>
2021-03-05 08:44:07 -0600

    updates to blog samples

--- src/Marten.Testing/Events/Examples/Project.cs
+++ src/Marten.Testing/Events/Examples/Project.cs
@@ -31,7 +31,7 @@ namespace Marten.Testing.Events.Examples
     {
         public string Name { get; set; }
         public string[] Tasks { get; set; }
-        public Guid ProjectId { get; set; } = Guid.NewGuid();
+        public string ProjectId { get; set; }
     }
 
     public class NewProjectHandler
@@ -50,6 +50,7 @@ namespace Marten.Testing.Events.Examples
 
             var tasks = command.Tasks
                 .Select(name => new TaskRecorded {Timestamp = timestamp, Title = name});
+
 
             _session.Events.StartStream(command.ProjectId, started);
             _session.Events.Append(command.ProjectId, tasks);
@@ -168,9 +169,38 @@ namespace Marten.Testing.Events.Examples
         public DateTimeOffset Timestamp { get; set; }
     }
 
+    public class CreateTaskCommand
+    {
+        public string ProjectId { get; set; }
+        public string Title { get; set; }
+    }
+
+
+    public class CreateTaskHandler
+    {
+        private readonly IDocumentSession _session;
+
+        public CreateTaskHandler(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public Task Handle(CreateTaskCommand command)
+        {
+            var recorded = new TaskRecorded
+            {
+                Timestamp = DateTimeOffset.UtcNow,
+                Title = command.Title
+            };
+
+            _session.Events.Append(command.ProjectId, recorded);
+            return _session.SaveChangesAsync();
+        }
+    }
+
     public class CompleteTaskCommand
     {
-        public Guid ProjectId { get; set; }
+        public string ProjectId { get; set; }
         public int TaskNumber { get; set; }
 
         // This is the version of the project data
@@ -203,6 +233,64 @@ namespace Marten.Testing.Events.Examples
                 // the existing version of the project event
                 // stream as it commits
                 command.ExpectedVersion,
+                @event);
+            return _session.SaveChangesAsync();
+        }
+    }
+
+    public class CompleteTaskHandler2
+    {
+        private readonly IDocumentSession _session;
+
+        public CompleteTaskHandler2(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public Task Handle(CompleteTaskCommand command)
+        {
+            var @event = new TaskFinished
+            {
+                Number = command.TaskNumber,
+                Timestamp = DateTimeOffset.UtcNow
+            };
+
+            // If some other process magically zips
+            // in and updates this project event stream
+            // between the call to AppendOptimistic()
+            // and SaveChangesAsync(), Marten will detect
+            // that and reject the transaction
+            _session.Events.AppendOptimistic(
+                command.ProjectId,
+                @event);
+            return _session.SaveChangesAsync();
+        }
+    }
+
+    public class CompleteTaskHandler3
+    {
+        private readonly IDocumentSession _session;
+
+        public CompleteTaskHandler3(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public Task Handle(CompleteTaskCommand command)
+        {
+            var @event = new TaskFinished
+            {
+                Number = command.TaskNumber,
+                Timestamp = DateTimeOffset.UtcNow
+            };
+
+            // This tries to acquire an exclusive
+            // lock on the stream identified by
+            // command.ProjectId in the database
+            // so that only one process at a time
+            // can update this event stream
+            _session.Events.AppendExclusive(
+                command.ProjectId,
                 @event);
             return _session.SaveChangesAsync();
         }
