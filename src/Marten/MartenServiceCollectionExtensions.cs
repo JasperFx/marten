2e02fa5994439b3f5003dcf65bfda61f5806329f
mysticmind <babu.annamalai@gmail.com>
2021-09-16 20:54:30 +0530

    Fix for GH-1888 to ensure the passed logger gets used

--- src/Marten/MartenServiceCollectionExtensions.cs
+++ src/Marten/MartenServiceCollectionExtensions.cs
@@ -55,8 +55,9 @@ namespace Marten
 
             services.AddSingleton<IDocumentStore>(s =>
             {
-                var logger = s.GetService<ILogger<IDocumentStore>>() ?? new NullLogger<IDocumentStore>();
                 var options = s.GetRequiredService<StoreOptions>();
+                if (options.Logger().GetType() != typeof(NulloMartenLogger)) return new DocumentStore(options);
+                var logger = s.GetService<ILogger<IDocumentStore>>() ?? new NullLogger<IDocumentStore>();
                 options.Logger(new DefaultMartenLogger(logger));
 
                 return new DocumentStore(options);
