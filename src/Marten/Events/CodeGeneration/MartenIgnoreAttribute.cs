6d8826dcc143c73d1d9cbb38c480a8b163b94e6b
Will Hausman <will.jhausman@outlook.com>
2021-09-20 12:06:34 -0500

    Make MartenIgnore public

--- src/Marten/Events/CodeGeneration/MartenIgnoreAttribute.cs
+++ src/Marten/Events/CodeGeneration/MartenIgnoreAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace Marten.Events.CodeGeneration
 {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property)]
-    internal class MartenIgnoreAttribute: Attribute
+    public class MartenIgnoreAttribute: Attribute
     {
 
     }
