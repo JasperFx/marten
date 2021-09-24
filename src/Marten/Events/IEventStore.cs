d85c0199d27c27a561786de6915d33a77ce8e19c
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-09-24 20:25:00 +0200

    Added readonly event store methods to IQuerySession. Achieved that by Adding IQueryEventStore interface and QueryEventStore implementation

--- src/Marten/Events/IEventStore.cs
+++ src/Marten/Events/IEventStore.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Marten.Linq;
 #nullable enable
+
 namespace Marten.Events
 {
     public interface IEventOperations
@@ -220,7 +219,7 @@ namespace Marten.Events
         StreamAction StartStream(params object[] events);
     }
 
-    public interface IEventStore: IEventOperations
+    public interface IEventStore: IEventOperations, IQueryEventStore
     {
         /// <summary>
         /// Append one or more events in order to an existing stream and verify that maximum event id for the stream
@@ -239,167 +238,6 @@ namespace Marten.Events
         /// <param name="events"></param>
         /// <returns></returns>
         StreamAction StartStream<TAggregate>(Guid id, IEnumerable<object> events) where TAggregate : class;
-
-        /// <summary>
-        /// Synchronously fetches all of the events for the named stream
-        /// </summary>
-        /// <param name="streamId"></param>
-        /// <param name="version">If set, queries for events up to and including this version</param>
-        /// <param name="timestamp">If set, queries for events captured on or before this timestamp</param>
-        /// <returns></returns>
-        IReadOnlyList<IEvent> FetchStream(Guid streamId, long version = 0, DateTime? timestamp = null);
-
-        /// <summary>
-        /// Synchronously fetches all of the events for the named stream
-        /// </summary>
-        /// <param name="streamId"></param>
-        /// <param name="version">If set, queries for events up to and including this version</param>
-        /// <param name="timestamp">If set, queries for events captured on or before this timestamp</param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<IReadOnlyList<IEvent>> FetchStreamAsync(Guid streamId, long version = 0, DateTime? timestamp = null, CancellationToken token = default);
-
-        /// <summary>
-        /// Synchronously fetches all of the events for the named stream
-        /// </summary>
-        /// <param name="streamKey"></param>
-        /// <param name="version">If set, queries for events up to and including this version</param>
-        /// <param name="timestamp">If set, queries for events captured on or before this timestamp</param>
-        /// <returns></returns>
-        IReadOnlyList<IEvent> FetchStream(string streamKey, long version = 0, DateTime? timestamp = null);
-
-        /// <summary>
-        /// Synchronously fetches all of the events for the named stream
-        /// </summary>
-        /// <param name="streamKey"></param>
-        /// <param name="version">If set, queries for events up to and including this version</param>
-        /// <param name="timestamp">If set, queries for events captured on or before this timestamp</param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<IReadOnlyList<IEvent>> FetchStreamAsync(string streamKey, long version = 0, DateTime? timestamp = null, CancellationToken token = default);
-
-        /// <summary>
-        /// Perform a live aggregation of the raw events in this stream to a T object
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="streamId"></param>
-        /// <param name="version"></param>
-        /// <param name="timestamp"></param>
-        /// <param name="state">Instance of T to apply events to</param>
-        /// <returns></returns>
-        T? AggregateStream<T>(Guid streamId, long version = 0, DateTime? timestamp = null, T? state = null) where T : class;
-
-        /// <summary>
-        /// Perform a live aggregation of the raw events in this stream to a T object
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="streamId"></param>
-        /// <param name="version"></param>
-        /// <param name="timestamp"></param>
-        /// <param name="state">Instance of T to apply events to</param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<T?> AggregateStreamAsync<T>(Guid streamId, long version = 0, DateTime? timestamp = null, T? state = null, CancellationToken token = default) where T : class;
-
-        /// <summary>
-        /// Perform a live aggregation of the raw events in this stream to a T object
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="streamKey"></param>
-        /// <param name="version"></param>
-        /// <param name="timestamp"></param>
-        /// <param name="state">Instance of T to apply events to</param>
-        /// <returns></returns>
-        T? AggregateStream<T>(string streamKey, long version = 0, DateTime? timestamp = null, T? state = null) where T : class;
-
-        /// <summary>
-        /// Perform a live aggregation of the raw events in this stream to a T object
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="streamKey"></param>
-        /// <param name="version"></param>
-        /// <param name="timestamp"></param>
-        /// <param name="state">Instance of T to apply events to</param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<T?> AggregateStreamAsync<T>(string streamKey, long version = 0, DateTime? timestamp = null, T? state = null, CancellationToken token = default) where T : class;
-
-        /// <summary>
-        /// Query directly against ONLY the raw event data. Use IQuerySession.Query() for aggregated documents!
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        IMartenQueryable<T> QueryRawEventDataOnly<T>();
-
-        /// <summary>
-        /// Query directly against the raw event data across all event types
-        /// </summary>
-        /// <returns></returns>
-        IMartenQueryable<IEvent> QueryAllRawEvents();
-
-        /// <summary>
-        /// Load a single event by its id knowing the event type upfront
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        IEvent<T> Load<T>(Guid id) where T : class;
-
-        /// <summary>
-        /// Load a single event by its id knowing the event type upfront
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="id"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<IEvent<T>> LoadAsync<T>(Guid id, CancellationToken token = default) where T : class;
-
-        /// <summary>
-        /// Load a single event by its id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        IEvent Load(Guid id);
-
-        /// <summary>
-        /// Load a single event by its id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<IEvent> LoadAsync(Guid id, CancellationToken token = default);
-
-        /// <summary>
-        /// Fetches only the metadata about a stream by id
-        /// </summary>
-        /// <param name="streamId"></param>
-        /// <returns></returns>
-        StreamState FetchStreamState(Guid streamId);
-
-        /// <summary>
-        /// Fetches only the metadata about a stream by id
-        /// </summary>
-        /// <param name="streamId"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<StreamState> FetchStreamStateAsync(Guid streamId, CancellationToken token = default);
-
-        /// <summary>
-        /// Fetches only the metadata about a stream by id
-        /// </summary>
-        /// <param name="streamKey"></param>
-        /// <returns></returns>
-        StreamState FetchStreamState(string streamKey);
-
-        /// <summary>
-        /// Fetches only the metadata about a stream by id
-        /// </summary>
-        /// <param name="streamKey"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        Task<StreamState> FetchStreamStateAsync(string streamKey, CancellationToken token = default);
-
-
 
         /// <summary>
         /// Append events to an existing stream with optimistic concurrency checks against the
---
+++ src/Marten/Events/QueryEventStore.cs
@@ -0,0 +1,235 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Baseline;
+using Marten.Events.Archiving;
+using Marten.Events.Querying;
+using Marten.Exceptions;
+using Marten.Internal.Sessions;
+using Marten.Internal.Storage;
+using Marten.Linq;
+using Marten.Linq.QueryHandlers;
+using Weasel.Postgresql;
+using Marten.Schema.Identity;
+using Marten.Storage;
+using Marten.Util;
+using Npgsql;
+using Weasel.Core;
+
+#nullable enable
+namespace Marten.Events
+{
+    internal class QueryEventStore: IQueryEventStore
+    {
+        private readonly QuerySession _session;
+        private readonly ITenant _tenant;
+        private readonly DocumentStore _store;
+
+        public QueryEventStore(QuerySession session, DocumentStore store, ITenant tenant)
+        {
+            _session = session;
+            _store = store;
+            _tenant = tenant;
+        }
+
+        public IReadOnlyList<IEvent> FetchStream(Guid streamId, long version = 0, DateTime? timestamp = null)
+        {
+            var selector = _store.Events.EnsureAsGuidStorage(_session);
+
+            var statement = new EventStatement(selector)
+            {
+                StreamId = streamId, Version = version, Timestamp = timestamp, TenantId = _tenant.TenantId
+            };
+
+            IQueryHandler<IReadOnlyList<IEvent>> handler = new ListQueryHandler<IEvent>(statement, selector);
+
+            return _session.ExecuteHandler(handler);
+        }
+
+        public Task<IReadOnlyList<IEvent>> FetchStreamAsync(Guid streamId, long version = 0, DateTime? timestamp = null, CancellationToken token = default)
+        {
+            var selector = _store.Events.EnsureAsGuidStorage(_session);
+
+            var statement = new EventStatement(selector)
+            {
+                StreamId = streamId, Version = version, Timestamp = timestamp, TenantId = _tenant.TenantId
+            };
+
+            IQueryHandler<IReadOnlyList<IEvent>> handler = new ListQueryHandler<IEvent>(statement, selector);
+
+            return _session.ExecuteHandlerAsync(handler, token);
+        }
+
+        public IReadOnlyList<IEvent> FetchStream(string streamKey, long version = 0, DateTime? timestamp = null)
+        {
+            var selector = _store.Events.EnsureAsStringStorage(_session);
+
+            var statement = new EventStatement(selector)
+            {
+                StreamKey = streamKey, Version = version, Timestamp = timestamp, TenantId = _tenant.TenantId
+            };
+
+            IQueryHandler<IReadOnlyList<IEvent>> handler = new ListQueryHandler<IEvent>(statement, selector);
+
+            return _session.ExecuteHandler(handler);
+        }
+
+        public Task<IReadOnlyList<IEvent>> FetchStreamAsync(string streamKey, long version = 0, DateTime? timestamp = null, CancellationToken token = default)
+        {
+            var selector = _store.Events.EnsureAsStringStorage(_session);
+
+            var statement = new EventStatement(selector)
+            {
+                StreamKey = streamKey, Version = version, Timestamp = timestamp, TenantId = _tenant.TenantId
+            };
+
+            IQueryHandler<IReadOnlyList<IEvent>> handler = new ListQueryHandler<IEvent>(statement, selector);
+
+            return _session.ExecuteHandlerAsync(handler, token);
+        }
+
+        public T? AggregateStream<T>(Guid streamId, long version = 0, DateTime? timestamp = null, T? state = null) where T : class
+        {
+            var events = FetchStream(streamId, version, timestamp);
+
+            var aggregator = _store.Options.Projections.AggregatorFor<T>();
+
+            if (!events.Any()) return null;
+
+            var aggregate = aggregator.Build(events, _session, state);
+
+            var storage = _session.StorageFor<T>();
+            if (storage is IDocumentStorage<T, Guid> s) s.SetIdentity(aggregate, streamId);
+
+            return aggregate;
+        }
+
+        public async Task<T?> AggregateStreamAsync<T>(Guid streamId, long version = 0, DateTime? timestamp = null,
+            T? state = null, CancellationToken token = default) where T : class
+        {
+            var events = await FetchStreamAsync(streamId, version, timestamp, token);
+            if (!events.Any()) return null;
+
+            var aggregator = _store.Options.Projections.AggregatorFor<T>();
+            var aggregate = await aggregator.BuildAsync(events, _session, state, token);
+
+            if (aggregate == null) return null;
+
+            var storage = _session.StorageFor<T>();
+            if (storage is IDocumentStorage<T, Guid> s) s.SetIdentity(aggregate, streamId);
+
+            return aggregate;
+        }
+
+        public T? AggregateStream<T>(string streamKey, long version = 0, DateTime? timestamp = null, T? state = null) where T : class
+        {
+            var events = FetchStream(streamKey, version, timestamp);
+            if (!events.Any())
+            {
+                return null;
+            }
+
+            var aggregator = _store.Options.Projections.AggregatorFor<T>();
+            var aggregate = aggregator.Build(events, _session, state);
+
+            var storage = _session.StorageFor<T>();
+            if (storage is IDocumentStorage<T, string> s) s.SetIdentity(aggregate, streamKey);
+
+            return aggregate;
+        }
+
+        public async Task<T?> AggregateStreamAsync<T>(string streamKey, long version = 0, DateTime? timestamp = null,
+            T? state = null, CancellationToken token = default) where T : class
+        {
+            var events = await FetchStreamAsync(streamKey, version, timestamp, token);
+            if (!events.Any())
+            {
+                return null;
+            }
+
+            var aggregator = _store.Options.Projections.AggregatorFor<T>();
+
+            var aggregate = await aggregator.BuildAsync(events, _session, state, token);
+
+            var storage = _session.StorageFor<T>();
+            if (storage is IDocumentStorage<T, string> s) s.SetIdentity(aggregate, streamKey);
+
+            return aggregate;
+        }
+
+        public IMartenQueryable<T> QueryRawEventDataOnly<T>()
+        {
+            _tenant.EnsureStorageExists(typeof(StreamAction));
+
+            _store.Events.AddEventType(typeof(T));
+
+            return _session.Query<T>();
+        }
+
+        public IMartenQueryable<IEvent> QueryAllRawEvents()
+        {
+            _tenant.EnsureStorageExists(typeof(StreamAction));
+
+            return _session.Query<IEvent>();
+        }
+
+        public IEvent<T> Load<T>(Guid id) where T : class
+        {
+            _tenant.EnsureStorageExists(typeof(StreamAction));
+
+            _store.Events.AddEventType(typeof(T));
+
+            return Load(id).As<Event<T>>();
+        }
+
+        public async Task<IEvent<T>> LoadAsync<T>(Guid id, CancellationToken token = default) where T : class
+        {
+            await _tenant.EnsureStorageExistsAsync(typeof(StreamAction), token);
+
+            _store.Events.AddEventType(typeof(T));
+
+            return (await LoadAsync(id, token)).As<Event<T>>();
+        }
+
+        public IEvent Load(Guid id)
+        {
+            var handler = new SingleEventQueryHandler(id, _session.EventStorage());
+            return _session.ExecuteHandler(handler);
+        }
+
+        public Task<IEvent> LoadAsync(Guid id, CancellationToken token = default)
+        {
+            _tenant.EnsureStorageExists(typeof(StreamAction));
+
+            var handler = new SingleEventQueryHandler(id, _session.EventStorage());
+            return _session.ExecuteHandlerAsync(handler, token);
+        }
+
+        public StreamState FetchStreamState(Guid streamId)
+        {
+            var handler = _tenant.EventStorage().QueryForStream(StreamAction.ForReference(streamId, _tenant));
+            return _session.ExecuteHandler(handler);
+        }
+
+        public Task<StreamState> FetchStreamStateAsync(Guid streamId, CancellationToken token = default)
+        {
+            var handler = _tenant.EventStorage().QueryForStream(StreamAction.ForReference(streamId, _tenant));
+            return _session.ExecuteHandlerAsync(handler, token);
+        }
+
+        public StreamState FetchStreamState(string streamKey)
+        {
+            var handler = _tenant.EventStorage().QueryForStream(StreamAction.ForReference(streamKey, _tenant));
+            return _session.ExecuteHandler(handler);
+        }
+
+        public Task<StreamState> FetchStreamStateAsync(string streamKey, CancellationToken token = default)
+        {
+            var handler = _tenant.EventStorage().QueryForStream(StreamAction.ForReference(streamKey, _tenant));
+            return _session.ExecuteHandlerAsync(handler, token);
+        }
+    }
+}
