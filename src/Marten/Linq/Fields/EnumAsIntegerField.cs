c5c62460f9413193b4758c6131391fdc3d40cf9e
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2021-10-27 10:16:15 -0500

    Fix for querying against enums that are deeply nested. Closes GH-1951

--- src/Marten/Linq/Fields/EnumAsIntegerField.cs
+++ src/Marten/Linq/Fields/EnumAsIntegerField.cs
@@ -13,7 +13,7 @@ namespace Marten.Linq.Fields
         public EnumAsIntegerField(string dataLocator, Casing casing, MemberInfo[] members) : base(dataLocator, "integer", casing, members)
         {
             PgType = "integer";
-            TypedLocator = $"CAST({dataLocator} ->> '{lastMemberName}' as {PgType})";
+            TypedLocator = $"CAST({RawLocator} as {PgType})";
         }
 
         public override string SelectorForDuplication(string pgType)
