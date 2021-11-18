dc67846111e05d62ea15737dba24f390fe1fac26
Alexander Langer <alexander.langer@mestus.de>
2021-11-18 17:23:31 +0100

    Add testcase for #1984

---
+++ src/Marten.Testing.OtherAssembly/Bug1984/GenericEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Marten.Testing.OtherAssembly.Bug1984
+{
+    public class GenericEntity<T>
+    {
+        public Guid Id { get; set; }
+        public T Data { get; set; }
+    }
+}
