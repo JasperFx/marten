2d15c66643b49001e07d542480a51c8d291ea349
Alexander Langer <alexander.langer@mestus.de>
2021-12-21 11:07:02 +0100

    Test case for #2018: Full Text Search on string list

---
+++ src/Marten.Testing/Bugs/Bug_2018_fts_string_list.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten.Testing.Harness;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_2018_fts_string_list: BugIntegrationContext
+    {
+        public sealed class BugFullTextSearchFields
+        {
+            public Guid Id { get; set; }
+            public string Text { get; set; }
+            public List<string> Data { get; set; }
+        }
+
+        public Bug_2018_fts_string_list()
+        {
+            StoreOptions(_ => _.Schema
+                .For<BugFullTextSearchFields>()
+                .GinIndexJsonData()
+                .FullTextIndex(index =>
+                    {
+                        index.Name = "mt_custom_my_index_name_fulltext_search";
+                        index.RegConfig = "english";
+                    },
+                    x => x.Text,
+                    x => x.Data)
+                .UseOptimisticConcurrency(true));
+        }
+
+        [PgVersionTargetedFact(MinimumVersion = "13.0")]
+        public void can_do_index_with_full_text_search()
+        {
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(new BugFullTextSearchFields()
+                {
+                    Id = Guid.NewGuid(),
+                    Text = "Hello my Darling, this is a long text",
+                    Data = new List<string>()
+                    {
+                        "Foo",
+                        "VeryLongEntry",
+                        "Baz"
+                    },
+                });
+
+                session.SaveChanges();
+            }
+        }
+    }
+}
