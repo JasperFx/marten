c18051ac855042f10c77f4ac9329df8abb8365fa
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-03-02 11:21:15 -0600

    Added support for the Weasel environment check for asserting database configuration. Closes GH-525

---
+++ src/Marten.CommandLine/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+using Weasel.CommandLine;
+
+namespace Marten.CommandLine
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add Oakton environment checks to assert that all the configured database configuration is up to date
+        /// See https://jasperfx.github.io/oakton/documentation/hostbuilder/environment/ for more information
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static MartenServiceCollectionExtensions.MartenConfigurationExpression AddEnvironmentChecks(this MartenServiceCollectionExtensions.MartenConfigurationExpression expression)
+        {
+            expression.Services.CheckAllWeaselDatabases();
+            return expression;
+        }
+    }
+}
