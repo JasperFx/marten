088136dea8d51c928906816f399ddc597bc5cd8d
mysticmind <babu.annamalai@gmail.com>
2022-03-04 13:25:34 +0530

    Fix GH-1802
    - Fix to not escape backslash when using case insensitive string search
    - Add unit test

--- src/DocumentDbTests/Reading/Linq/InvariantCultureIgnoreCase_filtering.cs
+++ src/DocumentDbTests/Reading/Linq/InvariantCultureIgnoreCase_filtering.cs
@@ -26,6 +26,23 @@ namespace DocumentDbTests.Reading.Linq
             }
         }
 
+        [Fact]
+        public void can_search_string_with_back_slash_case_insensitive()
+        {
+            var user = new User {UserName = @"DOMAIN\TEST_USER"};
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(user);
+                session.SaveChanges();
+            }
+
+            using (var query = theStore.QuerySession())
+            {
+                query.Query<User>().Single(x => x.UserName.Equals(@"domain\test_user", StringComparison.InvariantCultureIgnoreCase)).Id.ShouldBe(user.Id);
+            }
+        }
+
         public InvariantCultureIgnoreCase_filtering(DefaultStoreFixture fixture) : base(fixture)
         {
         }
