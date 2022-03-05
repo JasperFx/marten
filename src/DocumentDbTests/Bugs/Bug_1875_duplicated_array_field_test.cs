890dfaa49cb8cfe6c5005f32cad3792100ba5ea9
mysticmind <babu.annamalai@gmail.com>
2022-03-05 16:53:14 +0530

    Add another unit test to test duplicated field guid array

--- src/DocumentDbTests/Bugs/Bug_1875_duplicated_array_field_test.cs
+++ src/DocumentDbTests/Bugs/Bug_1875_duplicated_array_field_test.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Marten.Services;
@@ -13,7 +14,7 @@ namespace DocumentDbTests.Bugs
     public class Bug_1875_duplicated_array_field_test : BugIntegrationContext
     {
         [Fact]
-        public void query_on_duplicated_array_field_test()
+        public void query_on_duplicated_number_array_field_test()
         {
             StoreOptions(_ =>
             {
@@ -34,6 +35,29 @@ namespace DocumentDbTests.Bugs
             {
                 session.Query<Target>().Single(x => x.NumberArray.Contains(1))
                     .NumberArray[0].ShouldBe(1);
+            }
+        }
+
+        [Fact]
+        public void query_on_duplicated_guid_array_field_test()
+        {
+            StoreOptions(_ =>
+            {
+                _.Schema.For<Target>().Duplicate(t => t.GuidArray, "uuid[]");
+            });
+
+            var target = new Target {GuidArray = new Guid[] {Guid.NewGuid(), Guid.NewGuid()}};
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(target);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Query<Target>().Single(x => x.GuidArray.Contains(target.GuidArray[0]))
+                    .GuidArray[0].ShouldBe(target.GuidArray[0]);
             }
         }
     }
