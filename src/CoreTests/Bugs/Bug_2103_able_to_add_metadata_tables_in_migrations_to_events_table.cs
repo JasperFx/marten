869de46260352b00f5da1fe94984761f00e48e28
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-03-09 08:30:37 -0600

    extra test command to exercise appending events

---
+++ src/CoreTests/Bugs/Bug_2103_able_to_add_metadata_tables_in_migrations_to_events_table.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using Baseline;
+using Baseline.ImTools;
+using CoreTests.Diagnostics;
+using Marten;
+using Marten.Events;
+using Marten.Testing.Harness;
+using Weasel.Core;
+using Xunit;
+
+namespace CoreTests.Bugs
+{
+    public class Bug_2103_able_to_add_metadata_tables_in_migrations_to_events_table : BugIntegrationContext
+    {
+        [Fact]
+        public async Task should_add_new_metadata_table()
+        {
+            StoreOptions(opts =>
+            {
+                opts.Events.StreamIdentity = StreamIdentity.AsString;
+                opts.AutoCreateSchemaObjects = AutoCreate.None;
+                opts.Events.AddEventType(typeof(AEvent));
+            }, true);
+
+            await theStore.As<IDocumentStore>().Storage.ApplyAllConfiguredChangesToDatabaseAsync();
+
+            StoreOptions(opts =>
+            {
+                opts.Events.StreamIdentity = StreamIdentity.AsString;
+                opts.AutoCreateSchemaObjects = AutoCreate.None;
+                opts.Events.AddEventType(typeof(AEvent));
+                opts.Events.MetadataConfig.HeadersEnabled = true;
+            }, false);
+
+            var martenStorage = theStore.As<IDocumentStore>().Storage;
+            await martenStorage.ApplyAllConfiguredChangesToDatabaseAsync();
+            await martenStorage.Database.AssertDatabaseMatchesConfigurationAsync();
+
+            theSession.Events.Append(Guid.NewGuid().ToString(), new QuestStarted());
+            await theSession.SaveChangesAsync();
+
+        }
+    }
+}
---
+++ src/EventStoreMigrations/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Diagnostics;
+using LamarCodeGeneration;
+using Marten;
+using Marten.Events;
+using Marten.Events.Daemon.Resiliency;
+using Marten.Events.Projections;
+using Marten.Testing.Harness;
+using Microsoft.Extensions.Hosting;
+using Oakton;
+using Weasel.Core;
+
+namespace EventStoreMigrations
+{
+    public class Program
+    {
+        public static Task<int> Main(string[] args)
+        {
+            return CreateHostBuilder(args).RunOaktonCommands(args);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+
+                    services.AddMarten(opts =>
+                    {
+                        opts.AutoCreateSchemaObjects = AutoCreate.None;
+                        opts.DatabaseSchemaName = "cli";
+                        opts.Connection(ConnectionSource.ConnectionString);
+
+                        opts.GeneratedCodeMode = TypeLoadMode.Dynamic;
+
+                        opts.Events.StreamIdentity = StreamIdentity.AsString;
+                        opts.Events.MetadataConfig.HeadersEnabled = true;
+                        opts.Events.AddEventType(typeof(Started));
+                    });
+                });
+        }
+    }
+
+    public class Started{}
+
+
+}
