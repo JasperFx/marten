cd08a335a5f19db16af4500269762fe7472b9053
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-03-09 08:30:37 -0600

    extra test command to exercise appending events

---
+++ src/EventStoreMigrations/TestCommand.cs
@@ -0,0 +1,19 @@
+using Marten;
+using Microsoft.Extensions.DependencyInjection;
+using Oakton;
+
+namespace EventStoreMigrations;
+
+public class TestCommand : OaktonAsyncCommand<NetCoreInput>
+{
+    public override async Task<bool> Execute(NetCoreInput input)
+    {
+        using var host = input.BuildHost();
+        var store = host.Services.GetRequiredService<IDocumentStore>();
+        using var session = store.LightweightSession();
+        session.Events.StartStream(Guid.NewGuid().ToString(), new Started());
+        await session.SaveChangesAsync();
+
+        return true;
+    }
+}
