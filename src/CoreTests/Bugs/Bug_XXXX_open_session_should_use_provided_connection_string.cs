e5f18f5cec1883d1bc9a29231764e7e3d28dc193
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-03-18 08:58:34 -0500

    extra test asserts for user supplied connection strings. Closes GH-2134. Closes GH-2136

--- src/CoreTests/Bugs/Bug_XXXX_open_session_should_use_provided_connection_string.cs
+++ src/CoreTests/Bugs/Bug_XXXX_open_session_should_use_provided_connection_string.cs
@@ -6,14 +6,14 @@ using BugXXXX;
 using Marten;
 using Marten.Services;
 using Marten.Testing.Harness;
-
+using Npgsql;
 using Shouldly;
 
 using Weasel.Core;
 
 using Xunit;
 
-namespace DocumentDbTests.Bugs
+namespace CoreTests.Bugs
 {
     public class Bug_XXXX_open_session_should_use_provided_connection_string: BugIntegrationContext
     {
@@ -43,20 +43,32 @@ namespace DocumentDbTests.Bugs
                 x.Connection(ThrowAlways);
             });
 
-            await using (var session = await testStore.OpenSessionAsync(SessionOptions.ForConnectionString(ConnectionSource.ConnectionString)))
+            var builder = new NpgsqlConnectionStringBuilder(ConnectionSource.ConnectionString);
+            builder.Timeout = 11;
+            var connectionString = builder.ConnectionString;
+
+
+
+            await using (var session = await testStore.OpenSessionAsync(SessionOptions.ForConnectionString(connectionString)))
             {
+                new NpgsqlConnectionStringBuilder(session.Connection.ConnectionString).Timeout.ShouldBe(11);
                 session.Store(new TestEntity { Name = "Test 2" });
                 await session.SaveChangesAsync();
+
             }
 
-            await using (var session = await testStore.OpenSessionAsync(SessionOptions.ForConnectionString(ConnectionSource.ConnectionString)))
+            await using (var session = await testStore.OpenSessionAsync(SessionOptions.ForConnectionString(connectionString)))
             {
+                new NpgsqlConnectionStringBuilder(session.Connection.ConnectionString).Timeout.ShouldBe(11);
                 var entities = await session.Query<TestEntity>()
                     .ToListAsync();
 
                 entities.Count.ShouldBe(2);
+
             }
         }
+
+
     }
 }
 
