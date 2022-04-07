6b7f306debf16e089aae191189cf6f40add1f6c3
Michael Lehto <michael@dinero.dk>
2022-04-07 09:41:15 +0200

    Add failing test for append called several times for string identity

--- src/DocumentDbTests/Bugs/Bug_673_multiple_version_assertions.cs
+++ src/DocumentDbTests/Bugs/Bug_673_multiple_version_assertions.cs
@@ -1,4 +1,5 @@
 using System;
+using Marten.Events;
 using Marten.Testing.Harness;
 using Xunit;
 
@@ -10,6 +11,32 @@ namespace DocumentDbTests.Bugs
         public void replaces_the_max_version_assertion()
         {
             var streamId = Guid.NewGuid();
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Events.Append(streamId, new WhateverEvent(), new WhateverEvent());
+
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.OpenSession())
+            {
+                var state = session.Events.FetchStreamState(streamId);
+                // ... do some stuff
+                var expectedVersion = state.Version + 1;
+                session.Events.Append(streamId, expectedVersion, new WhateverEvent());
+                // ... do some more stuff
+                expectedVersion += 1;
+                session.Events.Append(streamId, expectedVersion, new WhateverEvent());
+                session.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void replaces_the_max_version_assertion_for_string_identity()
+        {
+            UseStreamIdentity(StreamIdentity.AsString);
+            var streamId = Guid.NewGuid().ToString();
 
             using (var session = theStore.OpenSession())
             {
