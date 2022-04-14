477b56dce6c0630c080feb601e68c6d2780189fe
mysticmind <babu.annamalai@gmail.com>
2022-04-14 12:26:35 +0530

    Fix GH-2177 to set tenancy properly for query session for async projections

---
+++ src/Marten.AsyncDaemon.Testing/Bugs/Bug_2177_query_session_tenancy_in_daemon.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Baseline.Dates;
+using Marten.Events;
+using Marten.Events.Aggregation;
+using Marten.Events.Projections;
+using Marten.Storage;
+using Marten.Testing.Harness;
+using Xunit;
+using Bug2177;
+using LamarCodeGeneration;
+using Marten;
+
+namespace Marten.AsyncDaemon.Testing.Bugs
+{
+    public class Bug_2177_query_session_tenancy_in_daemon : BugIntegrationContext
+    {
+        [Fact]
+        public async Task should_have_tenancy_set_correctly()
+        {
+            using var documentStore = SeparateStore(options =>
+            {
+                options.Policies.AllDocumentsAreMultiTenanted();
+                options.Events.TenancyStyle = TenancyStyle.Conjoined;
+                options.Events.StreamIdentity = StreamIdentity.AsGuid;
+                options.Events.MetadataConfig.EnableAll();
+
+                options.Projections.Add<TicketProjection>(ProjectionLifecycle.Async);
+                options.GeneratedCodeMode = TypeLoadMode.Auto;
+            });
+
+            using var daemon = await documentStore.BuildProjectionDaemonAsync();
+            await daemon.StartAllShards();
+
+            var tenantId = Guid.NewGuid().ToString();
+            var userId = Guid.NewGuid();
+            var ticketId = Guid.NewGuid();
+
+            await using var session = documentStore.OpenSession(tenantId);
+            session.Insert(new User { Id = userId, FirstName = "Tester", LastName = "McTestFace" });
+            await session.SaveChangesAsync();
+
+            session.Events.Append(ticketId, new TicketCreated(ticketId, "Test Projections"), new TicketAssigned(ticketId, userId));
+            await session.SaveChangesAsync();
+
+            await daemon.WaitForNonStaleData(1.Minutes());
+
+            var projection = await session.LoadAsync<Ticket>(ticketId);
+            projection?.User.ShouldNotBeNull();
+        }
+    }
+}
+
+namespace Bug2177
+{
+    public record TicketCreated(Guid TicketId, string Name);
+
+    public record TicketAssigned(Guid TicketId, Guid UserId);
+
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public User User { get; set; }
+    }
+
+    public class TicketProjection: AggregateProjection<Ticket>
+    {
+        public TicketProjection()
+        {
+            Lifecycle = ProjectionLifecycle.Async;
+        }
+
+        public Ticket Create(TicketCreated created) =>
+            new() { Id = created.TicketId, Name = created.Name };
+
+        public void Apply(Ticket ticket, TicketAssigned assigned, IQuerySession session) {
+            ticket.User = session.Load<User>(assigned.UserId);
+        }
+
+    }
+}
