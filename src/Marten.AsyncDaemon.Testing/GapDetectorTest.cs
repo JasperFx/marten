6b2e44e549861117e433346659ca21106765a8f9
Michael Lehto <michael@dinero.dk>
2022-04-26 11:34:28 +0200

    Add tests for gap detector and fix missing detection for gap right after start

---
+++ src/Marten.AsyncDaemon.Testing/GapDetectorTest.cs
@@ -0,0 +1,89 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Marten.AsyncDaemon.Testing.TestingSupport;
+using Marten.Events;
+using Marten.Events.Daemon.HighWater;
+using Marten.Services;
+using Marten.Testing;
+using NpgsqlTypes;
+using Shouldly;
+using Weasel.Postgresql;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Marten.AsyncDaemon.Testing
+{
+    public class GapDetectorTest: DaemonContext
+    {
+        private readonly GapDetector _gapDetector;
+        private readonly ISingleQueryRunner _runner;
+
+        public GapDetectorTest(ITestOutputHelper output) : base(output)
+        {
+            theStore.EnsureStorageExists(typeof(IEvent));
+
+            _gapDetector = new GapDetector(theStore.Events);
+            _runner = new AutoOpenSingleQueryRunner(theStore.Tenancy.Default.Database);
+        }
+
+        [Fact]
+        public async Task detect_first_gap()
+        {
+            NumberOfStreams = 10;
+            await PublishSingleThreaded();
+            await deleteEvents(NumberOfEvents - 100, NumberOfEvents - 50);
+
+            var current = await _runner.Query(_gapDetector, CancellationToken.None).ConfigureAwait(false);
+
+            current.ShouldBe(NumberOfEvents - 101);
+        }
+
+        [Fact]
+        public async Task detect_gap_if_gap_is_right_after_start()
+        {
+            NumberOfStreams = 10;
+            await PublishSingleThreaded();
+            await deleteEvents(NumberOfEvents - 100, NumberOfEvents - 50);
+            var current = await _runner.Query(_gapDetector, CancellationToken.None).ConfigureAwait(false);
+            _gapDetector.Start = current.Value;
+
+            current = await _runner.Query(_gapDetector, CancellationToken.None).ConfigureAwait(false);
+
+            current.ShouldBe(NumberOfEvents - 101);
+        }
+
+        [Fact]
+        public async Task get_max_seq_id_if_no_gap()
+        {
+            NumberOfStreams = 10;
+            await PublishSingleThreaded();
+
+            var current = await _runner.Query(_gapDetector, CancellationToken.None).ConfigureAwait(false);
+
+            current.ShouldBe(NumberOfEvents);
+        }
+
+        [Fact]
+        public async Task get_max_seq_id_if_start_is_max_seq_id()
+        {
+            NumberOfStreams = 10;
+            await PublishSingleThreaded();
+            _gapDetector.Start = NumberOfEvents;
+
+            var current = await _runner.Query(_gapDetector, CancellationToken.None).ConfigureAwait(false);
+
+            current.ShouldBe(NumberOfEvents);
+        }
+
+        protected async Task deleteEvents(params long[] ids)
+        {
+            using var conn = theStore.CreateConnection();
+            await conn.OpenAsync();
+
+            await conn
+                .CreateCommand($"delete from {theStore.Events.DatabaseSchemaName}.mt_events where seq_id = ANY(:ids)")
+                .With("ids", ids, NpgsqlDbType.Bigint | NpgsqlDbType.Array)
+                .ExecuteNonQueryAsync();
+        }
+    }
+}
