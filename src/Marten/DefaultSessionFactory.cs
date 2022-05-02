5fd6fc67f20c73de11af13022afd915a055e24cd
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-05-02 11:09:11 -0500

    Marking InitializeStore() as obsolete. Closes GH-2185

---
+++ src/Marten/DefaultSessionFactory.cs
@@ -0,0 +1,22 @@
+namespace Marten
+{
+    internal class DefaultSessionFactory: ISessionFactory
+    {
+        private readonly IDocumentStore _store;
+
+        public DefaultSessionFactory(IDocumentStore store)
+        {
+            _store = store;
+        }
+
+        public IQuerySession QuerySession()
+        {
+            return _store.QuerySession();
+        }
+
+        public IDocumentSession OpenSession()
+        {
+            return _store.OpenSession();
+        }
+    }
+}
