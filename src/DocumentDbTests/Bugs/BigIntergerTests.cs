579bbb62900563a27aa7d6ba3d5c3d41366992ad
Phillip <phillip.haydon@gmail.com>
2022-05-05 19:40:10 +0800

    added failing test for BigInteger for STJ and passing test for Newtonsoft

---
+++ src/DocumentDbTests/Bugs/BigIntergerTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Numerics;
+using Marten.Services.Json;
+using Marten.Testing.Harness;
+using Shouldly;
+using Xunit;
+
+namespace DocumentDbTests.Bugs
+{
+    public class BigIntegerTests : BugIntegrationContext
+    {
+        [Fact]
+        public void When_Querying_Using_Newtonsoft_Json_Should_Persist_And_Fetch_BigInteger_Values()
+        {
+            StoreOptions(options =>
+            {
+                options.UseDefaultSerialization(serializerType: SerializerType.Newtonsoft);
+
+                options.Schema.For<BigIntegerObject>()
+                    .Duplicate(x => x.Value);
+            });
+
+            using (var session = theStore.OpenSession())
+            {
+                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse("123456789012345678901234567890123456789012345678901234567890") };
+
+                session.Store(obj);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.OpenSession())
+            {
+                var result = session.Load<BigIntegerObject>(1);
+
+                result.Value.ToString().ShouldBe("123456789012345678901234567890123456789012345678901234567890");
+            }
+        }
+
+        [Fact]
+        public void When_Querying_Using_SystemTextJson_Should_Persist_And_Fetch_BigInteger_Values()
+        {
+            StoreOptions(options =>
+            {
+                options.UseDefaultSerialization(serializerType: SerializerType.SystemTextJson);
+
+                options.Schema.For<BigIntegerObject>()
+                    .Duplicate(x => x.Value);
+            });
+
+            using (var session = theStore.OpenSession())
+            {
+                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse("123456789012345678901234567890123456789012345678901234567890") };
+
+                session.Store(obj);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.OpenSession())
+            {
+                var result = session.Load<BigIntegerObject>(1);
+
+                result.Value.ToString().ShouldBe("123456789012345678901234567890123456789012345678901234567890");
+            }
+        }
+    }
+
+    public class BigIntegerObject
+    {
+        public int Id { get; set; }
+
+        public BigInteger Value { get; set; }
+    }
+}
