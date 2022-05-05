8aa37b91a5f3919f5cf6553a78ac564bd43ff62a
Phillip <phillip.haydon@gmail.com>
2022-05-05 19:50:35 +0800

    added test to persist and fetch with different serializers

--- src/DocumentDbTests/Bugs/BigIntergerTests.cs
+++ src/DocumentDbTests/Bugs/BigIntergerTests.cs
@@ -8,6 +8,8 @@ namespace DocumentDbTests.Bugs
 {
     public class BigIntegerTests : BugIntegrationContext
     {
+        private static readonly string LargerThanLongValue = "123456789012345678901234567890123456789012345678901234567890";
+
         [Fact]
         public void When_Querying_Using_Newtonsoft_Json_Should_Persist_And_Fetch_BigInteger_Values()
         {
@@ -21,7 +23,7 @@ namespace DocumentDbTests.Bugs
 
             using (var session = theStore.OpenSession())
             {
-                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse("123456789012345678901234567890123456789012345678901234567890") };
+                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse(LargerThanLongValue) };
 
                 session.Store(obj);
                 session.SaveChanges();
@@ -31,7 +33,7 @@ namespace DocumentDbTests.Bugs
             {
                 var result = session.Load<BigIntegerObject>(1);
 
-                result.Value.ToString().ShouldBe("123456789012345678901234567890123456789012345678901234567890");
+                result.Value.ToString().ShouldBe(LargerThanLongValue);
             }
         }
 
@@ -48,7 +50,7 @@ namespace DocumentDbTests.Bugs
 
             using (var session = theStore.OpenSession())
             {
-                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse("123456789012345678901234567890123456789012345678901234567890") };
+                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse(LargerThanLongValue) };
 
                 session.Store(obj);
                 session.SaveChanges();
@@ -58,7 +60,77 @@ namespace DocumentDbTests.Bugs
             {
                 var result = session.Load<BigIntegerObject>(1);
 
-                result.Value.ToString().ShouldBe("123456789012345678901234567890123456789012345678901234567890");
+                result.Value.ToString().ShouldBe(LargerThanLongValue);
+            }
+        }
+
+        [Fact]
+        public void When_Stored_With_Newtonsoft_And_Fetched_With_STJ_Should_Succeed()
+        {
+            StoreOptions(options =>
+            {
+                options.UseDefaultSerialization(serializerType: SerializerType.Newtonsoft);
+
+                options.Schema.For<BigIntegerObject>()
+                    .Duplicate(x => x.Value);
+            });
+
+            using (var session = theStore.OpenSession())
+            {
+                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse(LargerThanLongValue) };
+
+                session.Store(obj);
+                session.SaveChanges();
+            }
+
+            StoreOptions(options =>
+            {
+                options.UseDefaultSerialization(serializerType: SerializerType.SystemTextJson);
+
+                options.Schema.For<BigIntegerObject>()
+                    .Duplicate(x => x.Value);
+            }, false);
+
+            using (var session = theStore.OpenSession())
+            {
+                var result = session.Load<BigIntegerObject>(1);
+
+                result.Value.ToString().ShouldBe(LargerThanLongValue);
+            }
+        }
+
+        [Fact]
+        public void When_Stored_With_STJ_And_Fetched_With_Newtonsoft_Should_Succeed()
+        {
+            StoreOptions(options =>
+            {
+                options.UseDefaultSerialization(serializerType: SerializerType.SystemTextJson);
+
+                options.Schema.For<BigIntegerObject>()
+                    .Duplicate(x => x.Value);
+            });
+
+            using (var session = theStore.OpenSession())
+            {
+                var obj = new BigIntegerObject { Id = 1, Value = BigInteger.Parse(LargerThanLongValue) };
+
+                session.Store(obj);
+                session.SaveChanges();
+            }
+
+            StoreOptions(options =>
+            {
+                options.UseDefaultSerialization(serializerType: SerializerType.Newtonsoft);
+
+                options.Schema.For<BigIntegerObject>()
+                    .Duplicate(x => x.Value);
+            }, false);
+
+            using (var session = theStore.OpenSession())
+            {
+                var result = session.Load<BigIntegerObject>(1);
+
+                result.Value.ToString().ShouldBe(LargerThanLongValue);
             }
         }
     }
