6dc10249721f9aad953d48cc16c5627ef23548ba
dionfoster <dion.foster@gmail.com>
2022-08-04 14:21:50 +0800

    Add test to show inline projection rebuilds stopping at gaps.

--- src/Marten.AsyncDaemon.Testing/build_aggregate_multiple_projections.cs
+++ src/Marten.AsyncDaemon.Testing/build_aggregate_multiple_projections.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Baseline.Dates;
@@ -6,7 +7,10 @@ using Marten.AsyncDaemon.Testing.TestingSupport;
 using Marten.Events.Aggregation;
 using Marten.Events.Daemon;
 using Marten.Events.Projections;
+using Marten.Testing;
+using NpgsqlTypes;
 using Shouldly;
+using Weasel.Postgresql;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -147,6 +151,108 @@ namespace Marten.AsyncDaemon.Testing
                 truckName.ShouldBe("truck-name-2");
             }
 
+        }
+
+        [Fact]
+        public async Task bug_repro_2()
+        {
+            // register projection
+            StoreOptions(x =>
+            {
+                x.Projections.Add<CarAggregation>();
+            }, true);
+
+            // create car stream with 1000 events
+            using (var session = theStore.LightweightSession())
+            {
+                var events = new List<CarNamed>();
+
+                for (var i = 0; i < 1000; i++)
+                {
+                    events.Add(new CarNamed { Value = $"car-name-{i}" });
+                }
+
+                session.Events.StartStream(Guid.NewGuid(), events);
+                await session.SaveChangesAsync();
+            }
+
+            // create some gaps
+            long startingId = 25;
+            var group1 = Enumerable.Range(0, 5).Select(x => x + startingId).ToArray();
+            var group2 = Enumerable.Range(0, 5).Select(x => x + startingId + 10).ToArray();
+            var group3 = Enumerable.Range(0, 5).Select(x => x + startingId + 20).ToArray();
+            var group4 = Enumerable.Range(0, 5).Select(x => x + startingId + 45).ToArray();
+
+            var groupsToRemove = group1.Concat(group2).Concat(group3).Concat(group4).ToArray();
+
+            await deleteEvents(groupsToRemove);
+
+            var projections = new[] { "CarView" };
+
+            // rebuild the projection
+            var daemon = await theStore.BuildProjectionDaemonAsync(logger: Logger);
+
+            await daemon.StartDaemon();
+
+            try
+            {
+                foreach (var projection in projections)
+                {
+                    await daemon.RebuildProjection($"{nameof(build_aggregate_multiple_projections)}.{projection}", default);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                await daemon.StopAll();
+                daemon.Dispose();
+            }
+
+            // assert that the projections have reached max seq_id
+            using (var session = theStore.QuerySession())
+            {
+                var waterMark = await GetHighWaterMark();
+                var maxSeqId = await GetMaxSeqId();
+
+                var cars = await session.Query<CarView>().ToListAsync();
+
+                waterMark.ShouldBe(maxSeqId);
+                cars.Last().Name.ShouldBe("car-name-999");
+            }
+        }
+
+        protected async Task deleteEvents(params long[] ids)
+        {
+            using var conn = theStore.CreateConnection();
+            await conn.OpenAsync();
+
+            await conn
+                .CreateCommand($"delete from {theStore.Events.DatabaseSchemaName}.mt_events where seq_id = ANY(:ids)")
+                .With("ids", ids, NpgsqlDbType.Bigint | NpgsqlDbType.Array)
+                .ExecuteNonQueryAsync();
+        }
+
+        protected async Task<long> GetHighWaterMark()
+        {
+            using var conn = theStore.CreateConnection();
+            await conn.OpenAsync();
+
+            return (long)await conn
+                .CreateCommand($"select last_seq_id from {theStore.Events.DatabaseSchemaName}.mt_event_progression where name = 'HighWaterMark'")
+                .ExecuteScalarAsync();
+        }
+
+        protected async Task<long> GetMaxSeqId()
+        {
+            using var conn = theStore.CreateConnection();
+            await conn.OpenAsync();
+
+            return (long)await conn
+                .CreateCommand($"select max(seq_id) from {theStore.Events.DatabaseSchemaName}.mt_events")
+                .ExecuteScalarAsync();
         }
     }
 }
