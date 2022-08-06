9fe8c618345efd747cd7e048dae324ca5765e67a
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-08-06 14:35:37 +0200

    Merged CLR typed upcasters into one as there's no need for parallel implementations, updated samples accordingly

--- src/Marten/Services/Json/Transformations/EventUpcaster.cs
+++ src/Marten/Services/Json/Transformations/EventUpcaster.cs
@@ -3,6 +3,7 @@ using System;
 using System.Data.Common;
 using System.Threading;
 using System.Threading.Tasks;
+using Marten.Events;
 
 namespace Marten.Services.Json.Transformations
 {
@@ -30,5 +31,21 @@ namespace Marten.Services.Json.Transformations
     public abstract class EventUpcaster<TEvent>: EventUpcaster
     {
         public override Type EventType => typeof(TEvent);
+    }
+
+    public abstract class EventUpcaster<TOldEvent, TEvent>: EventUpcaster<TEvent>
+        where TOldEvent : notnull where TEvent : notnull
+    {
+        public override string EventTypeName => (typeof(TOldEvent)).GetEventTypeName();
+
+        public override object FromDbDataReader(ISerializer serializer, DbDataReader dbDataReader, int index) =>
+            Transformations.FromDbDataReader<TOldEvent, TEvent>(Upcast)(serializer, dbDataReader, index);
+
+        public override Task<object> FromDbDataReaderAsync(ISerializer serializer, DbDataReader dbDataReader,
+            int index, CancellationToken ct) =>
+            Transformations.FromDbDataReaderAsync<TOldEvent, TEvent>(Upcast)(serializer, dbDataReader,
+                index, ct);
+
+        protected abstract TEvent Upcast(TOldEvent oldEvent);
     }
 }
