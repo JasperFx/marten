790ad5e1b7c153654cb261e6e2f12e615a317f95
mysticmind <babu.annamalai@gmail.com>
2022-11-04 22:43:00 +0530

    Add option to specify projection rebuild command timeout for CommandLine

--- src/Marten.CommandLine/Commands/Projection/IProjectionHost.cs
+++ src/Marten.CommandLine/Commands/Projection/IProjectionHost.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Baseline.Dates;
 using Marten.Events.Daemon;
 
 namespace Marten.CommandLine.Commands.Projection;
@@ -14,7 +16,7 @@ public interface IProjectionHost
 {
     IReadOnlyList<IProjectionStore> AllStores();
     void ListenForUserTriggeredExit();
-    Task<RebuildStatus> TryRebuildShards(IProjectionDatabase database, IReadOnlyList<AsyncProjectionShard> asyncProjectionShards);
+    Task<RebuildStatus> TryRebuildShards(IProjectionDatabase database, IReadOnlyList<AsyncProjectionShard> asyncProjectionShards, TimeSpan? shardTimeout=null);
     Task StartShards(IProjectionDatabase database, IReadOnlyList<AsyncProjectionShard> shards);
     Task WaitForExit();
 }
