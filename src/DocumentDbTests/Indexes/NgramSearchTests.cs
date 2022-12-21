a74b0c883aa58d1154aaf79166ba33c6fd5b79e8
mysticmind <babu.annamalai@gmail.com>
2022-12-21 13:27:16 +0530

    Update NGram search units to include with and without db schema

--- src/DocumentDbTests/Indexes/NgramSearchTests.cs
+++ src/DocumentDbTests/Indexes/NgramSearchTests.cs
@@ -30,6 +30,44 @@ public class NgramSearchTests : OneOffConfigurationsContext
         {
             _.Connection(ConnectionSource.ConnectionString);
 
+            // This creates
+            _.Schema.For<User>().Index(x => x.UserName);
+        });
+
+        await using var session = store.OpenSession();
+
+        string term = null;
+        for (var i = 1; i < 4; i++)
+        {
+            var guid = $"{Guid.NewGuid():N}";
+            term ??= guid.Substring(5);
+
+            var newUser = new User(i, $"Test user {guid}");
+
+            session.Store(newUser);
+        }
+
+        await session.SaveChangesAsync();
+
+        #region sample_ngram_search
+        var result = await session
+            .Query<User>()
+            .Where(x => x.UserName.NgramSearch(term))
+            .ToListAsync();
+        #endregion
+
+        result.ShouldNotBeNull();
+        result.ShouldHaveSingleItem();
+        // result[0].UserName.ShouldContain(term);
+    }
+
+    [Fact]
+    public async Task test_ngram_search_returns_data_using_db_schema()
+    {
+        var store = DocumentStore.For(_ =>
+        {
+            _.Connection(ConnectionSource.ConnectionString);
+
             _.DatabaseSchemaName = "ngram_test";
 
             // This creates
