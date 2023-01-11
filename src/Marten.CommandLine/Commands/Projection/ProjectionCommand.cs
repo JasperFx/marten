dd9db66012dd9776ea10b6eccb6cbcb82aeef169
Günther Foidl <gue@korporal.at>
2023-01-11 14:54:58 +0100

    Fixed ProjectionCommand dispose / using bug

--- src/Marten.CommandLine/Commands/Projection/ProjectionCommand.cs
+++ src/Marten.CommandLine/Commands/Projection/ProjectionCommand.cs
@@ -6,12 +6,12 @@ namespace Marten.CommandLine.Commands.Projection;
 [Description("Marten's asynchronous projection and projection rebuilds")]
 public class ProjectionsCommand: OaktonAsyncCommand<ProjectionInput>
 {
-    public override Task<bool> Execute(ProjectionInput input)
+    public override async Task<bool> Execute(ProjectionInput input)
     {
         using var host = input.BuildHost();
 
         var controller = new ProjectionController(new ProjectionHost(host), new ConsoleView());
 
-        return controller.Execute(input);
+        return await controller.Execute(input).ConfigureAwait(false);
     }
 }
