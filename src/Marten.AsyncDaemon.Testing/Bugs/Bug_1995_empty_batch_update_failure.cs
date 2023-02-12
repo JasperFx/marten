0e9dccef6ff127738c02a67c84d623f79956068f
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-02-12 18:01:24 +0100

    Reverted async session creation in tests

--- src/Marten.AsyncDaemon.Testing/Bugs/Bug_1995_empty_batch_update_failure.cs
+++ src/Marten.AsyncDaemon.Testing/Bugs/Bug_1995_empty_batch_update_failure.cs
@@ -28,7 +28,7 @@ public class Bug_1995_empty_batch_update_failure : BugIntegrationContext
     {
         await documentStore.Advanced.Clean.CompletelyRemoveAllAsync();
 
-        await using (var session = await documentStore.LightweightSessionAsync())
+        await using (var session = documentStore.LightweightSession())
         {
             for (var i = 0; i < 499; i++)
             {
