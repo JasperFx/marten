6889532adc1f969b20a340d12d19df5834a51e2c
JT <Hawxy@users.noreply.github.com>
2023-02-16 22:19:30 +0800

    Enable nested sidebar

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,5 +3,6 @@ export default {
       fsServe: {
         root: '../' 
       }
-    }
+    },
+    optimizeDeps: { include: ['moment-mini', '@braintree/sanitize-url', 'dagre', 'dagre-d3', 'graphlib'] }
   }
