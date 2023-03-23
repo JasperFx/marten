70b3698dfaf6d76981ed8faf48daf278aa140b8c
JT <Hawxy@users.noreply.github.com>
2023-03-23 21:04:59 +0800

    Add failure scenario for HashSet contains

---
+++ src/DocumentDbTests/Bugs/hashset_contains.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten;
+using Marten.Testing.Documents;
+using Marten.Testing.Harness;
+using Xunit;
+
+namespace DocumentDbTests.Bugs;
+
+public class hashset_contains : BugIntegrationContext
+{
+    [Fact]
+    public async Task Can_query_by_hashset_contains()
+    {
+        var value = Guid.NewGuid().ToString();
+        theSession.Store(new MySpecialType(Guid.NewGuid(), value));
+
+        var hashset = new HashSet<string>() {value};
+        var items = await theSession
+            .Query<MySpecialType>()
+            .Where(x => hashset.Contains(x.Value))
+            .ToListAsync();
+
+        Assert.Equal(1, items.Count);
+    }
+
+    public record MySpecialType(Guid Id, string Value);
+}
