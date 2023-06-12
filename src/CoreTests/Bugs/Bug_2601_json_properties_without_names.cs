6ceef739c0fdcb96624e3ddbb48a7524379c320d
Ben Edwards <bene@pageuppeople.com>
2023-06-13 09:40:36 +1000

    Map to bug #

--- src/CoreTests/Bugs/Bug_2601_json_properties_without_names.cs
+++ src/CoreTests/Bugs/Bug_2601_json_properties_without_names.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace CoreTests.Bugs;
 
-public class Bug_9001_json_properties_without_names: BugIntegrationContext
+public class Bug_2601_json_properties_without_names: BugIntegrationContext
 {
     [Fact]
     public async Task document_with_json_properties_without_names_should_still_be_queryable()
@@ -20,6 +20,11 @@ public class Bug_9001_json_properties_without_names: BugIntegrationContext
         theSession.Store(newDoc);
         await theSession.SaveChangesAsync();
 
+        // expected query
+        //  where (d.data ->> 'Name' = $1)
+
+        // actual query
+        //  where (d.data ->> '' = $1)
         var lookup = await theSession.Query<DocWithJsonProperties>().Where(x => x.Name == "foo").FirstOrDefaultAsync();
         Assert.NotNull(lookup);
         Assert.Equal(newDoc.Id, lookup.Id);
@@ -29,6 +34,7 @@ public class Bug_9001_json_properties_without_names: BugIntegrationContext
     {
         public Guid Id { get; set; }
 
+        // Lack of "propertyName" here
         [JsonProperty]
         public string Name { get; set; }
     }
