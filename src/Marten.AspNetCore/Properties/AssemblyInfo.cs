ff4b17455c9b0105f82753c0dccbdb4f6fa7a8e6
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-09-08 10:37:29 +0200

    Bumped to 6.1.0
    Moved Healthchecks to Marten.AspNetCore package
    Bumped other dependencies

---
+++ src/Marten.AspNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Marten.Testing")]
+[assembly: InternalsVisibleTo("Marten.CommandLine")]
+[assembly: InternalsVisibleTo("Marten.PLv8")]
+[assembly: InternalsVisibleTo("Marten.PLv8.Testing")]
+[assembly: InternalsVisibleTo("Marten.Schema.Testing")]
+[assembly: InternalsVisibleTo("Marten.AsyncDaemon.Testing")]
+[assembly: InternalsVisibleTo("ConfigurationTests")]
+[assembly: InternalsVisibleTo("CoreTests")]
+[assembly: InternalsVisibleTo("DocumentDbTests")]
+[assembly: InternalsVisibleTo("EventSourcingTests")]
+[assembly: InternalsVisibleTo("Examples")]
