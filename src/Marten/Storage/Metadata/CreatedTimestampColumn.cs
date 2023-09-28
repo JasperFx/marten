a1656f917b816e33d89b24ed5ecab913de477f66
bboughan <bboughan@gogreen.co.uk>
2023-09-29 00:35:01 +0100

    add mt_created_timestamp field to documents

--- src/Marten/Storage/Metadata/CreatedTimestampColumn.cs
+++ src/Marten/Storage/Metadata/CreatedTimestampColumn.cs
@@ -12,7 +12,6 @@ internal class CreatedTimestampColumn: MetadataColumn<DateTimeOffset>, ISelectab
     {
         DefaultExpression = "(transaction_timestamp())";
         Type = "timestamp with time zone";
-        Enabled = false;
     }
 
     public void GenerateCode(StorageStyle storageStyle, GeneratedType generatedType, GeneratedMethod async,
