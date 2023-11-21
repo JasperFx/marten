ad42d908c3d9a7d989fe3e6bb422a8b763953e11
mysticmind <babu.annamalai@gmail.com>
2023-11-21 11:07:27 +0530

    Update the logic to identify .NET target framework in build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-version="$(dotnet --version)"
-if [[ $version = 6.* ]]; then
-  target_framework="net6.0"
-elif [[ $version = 7.* ]]; then
-  target_framework="net7.0"
-else
-  echo "BUILD FAILURE: .NET 6, .NET 7 SDK required to run build"
+versions=($(dotnet --list-sdks | awk '{print $1}' | cut -d '[' -f 1))
+target_framework=""
+for version in "${versions[@]}"; do
+  if [[ $version = 6.* ]]; then
+    target_framework="net6.0"
+  elif [[ $version = 7.* ]]; then
+    target_framework="net7.0"
+  fi
+done
+
+if [ -z "$target_framework" ]; then
+  echo "BUILD FAILURE: .NET 6 or .NET 7 SDK required to run build"
   exit 1
 fi
 
-dotnet run --project build/build.csproj -f $target_framework -c Release -- "$@"
+echo "Using $target_framework"
+# dotnet run --project build/build.csproj -f $target_framework -c Release -- "$@"
