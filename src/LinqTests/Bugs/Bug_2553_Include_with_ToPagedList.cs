2f1f5955c9fb5289feadd3f7d9d199b79a5d3fcb
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-11-27 11:19:46 -0600

    Test to verify that Include() plus ToPagedList() works after the LINQ overhaul. Closes GH-2553

---
+++ src/LinqTests/Bugs/Bug_2553_Include_with_ToPagedList.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Marten.Pagination;
+using Marten.Testing.Harness;
+using Shouldly;
+
+namespace LinqTests.Bugs;
+
+public class Bug_2553_Include_with_ToPagedList: BugIntegrationContext
+{
+    public record Foo(Guid Id);
+
+    public record Bar(Guid Id, Guid FooId);
+
+    [Fact]
+    public void query_should_return_included_docs()
+    {
+        var foos = Enumerable.Range(start: 0, count: 100).Select(i => new Foo(Guid.NewGuid())).ToArray();
+        var bars = Enumerable.Range(start: 0, count: 100).Select(i => new Bar(Guid.NewGuid(), FooId: foos[i].Id));
+
+        foreach (var foo in foos)
+        {
+            theSession.Store(foo);
+        }
+
+        foreach (var bar in bars)
+        {
+            theSession.Store(bar);
+        }
+
+        theSession.SaveChanges();
+
+        var includedFoos = new Dictionary<Guid, Foo>();
+
+        var queriedBars = theSession.Query<Bar>()
+            .Include(bar => bar.FooId, dictionary: includedFoos)
+            .ToPagedList(pageNumber: 1, pageSize: 4);
+
+        foreach (var queriedBar in queriedBars)
+        {
+            includedFoos.ShouldContainKey(key: queriedBar.FooId);
+        }
+    }
+}
