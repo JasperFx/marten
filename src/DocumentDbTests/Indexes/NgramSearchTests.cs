f12d13dd0c8186c48868cea305901e39a00c169b
mysticmind <babu.annamalai@gmail.com>
2023-12-03 20:00:12 +0530

    Fix NGram index on nested property throwing an exception
    - Fix NGram index logic for data config generation and indexname generation
    - Add unit test for ngram index on nested property
    
    Fixes GH-2304

--- src/DocumentDbTests/Indexes/NgramSearchTests.cs
+++ src/DocumentDbTests/Indexes/NgramSearchTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Marten;
+using Marten.Testing.Documents;
+using Marten.Testing.Harness;
 using Shouldly;
 using Xunit;
 
@@ -10,15 +12,29 @@ namespace DocumentDbTests.Indexes;
 public class NgramSearchTests : Marten.Testing.Harness.OneOffConfigurationsContext
 {
 
+    public sealed class Address
+    {
+        public Address(string line1, string line2)
+        {
+            Line1 = line1;
+            Line2 = line2;
+        }
+
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+    }
     public sealed class User
     {
         public int Id { get; set; }
         public string UserName { get; set; }
 
-        public User(int id, string userName)
+        public Address Address { get; set; }
+
+        public User(int id, string userName, Address address=null)
         {
             Id = id;
             UserName = userName;
+            Address = address;
         }
     }
 
@@ -57,7 +73,7 @@ public class NgramSearchTests : Marten.Testing.Harness.OneOffConfigurationsConte
 
         result.ShouldNotBeNull();
         result.ShouldHaveSingleItem();
-        result[0].UserName.ShouldContain(term);
+        ShouldBeStringTestExtensions.ShouldContain(result[0].UserName, term);
     }
 
     [Fact]
@@ -97,6 +113,44 @@ public class NgramSearchTests : Marten.Testing.Harness.OneOffConfigurationsConte
 
         result.ShouldNotBeNull();
         result.ShouldHaveSingleItem();
-        result[0].UserName.ShouldContain(term);
+        ShouldBeStringTestExtensions.ShouldContain(result[0].UserName, term);
+    }
+
+    [Fact]
+    public async Task test_ngram_on_nested_prop_search_returns_data()
+    {
+        var store = DocumentStore.For(_ =>
+        {
+            _.Connection(Marten.Testing.Harness.ConnectionSource.ConnectionString);
+
+            // This creates
+            _.Schema.For<User>().NgramIndex(x => x.Address.Line1);
+        });
+
+        await using var session = store.LightweightSession();
+
+        string term = null;
+        for (var i = 1; i < 4; i++)
+        {
+            var guid = $"{Guid.NewGuid():N}";
+            term ??= guid.Substring(5);
+            var address = new Address(guid, guid);
+            var newUser = new User(i, $"Test user {guid}", address);
+
+            session.Store(newUser);
+        }
+
+        await session.SaveChangesAsync();
+
+        #region sample_ngram_search
+        var result = await session
+            .Query<User>()
+            .Where(x => x.Address.Line1.NgramSearch(term))
+            .ToListAsync();
+        #endregion
+
+        result.ShouldNotBeNull();
+        result.ShouldHaveSingleItem();
+        ShouldBeStringTestExtensions.ShouldContain(result[0].Address.Line1, term);
     }
 }
