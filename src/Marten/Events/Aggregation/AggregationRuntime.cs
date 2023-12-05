e732c01b4dca41fe100f6dfd6c174257890738da
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-12-05 09:37:36 -0600

    WIP: bug repro steps for Bug 2666, hard coded CreateDefault() in projections

--- src/Marten/Events/Aggregation/AggregationRuntime.cs
+++ src/Marten/Events/Aggregation/AggregationRuntime.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using JasperFx.Core.Reflection;
 using Marten.Events.Daemon;
 using Marten.Events.Projections;
 using Marten.Exceptions;
@@ -162,4 +164,16 @@ public abstract class AggregationRuntime<TDoc, TId>: IAggregationRuntime<TDoc, T
     public abstract ValueTask<TDoc> ApplyEvent(IQuerySession session, EventSlice<TDoc, TId> slice,
         IEvent evt, TDoc? aggregate,
         CancellationToken cancellationToken);
+
+    public TDoc CreateDefault(IEvent @event)
+    {
+        try
+        {
+            return (TDoc)Activator.CreateInstance(typeof(TDoc), true);
+        }
+        catch (Exception e)
+        {
+            throw new System.InvalidOperationException($"There is no default constructor for {typeof(TDoc).FullNameInCode()} or Create method for {@event.DotNetTypeName} event type.Check more about the create method convention in documentation: https://martendb.io/events/projections/event-projections.html#create-method-convention. If you're using Upcasting, check if {@event.DotNetTypeName} is an old event type. If it is, make sure to define transformation for it to new event type. Read more in Upcasting docs: https://martendb.io/events/versioning.html#upcasting-advanced-payload-transformations.");
+        }
+    }
 }
