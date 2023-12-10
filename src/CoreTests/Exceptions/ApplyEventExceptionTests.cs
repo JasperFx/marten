b0f642eb16606ab4e079793d450a07ebdfa27653
Anne Erdtsieck <anne.erdtsieck@topicus.nl>
2023-12-10 17:28:03 +0100

    #2849 ApplyEventException can leak event data to logs

---
+++ src/CoreTests/Exceptions/ApplyEventExceptionTests.cs
@@ -0,0 +1,26 @@
+using Marten.Events;
+using Marten.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace CoreTests.Exceptions;
+
+public class ApplyEventExceptionTests
+{
+    public class FakeEventThatContainsSecretInformation
+    {
+        public string Secret { get; set; }
+    }
+
+    [Fact]
+    public void should_only_include_sequence_and_id_no_data()
+    {
+        var @event = new Event<FakeEventThatContainsSecretInformation>(new()
+        {
+            Secret = "very secret!!!"
+        });
+        var exception = new ApplyEventException(@event, new("inner"));
+
+        exception.Message.ShouldBe($"Failure to apply event #{@event.Sequence} Id({@event.Id})");
+    }
+}
