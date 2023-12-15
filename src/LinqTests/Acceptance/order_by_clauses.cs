e3303354e74d02f7bda28f3f183744f7b905d4f8
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-12-15 06:46:38 -0600

    Using the cast on descending order by. Closes GH-2862

--- src/LinqTests/Acceptance/order_by_clauses.cs
+++ src/LinqTests/Acceptance/order_by_clauses.cs
@@ -1,26 +1,42 @@
 using System.Linq;
+using System.Threading.Tasks;
 using LinqTests.Acceptance.Support;
+using Xunit.Abstractions;
 
 namespace LinqTests.Acceptance;
 
 public class order_by_clauses: LinqTestContext<order_by_clauses>
 {
-    public order_by_clauses(DefaultQueryFixture fixture) : base(fixture)
+    public order_by_clauses(DefaultQueryFixture fixture, ITestOutputHelper output) : base(fixture)
     {
+        TestOutput = output;
     }
 
     static order_by_clauses()
     {
-        ordered(t => t.OrderBy(x => x.String));
-        ordered(t => t.OrderByDescending(x => x.String));
+        ordered(t => t.OrderBy(x => x.String).ThenBy(x => x.Id));
+        ordered(t => t.OrderByDescending(x => x.String).ThenBy(x => x.Id));
 
-        ordered(t => t.OrderBy(x => x.Number).ThenBy(x => x.String));
-        ordered(t => t.OrderBy(x => x.Number).ThenByDescending(x => x.String));
-        ordered(t => t.OrderByDescending(x => x.Number).ThenBy(x => x.String));
+        ordered(t => t.OrderBy(x => x.Number).ThenBy(x => x.String).ThenBy(x => x.Id).Take(10));
+        ordered(t => t.OrderBy(x => x.Number).ThenByDescending(x => x.String).ThenBy(x => x.Id).Take(10));
+        ordered(t => t.OrderByDescending(x => x.Number).ThenBy(x => x.String).ThenBy(x => x.Id).Take(10));
 
-        ordered(t => t.OrderBy(x => x.String).Take(2));
-        ordered(t => t.OrderBy(x => x.String).Skip(2));
-        ordered(t => t.OrderBy(x => x.String).Take(2).Skip(2));
+        ordered(t => t.OrderByDescending(x => x.Inner.Number));
+    }
+
+
+    [Theory]
+    [MemberData(nameof(GetDescriptions))]
+    public Task run_query(string description)
+    {
+        return assertTestCase(description, Fixture.Store);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetDescriptions))]
+    public Task with_duplicated_fields(string description)
+    {
+        return assertTestCase(description, Fixture.DuplicatedFieldStore);
     }
 
 }
