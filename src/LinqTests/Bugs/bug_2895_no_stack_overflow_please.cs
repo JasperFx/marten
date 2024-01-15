61bb289ddf9d5c556d2b28b160abc583047ac75e
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-01-15 06:41:48 -0600

    Trapping the comparison of a constant to a constant in the LINQ parsing that was causing StackOverflow conditions. Closes GH-2895

---
+++ src/LinqTests/Bugs/bug_2895_no_stack_overflow_please.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten;
+using Marten.Testing.Harness;
+
+namespace LinqTests.Bugs;
+
+public class bug_2895_no_stack_overflow_please : BugIntegrationContext
+{
+    [Fact]
+    public async Task do_not_blow_up_in_linq_parsing()
+    {
+        string? category = null;
+        await theSession.Query<Entity4>().Where(p =>
+                p.SomeBool == false && (category == null || p.StringArray.Contains(category)))
+            .ToListAsync();
+    }
+}
+
+public sealed record Entity4(Guid Id, bool SomeBool, string[] StringArray);
