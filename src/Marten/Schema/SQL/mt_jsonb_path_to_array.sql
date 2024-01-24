bae60ff56b5d5ea4c1599133bdf0f8187970fc03
mysticmind <babu.annamalai@gmail.com>
2024-01-24 10:02:59 +0530

    Further fixes to function signature default value added to system functions

--- src/Marten/Schema/SQL/mt_jsonb_path_to_array.sql
+++ src/Marten/Schema/SQL/mt_jsonb_path_to_array.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION {databaseSchema}.mt_jsonb_path_to_array(text, char(1) DEFAULT '\.')
+CREATE OR REPLACE FUNCTION {databaseSchema}.mt_jsonb_path_to_array(text, char(1) default '\.')
 RETURNS text[] AS $$
 DECLARE
     location ALIAS FOR $1;
