7184e8cd34af82c92ef25c13a38e490467a128f2
mysticmind <babu.annamalai@gmail.com>
2024-01-24 10:28:26 +0530

    Remove default values for patch functions

--- src/Marten/Schema/SQL/mt_jsonb_path_to_array.sql
+++ src/Marten/Schema/SQL/mt_jsonb_path_to_array.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION {databaseSchema}.mt_jsonb_path_to_array(text, char(1) default '\.')
+CREATE OR REPLACE FUNCTION {databaseSchema}.mt_jsonb_path_to_array(text, char(1))
 RETURNS text[] AS $$
 DECLARE
     location ALIAS FOR $1;
