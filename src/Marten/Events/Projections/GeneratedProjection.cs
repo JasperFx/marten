997933771b8e70fcf933675d4851079dfcf98c99
Anne Erdtsieck <anne.erdtsieck@topicus.nl>
2024-02-03 14:32:18 +0100

    #2943 Do not fail aggregate code generation on parallel execution

--- src/Marten/Events/Projections/GeneratedProjection.cs
+++ src/Marten/Events/Projections/GeneratedProjection.cs
@@ -1,7 +1,6 @@
 #nullable enable
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -37,11 +36,14 @@ public abstract class GeneratedProjection: ProjectionBase, IProjectionSource, IC
 
     void ICodeFile.AssembleTypes(GeneratedAssembly assembly)
     {
-        if (!_hasGenerated)
+        lock (_assembleLocker)
         {
+            if (_hasGenerated)
+                return;
             lock (_assembleLocker)
             {
-                if (_hasGenerated) return;
+                if (_hasGenerated)
+                    return;
                 assembleTypes(assembly, StoreOptions);
                 _hasGenerated = true;
             }
@@ -95,29 +97,38 @@ public abstract class GeneratedProjection: ProjectionBase, IProjectionSource, IC
 
     private void generateIfNecessary(DocumentStore store)
     {
-        if (_hasGenerated)
+        lock (_assembleLocker)
         {
-            return;
-        }
-
-        StoreOptions = store.Options;
-        var rules = store.Options.CreateGenerationRules();
-        rules.ReferenceTypes(GetType());
-        this.As<ICodeFile>().InitializeSynchronously(rules, store.Options.EventGraph, null);
-
-        if (needsSettersGenerated())
-        {
-            lock (_assembleLocker)
+            if (_hasGenerated)
             {
-                var generatedAssembly = new GeneratedAssembly(rules);
-                assembleTypes(generatedAssembly, store.Options);
-
-                // This will force it to create any setters or dynamic funcs
-                generatedAssembly.GenerateCode();
+                return;
             }
+
+            generateIfNecessaryLocked();
+
+            _hasGenerated = true;
         }
 
-        _hasGenerated = true;
+        return;
+
+        void generateIfNecessaryLocked()
+        {
+            StoreOptions = store.Options;
+            var rules = store.Options.CreateGenerationRules();
+            rules.ReferenceTypes(GetType());
+            this.As<ICodeFile>().InitializeSynchronously(rules, store.Options.EventGraph, null);
+
+            if (!needsSettersGenerated())
+            {
+                return;
+            }
+
+            var generatedAssembly = new GeneratedAssembly(rules);
+            assembleTypes(generatedAssembly, store.Options);
+
+            // This will force it to create any setters or dynamic funcs
+            generatedAssembly.GenerateCode();
+        }
     }
 
     /// <summary>
