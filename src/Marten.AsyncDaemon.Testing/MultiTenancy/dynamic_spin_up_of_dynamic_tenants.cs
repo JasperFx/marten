e57e2ffc3ec4a6f391bdf9554c6e95bed4a85854
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-02-21 14:26:06 -0600

    comments from a blog post

--- src/Marten.AsyncDaemon.Testing/MultiTenancy/dynamic_spin_up_of_dynamic_tenants.cs
+++ src/Marten.AsyncDaemon.Testing/MultiTenancy/dynamic_spin_up_of_dynamic_tenants.cs
@@ -54,11 +54,15 @@ public class dynamic_spin_up_of_dynamic_tenants : IAsyncLifetime
         tenant3ConnectionString = await CreateDatabaseIfNotExists(conn, "tenant3");
         tenant4ConnectionString = await CreateDatabaseIfNotExists(conn, "tenant4");
 
+        // Setting up a Host with Multi-tenancy
         _host = await Host.CreateDefaultBuilder()
             .ConfigureServices(services =>
             {
                 services.AddMarten(opts =>
                     {
+                        // This is a new strategy for configuring tenant databases with Marten
+                        // In this usage, Marten is tracking the tenant databases in a single table in the "master"
+                        // database by tenant
                         opts.MultiTenantedDatabasesWithMasterDatabaseTable(ConnectionSource.ConnectionString, "tenants");
 
                         opts.RegisterDocumentType<User>();
@@ -83,19 +87,24 @@ public class dynamic_spin_up_of_dynamic_tenants : IAsyncLifetime
     [Fact]
     public async Task add_tenant_database_and_verify_the_daemon_projections_are_running()
     {
+        // In this code block, I'm adding new tenant databases to the system that I
+        // would expect Marten to discover and start up an asynchronous projection
+        // daemon for all three newly discovered databases
         var tenancy = (MasterTableTenancy)theStore.Options.Tenancy;
         await tenancy.AddDatabaseRecordAsync("tenant1", tenant1ConnectionString);
         await tenancy.AddDatabaseRecordAsync("tenant2", tenant2ConnectionString);
         await tenancy.AddDatabaseRecordAsync("tenant3", tenant3ConnectionString);
 
+        // This is a new service in Marten specifically to help you interrogate or
+        // manipulate the state of running asynchronous projections within the current process
         var coordinator = _host.Services.GetRequiredService<IProjectionCoordinator>();
         var daemon1 = await coordinator.DaemonForDatabase("tenant1");
         var daemon2 = await coordinator.DaemonForDatabase("tenant2");
         var daemon3 = await coordinator.DaemonForDatabase("tenant3");
 
-        // I hate this.
-        await Task.Delay(3.Seconds());
-
+        // Just proving that the configured projections for the 3 new databases
+        // are indeed spun up and running after Marten's new daemon coordinator
+        // "finds" the new databases
         await daemon1.WaitForShardToBeRunning("TripCustomName:All", 30.Seconds());
         await daemon2.WaitForShardToBeRunning("TripCustomName:All", 30.Seconds());
         await daemon3.WaitForShardToBeRunning("TripCustomName:All", 30.Seconds());
