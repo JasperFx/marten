da37afaf406b5fd71d4998dbafc1f95ef0e8880b
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-02-21 14:38:20 -0600

    Test to prove an open bug is fixed. Closes GH-2980

--- src/LinqTests/Bugs/Bug_using_literal_false_in_where_clause.cs
+++ src/LinqTests/Bugs/Bug_using_literal_false_in_where_clause.cs
@@ -48,6 +48,31 @@ public class Bug_using_literal_false_in_where_clause : BugIntegrationContext
 
         actual.ShouldBeEmpty();
     }
+
+    [Fact]
+    public async Task Bug_2980()
+    {
+        var aggregate1 = new DeletableAggregate
+        {
+            Id = Guid.NewGuid(),
+            Deleted = true,
+        };
+        theSession.Store(aggregate1);
+        var aggregate2 = new DeletableAggregate
+        {
+            Id = Guid.NewGuid(),
+            Deleted = false,
+        };
+        theSession.Store(aggregate2);
+        await theSession.SaveChangesAsync();
+
+        var actual = await theSession
+            .Query<DeletableAggregate>()
+            .Where(x => false)
+            .ToListAsync();
+
+        actual.ShouldBeEmpty();
+    }
 }
 
 public class DeletableAggregate: ISoftDeleted
