3b10164b7b91420b87613a125333e97b65c51c19
aleksandr 'sascha' shtifanov <a.shtifanov@layagroup.de>
2024-03-20 14:56:52 +0100

    extend StoreOptions with a possibility to extend default error handling policies via `.ExtendPolly()`

---
+++ src/Marten/Util/ResilientPipelineBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Marten.Events.Daemon.Internals;
+using Marten.Exceptions;
+using Npgsql;
+using Polly;
+
+namespace Marten.Util;
+
+internal static class ResilientPipelineBuilderExtensions
+{
+    public static ResiliencePipelineBuilder AddMartenDefaults(this ResiliencePipelineBuilder builder)
+    {
+        #region sample_default_Polly_setup
+
+        // default Marten policies
+        return builder
+           .AddRetry(new()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<NpgsqlException>().Handle<MartenCommandException>().Handle<EventLoaderException>(),
+                MaxRetryAttempts = 3,
+                Delay = TimeSpan.FromMilliseconds(50),
+                BackoffType = DelayBackoffType.Exponential
+            });
+
+        #endregion
+    }
+}
