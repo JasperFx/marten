d02ce6c7aab5b142bc90e9ca8f8601ecc3e40475
felixkmh <24227002+felixkmh@users.noreply.github.com>
2024-04-02 23:37:37 +0200

    Extracted CreateDocumentProvider method

--- src/Marten/Internal/ProviderGraph.cs
+++ src/Marten/Internal/ProviderGraph.cs
@@ -13,8 +13,8 @@ namespace Marten.Internal;
 
 public class ProviderGraph: IProviderGraph
 {
-    private readonly object _storageLock = new();
     private readonly StoreOptions _options;
+    private readonly object _storageLock = new();
     private ImHashMap<Type, object> _storage = ImHashMap<Type, object>.Empty;
 
     public ProviderGraph(StoreOptions options)
@@ -46,71 +46,78 @@ public class ProviderGraph: IProviderGraph
                 return stored.As<DocumentProvider<T>>();
             }
 
-            if (documentType == typeof(IEvent))
+            return CreateDocumentProvider<T>();
+        }
+    }
+
+    private DocumentProvider<T> CreateDocumentProvider<T>() where T : notnull
+    {
+        var documentType = typeof(T);
+
+        if (documentType == typeof(IEvent))
+        {
+            var rules = _options.CreateGenerationRules();
+            _options.EventGraph.InitializeSynchronously(rules, _options.EventGraph, null);
+
+            _storage = _storage.AddOrUpdate(documentType, _options.EventGraph.Provider);
+
+            return _options.EventGraph.Provider.As<DocumentProvider<T>>();
+        }
+
+        var mapping = _options.Storage.FindMapping(documentType);
+
+        switch (mapping)
+        {
+            case DocumentMapping m:
             {
-                var rules = _options.CreateGenerationRules();
-                _options.EventGraph.InitializeSynchronously(rules, _options.EventGraph, null);
-
-                _storage = _storage.AddOrUpdate(documentType, _options.EventGraph.Provider);
-
-                return _options.EventGraph.Provider.As<DocumentProvider<T>>();
-            }
-
-            var mapping = _options.Storage.FindMapping(documentType);
-
-            switch (mapping)
-            {
-                case DocumentMapping m:
+                try
                 {
-                    try
-                    {
-                        var builder = new DocumentProviderBuilder(m, _options);
+                    var builder = new DocumentProviderBuilder(m, _options);
 
-                        var rules = _options.CreateGenerationRules();
-                        rules.ReferenceTypes(m.DocumentType);
-                        builder.InitializeSynchronously(rules, _options, null);
-                        var slot = builder.BuildProvider<T>();
+                    var rules = _options.CreateGenerationRules();
+                    rules.ReferenceTypes(m.DocumentType);
+                    builder.InitializeSynchronously(rules, _options, null);
+                    var slot = builder.BuildProvider<T>();
 
-                        _storage = _storage.AddOrUpdate(documentType, slot);
-
-                        return slot;
-                    }
-                    catch (Exception e)
-                    {
-                        if (e.Message.Contains("is inaccessible due to its protection level"))
-                        {
-                            throw new InvalidOperationException(
-                                $"Requested document type '{mapping.DocumentType.FullNameInCode()}' must be scoped as 'public' in order to be used as a document type inside of Marten",
-                                e);
-                        }
-
-                        throw;
-                    }
-                }
-                case SubClassMapping s:
-                {
-                    var loader =
-                        typeof(SubClassLoader<,,>).CloseAndBuildAs<ISubClassLoader<T>>(mapping.Root.DocumentType,
-                            documentType,
-                            mapping.IdType);
-
-                    var slot = loader.BuildPersistence(this, s);
                     _storage = _storage.AddOrUpdate(documentType, slot);
 
                     return slot;
                 }
-                case EventMapping em:
+                catch (Exception e)
                 {
-                    var storage = (IDocumentStorage<T>)em;
-                    var slot = new DocumentProvider<T>(null, storage, storage, storage, storage);
-                    _storage = _storage.AddOrUpdate(documentType, slot);
+                    if (e.Message.Contains("is inaccessible due to its protection level"))
+                    {
+                        throw new InvalidOperationException(
+                            $"Requested document type '{mapping.DocumentType.FullNameInCode()}' must be scoped as 'public' in order to be used as a document type inside of Marten",
+                            e);
+                    }
 
-                    return slot;
+                    throw;
                 }
-                default:
-                    throw new NotSupportedException("Unable to build document persistence handlers for " +
-                                                    mapping.DocumentType.FullNameInCode());
             }
+            case SubClassMapping s:
+            {
+                var loader =
+                    typeof(SubClassLoader<,,>).CloseAndBuildAs<ISubClassLoader<T>>(mapping.Root.DocumentType,
+                        documentType,
+                        mapping.IdType);
+
+                var slot = loader.BuildPersistence(this, s);
+                _storage = _storage.AddOrUpdate(documentType, slot);
+
+                return slot;
+            }
+            case EventMapping em:
+            {
+                var storage = (IDocumentStorage<T>)em;
+                var slot = new DocumentProvider<T>(null, storage, storage, storage, storage);
+                _storage = _storage.AddOrUpdate(documentType, slot);
+
+                return slot;
+            }
+            default:
+                throw new NotSupportedException("Unable to build document persistence handlers for " +
+                                                mapping.DocumentType.FullNameInCode());
         }
     }
 
