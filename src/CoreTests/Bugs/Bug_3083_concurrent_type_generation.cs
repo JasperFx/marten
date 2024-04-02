1c094ea7b80f48f0c71b11954c8a91ed7eb3dcc7
felixkmh <24227002+felixkmh@users.noreply.github.com>
2024-04-03 00:00:22 +0200

    Added test for concurrently appending DocumentProviders

--- src/CoreTests/Bugs/Bug_3083_concurrent_type_generation.cs
+++ src/CoreTests/Bugs/Bug_3083_concurrent_type_generation.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Marten;
 using Marten.Internal;
 using Marten.Internal.Storage;
+using Marten.Schema.BulkLoading;
 using Marten.Testing.Harness;
 using Shouldly;
 using Xunit;
@@ -36,7 +37,60 @@ public class Bug_3083_concurrent_type_generation: BugIntegrationContext
         storages.ShouldHaveSingleItem();
     }
 
+    [Fact]
+    public async Task concurrent_append_providers()
+    {
+        var graph = new ProviderGraph(new StoreOptions());
+
+        var tasks = new List<Task>();
+
+        var documentProvider1 = new MockDocumentProvider<SomeDocument>();
+        var documentProvider2 = new MockDocumentProvider<OtherDocument>();
+        var documentProvider3 = new MockDocumentProvider<ThirdDocument>();
+        var documentProvider4 = new MockDocumentProvider<ForthDocument>();
+
+        tasks.Add(Task.Run(() => graph.Append(documentProvider1)));
+        tasks.Add(Task.Run(() => graph.Append(documentProvider2)));
+        tasks.Add(Task.Run(() => graph.Append(documentProvider3)));
+        tasks.Add(Task.Run(() => graph.Append(documentProvider4)));
+
+        await Task.WhenAll(tasks);
+
+        graph.StorageFor<SomeDocument>().ShouldBeTheSameAs(documentProvider1);
+        graph.StorageFor<OtherDocument>().ShouldBeTheSameAs(documentProvider2);
+        graph.StorageFor<ThirdDocument>().ShouldBeTheSameAs(documentProvider3);
+        graph.StorageFor<ForthDocument>().ShouldBeTheSameAs(documentProvider4);
+    }
+
+    public class MockDocumentProvider<T>: DocumentProvider<T> where T : notnull
+    {
+        public MockDocumentProvider(): this(null, null, null, null, null)
+        {
+        }
+
+        public MockDocumentProvider(IBulkLoader<T> bulkLoader, IDocumentStorage<T> queryOnly,
+            IDocumentStorage<T> lightweight, IDocumentStorage<T> identityMap, IDocumentStorage<T> dirtyTracking): base(
+            bulkLoader, queryOnly, lightweight, identityMap, dirtyTracking)
+        {
+        }
+    }
+
     public class SomeDocument
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+
+    public class OtherDocument
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+
+    public class ThirdDocument
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+
+    public class ForthDocument
     {
         public string Id { get; set; } = string.Empty;
     }
