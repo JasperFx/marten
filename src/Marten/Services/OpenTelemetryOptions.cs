ac9b13109ca36241b69084e2ce1732ecad190caa
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2024-04-03 14:25:28 +0100

    OpenTelemetryOptions updates.

--- src/Marten/Services/OpenTelemetryOptions.cs
+++ src/Marten/Services/OpenTelemetryOptions.cs
@@ -6,5 +6,5 @@ public sealed class OpenTelemetryOptions
     /// <summary>
     /// Used to track OpenTelemetry events for a connection, for example when a command or data reader has been executed. This defaults to false.
     /// </summary>
-    public bool TrackConnectionEvents { get; set; } = false;
+    public bool TrackConnectionEvents { get; set; }
 }
