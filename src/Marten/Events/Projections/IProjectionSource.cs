bbf78e2a05b6e1510f7c21548a9eadffa5328138
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2024-05-19 12:13:06 +0200

    docs: explain intend of IProjection and IProjectionSource

--- src/Marten/Events/Projections/IProjectionSource.cs
+++ src/Marten/Events/Projections/IProjectionSource.cs
@@ -9,7 +9,11 @@ using Weasel.Core;
 
 namespace Marten.Events.Projections;
 
-
+/// <summary>
+/// Interface for sources of projections
+/// Sources of projections are used to define the behavior how a projection is built for a given projection type
+/// Optimized for async usage
+/// </summary>
 public interface IProjectionSource: IReadOnlyProjectionData
 {
     AsyncOptions Options { get; }
