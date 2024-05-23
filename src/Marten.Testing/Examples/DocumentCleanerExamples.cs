3028d54dc1dd298a12b4f19ebfd34d59540396ab
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2024-05-23 10:55:28 +0200

    docs: await async usages

--- src/Marten.Testing/Examples/DocumentCleanerExamples.cs
+++ src/Marten.Testing/Examples/DocumentCleanerExamples.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Marten.Testing.Documents;
 
 namespace Marten.Testing.Examples;
@@ -5,29 +6,29 @@ namespace Marten.Testing.Examples;
 public class DocumentCleanerExamples
 {
     #region sample_clean_out_documents
-    public void clean_out_documents(IDocumentStore store)
+    public async Task clean_out_documents(IDocumentStore store)
     {
         // Completely remove all the database schema objects related
         // to the User document type
-        store.Advanced.Clean.CompletelyRemoveAsync(typeof(User));
+        await store.Advanced.Clean.CompletelyRemoveAsync(typeof(User));
 
         // Tear down and remove all Marten related database schema objects
-        store.Advanced.Clean.CompletelyRemoveAllAsync();
+        await store.Advanced.Clean.CompletelyRemoveAllAsync();
 
         // Deletes all the documents stored in a Marten database
-        store.Advanced.Clean.DeleteAllDocumentsAsync();
+        await store.Advanced.Clean.DeleteAllDocumentsAsync();
 
         // Deletes all the event data stored in a Marten database
-        store.Advanced.Clean.DeleteAllEventDataAsync();
+        await store.Advanced.Clean.DeleteAllEventDataAsync();
 
         // Deletes all of the persisted User documents
-        store.Advanced.Clean.DeleteDocumentsByTypeAsync(typeof(User));
+        await store.Advanced.Clean.DeleteDocumentsByTypeAsync(typeof(User));
 
         // For cases where you may want to keep some document types,
         // but eliminate everything else. This is here specifically to support
         // automated testing scenarios where you have some static data that can
         // be safely reused across tests
-        store.Advanced.Clean.DeleteDocumentsExceptAsync(typeof(Company), typeof(User));
+        await store.Advanced.Clean.DeleteDocumentsExceptAsync(typeof(Company), typeof(User));
     }
 
     #endregion
