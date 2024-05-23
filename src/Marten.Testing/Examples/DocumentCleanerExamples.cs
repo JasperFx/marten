62663cece40cfc03201e0a1decd63398557778eb
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2024-05-23 10:53:24 +0200

    docs: show usage of async methods instead of obsolete synchronous versions

--- src/Marten.Testing/Examples/DocumentCleanerExamples.cs
+++ src/Marten.Testing/Examples/DocumentCleanerExamples.cs
@@ -9,22 +9,22 @@ public class DocumentCleanerExamples
     {
         // Completely remove all the database schema objects related
         // to the User document type
-        store.Advanced.Clean.CompletelyRemove(typeof(User));
+        store.Advanced.Clean.CompletelyRemoveAsync(typeof(User));
 
         // Tear down and remove all Marten related database schema objects
-        store.Advanced.Clean.CompletelyRemoveAll();
+        store.Advanced.Clean.CompletelyRemoveAllAsync();
 
         // Deletes all the documents stored in a Marten database
-        store.Advanced.Clean.DeleteAllDocuments();
+        store.Advanced.Clean.DeleteAllDocumentsAsync();
 
         // Deletes all of the persisted User documents
-        store.Advanced.Clean.DeleteDocumentsByType(typeof(User));
+        store.Advanced.Clean.DeleteDocumentsByTypeAsync(typeof(User));
 
         // For cases where you may want to keep some document types,
         // but eliminate everything else. This is here specifically to support
         // automated testing scenarios where you have some static data that can
         // be safely reused across tests
-        store.Advanced.Clean.DeleteDocumentsExcept(typeof(Company), typeof(User));
+        store.Advanced.Clean.DeleteDocumentsExceptAsync(typeof(Company), typeof(User));
     }
 
     #endregion
