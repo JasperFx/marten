96ed30848d122894206eb4ec7e96dcb6366555da
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-06-07 08:36:59 -0500

    Mild refactoring of the identity determination to move it to StoreOptions for later

---
+++ src/ValueTypeTests/TestingTypes.cs
@@ -0,0 +1,27 @@
+using System;
+using JasperFx.CodeGeneration.Frames;
+using Vogen;
+
+namespace ValueTypeTests;
+
+[ValueObject<Guid>]
+public partial struct GuidId;
+
+[ValueObject<int>]
+public partial struct IntId;
+
+[ValueObject<long>]
+public partial struct LongId;
+
+[ValueObject<string>]
+public partial struct StringId;
+
+[ValueObject<DateOnly>]
+public partial struct DateId;
+
+public record NewGuidId(Guid Value);
+public record NewIntId(int Value);
+public record NewStringId(string Value);
+public record NewLongId(long Value);
+
+public record NewDateId(DateOnly Value);
