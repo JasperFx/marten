7623960fc80ec7c6d6713d45c7ab368f34b8a10f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-07-19 15:07:19 -0500

    first positive step toward batching more requests in event appending

--- src/Marten/Events/IEventAppender.cs
+++ src/Marten/Events/IEventAppender.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Marten.Events.Projections;
 using Marten.Internal.Sessions;
+using Marten.Services.BatchQuerying;
 
 namespace Marten.Events;
+
+public interface IEventAppendingStep
+{
+    ValueTask ApplyAsync(DocumentSessionBase session, EventGraph eventGraph, Queue<long> sequences,
+        IEventStorage storage,
+        CancellationToken cancellationToken);
+}
+
+public interface IEventAppendPreProcessor
+{
+    IEventAppendingStep TryPreFetch(IBatchedQuery query, DocumentSessionBase session, StreamAction[] actions);
+}
 
 internal interface IEventAppender
 {
