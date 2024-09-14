0d632e5fa0d019cb50952d36c37be285a2afdd50
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-09-14 15:48:07 -0500

    Test for application of migrations while adding partitions. Includes upgrade to Weasel 7.9

---
+++ src/CoreTests/Partitioning/partitioning_migrations.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using Marten.Testing.Documents;
+using Marten.Testing.Harness;
+using Shouldly;
+using Weasel.Core;
+using Xunit;
+
+namespace CoreTests.Partitioning;
+
+public class partitioning_migrations : OneOffConfigurationsContext
+{
+    [Fact]
+    public async Task should_create_delta_for_adding_partitioning_period()
+    {
+        StoreOptions(opts => opts.Schema.For<Target>().MultiTenanted());
+
+        await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
+
+        var store2 = SeparateStore(opts =>
+        {
+            opts.Schema.For<Target>().MultiTenantedWithPartitioning(x => x.ByRange()
+                .AddRange("one", "t1", "t2")
+                .AddRange("two", "t3", "t4"));
+        });
+
+        var migration = await store2.Storage.CreateMigrationAsync();
+        migration.Difference.ShouldBe(SchemaPatchDifference.Update);
+    }
+
+    [Fact]
+    public async Task should_create_delta_for_adding_all_new_partitions()
+    {
+        StoreOptions(opts =>
+        {
+            opts.Schema.For<Target>().MultiTenantedWithPartitioning(x => x.ByRange()
+                .AddRange("one", "t1", "t2")
+                .AddRange("two", "t3", "t4"));
+        });
+
+        await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
+
+        var store2 = SeparateStore(opts =>
+        {
+            opts.Schema.For<Target>().MultiTenantedWithPartitioning(x => x.ByRange()
+                .AddRange("one", "t1", "t2")
+                .AddRange("two", "t3", "t4")
+                .AddRange("three", "t5", "t6")
+
+            );
+        });
+
+        var migration = await store2.Storage.CreateMigrationAsync();
+        migration.Difference.ShouldBe(SchemaPatchDifference.Update);
+    }
+}
