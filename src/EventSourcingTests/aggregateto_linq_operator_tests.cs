b3de518a6a9d2b75c3e64b93ed9950807afc43c0
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-09-15 19:03:13 +0200

    AggregateTo sets the aggregate identity. Closes GH-2891

--- src/EventSourcingTests/aggregateto_linq_operator_tests.cs
+++ src/EventSourcingTests/aggregateto_linq_operator_tests.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventSourcingTests.Projections;
 using Marten.Events;
 using Marten.Testing.Harness;
+using Shouldly;
 using Xunit;
 
 namespace EventSourcingTests;
@@ -79,6 +81,33 @@ public class aggregateTo_linq_operator_tests: DestructiveIntegrationContext
         var questParty = await theSession.Events.QueryAllRawEvents().AggregateToAsync(initialParty);
 
         questParty.Members.ShouldHaveTheSameElementsAs("Lan", "Rand", "Matrim", "Perrin", "Elayne", "Elmindreda");
+    }
+
+    [Fact]
+    public async Task gets_the_id_set()
+    {
+        var initialParty = new QuestParty { Members = new List<string> { "Lan" } };
+        var id = theSession.Events.StartStream<Quest>(_joined1, _departed1).Id;
+        theSession.Events.StartStream<Quest>(_joined2, _departed2);
+        await theSession.SaveChangesAsync();
+
+        var questParty = await theSession.Events.QueryAllRawEvents().Where(x => x.StreamId == id).AggregateToAsync(initialParty);
+        questParty.Id.ShouldBe(id);
+    }
+
+    [Fact]
+    public async Task gets_the_key_set()
+    {
+        UseStreamIdentity(StreamIdentity.AsString);
+
+        var key = Guid.NewGuid().ToString();
+
+        theSession.Events.StartStream<Quest>(key, _joined1, _departed1);
+        theSession.Events.StartStream<Quest>(Guid.NewGuid().ToString(),_joined2, _departed2);
+        await theSession.SaveChangesAsync();
+
+        var questParty = await theSession.Events.QueryAllRawEvents().Where(x => x.StreamKey == key).AggregateToAsync<QuestPartyWithStringIdentifier>(null);
+        questParty.Id.ShouldBe(key);
     }
 
     public aggregateTo_linq_operator_tests(DefaultStoreFixture fixture): base(fixture)
