caa89558b68763eb1b1de86cfacdfed53a0f822e
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-10-23 08:36:31 -0500

    Trying to better isolate flaky tests

--- src/Marten.Testing/Harness/DestructiveIntegrationContext.cs
+++ src/Marten.Testing/Harness/DestructiveIntegrationContext.cs
@@ -6,7 +6,6 @@ namespace Marten.Testing.Harness
     /// Use this base class if a test fixture needs to do anything
     /// destructive to the default database schema
     /// </summary>
-    [Collection("integration")]
     public class DestructiveIntegrationContext: IntegrationContext
     {
         public DestructiveIntegrationContext(DefaultStoreFixture fixture) : base(fixture)
