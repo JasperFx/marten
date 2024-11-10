3d4354634b4ca86d8dcd0584d55c5422a7e9b5f9
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-10 11:36:11 -0600

    Moved ValueTypeInfo up into JasperFx

--- src/Marten/Events/Aggregation/ByStreamId.cs
+++ src/Marten/Events/Aggregation/ByStreamId.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JasperFx.Core.Reflection;
 using JasperFx.Events;
 using JasperFx.Events.Grouping;
 using Marten.Events.Projections;
--- src/Marten/Internal/ValueTypeIdentifiedDocumentStorage.cs
+++ src/Marten/Internal/ValueTypeIdentifiedDocumentStorage.cs
@@ -1,24 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using FastExpressionCompiler;
 using JasperFx.Core.Reflection;
-using JasperFx.Events;
-using Marten.Events;
-using Marten.Internal.Operations;
 using Marten.Internal.Storage;
 using Marten.Linq;
 using Marten.Linq.Members;
-using Marten.Linq.Members.ValueCollections;
 using Marten.Linq.QueryHandlers;
 using Marten.Linq.Selectors;
 using Marten.Linq.SqlGeneration;
-using Marten.Services;
 using Marten.Storage;
 using Npgsql;
 using Weasel.Core;
@@ -27,108 +18,6 @@ using Weasel.Postgresql;
 using Weasel.Postgresql.SqlGeneration;
 
 namespace Marten.Internal;
-
-/// <summary>
-/// Internal model of a custom "wrapped" value type Marten uses
-/// for LINQ generation
-/// </summary>
-public class ValueTypeInfo
-{
-    private object _converter;
-    public Type OuterType { get; }
-    public Type SimpleType { get; }
-    public PropertyInfo ValueProperty { get; }
-    public MethodInfo Builder { get; }
-    public ConstructorInfo Ctor { get; }
-
-    public ValueTypeInfo(Type outerType, Type simpleType, PropertyInfo valueProperty, ConstructorInfo ctor)
-    {
-        OuterType = outerType;
-        SimpleType = simpleType;
-        ValueProperty = valueProperty;
-        Ctor = ctor;
-    }
-
-    public ValueTypeInfo(Type outerType, Type simpleType, PropertyInfo valueProperty, MethodInfo builder)
-    {
-        OuterType = outerType;
-        SimpleType = simpleType;
-        ValueProperty = valueProperty;
-        Builder = builder;
-    }
-
-    public Func<TInner, TOuter> CreateConverter<TOuter, TInner>()
-    {
-        if (_converter != null)
-        {
-            return (Func<TInner, TOuter>)_converter;
-        }
-
-        var inner = Expression.Parameter(typeof(TInner), "inner");
-        Expression builder;
-        if (Builder != null)
-        {
-            builder = Expression.Call(null, Builder, inner);
-        }
-        else if (Ctor != null)
-        {
-            builder = Expression.New(Ctor, inner);
-        }
-        else
-        {
-            throw new NotSupportedException("Marten cannot build a type converter for strong typed id type " +
-                                            OuterType.FullNameInCode());
-        }
-
-        var lambda = Expression.Lambda<Func<TInner, TOuter>>(builder, inner);
-
-        _converter = lambda.CompileFast();
-        return (Func<TInner, TOuter>)_converter;
-    }
-
-    public Func<TOuter, TInner> ValueAccessor<TOuter, TInner>()
-    {
-        var outer = Expression.Parameter(typeof(TOuter), "outer");
-        var getter = ValueProperty.GetMethod;
-        var lambda = Expression.Lambda<Func<TOuter, TInner>>(Expression.Call(outer, getter), outer);
-        return lambda.CompileFast();
-    }
-
-    public Func<IEvent,TId> CreateAggregateIdentitySource<TId>() where TId : notnull
-    {
-        var e = Expression.Parameter(typeof(IEvent), "e");
-        var eMember = SimpleType == typeof(Guid)
-            ? ReflectionHelper.GetProperty<IEvent>(x => x.StreamId)
-            : ReflectionHelper.GetProperty<IEvent>(x => x.StreamKey);
-
-        var raw = Expression.Call(e, eMember.GetMethod);
-        Expression wrapped = null;
-        if (Builder != null)
-        {
-            wrapped = Expression.Call(null, Builder, raw);
-        }
-        else if (Ctor != null)
-        {
-            wrapped = Expression.New(Ctor, raw);
-        }
-        else
-        {
-            throw new NotSupportedException("Marten cannot build a type converter for strong typed id type " +
-                                            OuterType.FullNameInCode());
-        }
-
-        var lambda = Expression.Lambda<Func<IEvent, TId>>(wrapped, e);
-
-        return lambda.CompileFast();
-    }
-}
-
-internal class ValueTypeElementMember: ElementMember
-{
-    public ValueTypeElementMember(Type declaringType, Type reflectedType) : base(declaringType, reflectedType)
-    {
-    }
-}
 
 internal class ValueTypeIdentifiedDocumentStorage<TDoc, TSimple, TValueType>: IDocumentStorage<TDoc, TSimple>
 {
