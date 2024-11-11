b3c256b5e6b9780d25efe6aaa118b3f491ba43ad
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-11 07:59:50 -0600

    Moved IReadOnlyProjectionData to JasperFx

--- src/Marten/Events/EventGraph.cs
+++ src/Marten/Events/EventGraph.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using JasperFx.Core;
 using JasperFx.Core.Reflection;
 using JasperFx.Events;
+using JasperFx.Events.Projections;
 using Marten.Events.Aggregation;
 using Marten.Events.Daemon;
 using Marten.Events.Daemon.Resiliency;
--- src/Marten/Events/IReadOnlyEventStoreOptions.cs
+++ src/Marten/Events/IReadOnlyEventStoreOptions.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using System.Collections.Generic;
 using JasperFx.Events;
+using JasperFx.Events.Projections;
 using Marten.Events.Aggregation;
 using Marten.Events.Daemon;
 using Marten.Events.Projections;
