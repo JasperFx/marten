fd115a0e04807ab428fa4c058337c54fcaaa1fdb
nkosi23 <hzupgp0ul@mozmail.com>
2024-12-11 13:02:17 +0100

    - Added additional F# unit tests for good measure - Required putting the Target type in a separate shared library to avoid a circular dependency - Moved Fsharp acceptance tests to different test classes to avoid affecting other tests due to the specific serializer used for F#

--- src/FSharpTypes/Library.fs
+++ src/FSharpTypes/Library.fs
@@ -1,6 +1,9 @@
 ﻿module FSharpTypes
 
 open System
+open System.Linq.Expressions
+open Marten.Testing.Documents
+open Microsoft.FSharp.Linq.RuntimeHelpers
 
 type OrderId = Id of Guid
 
@@ -14,3 +17,24 @@ type RecordTypeOrderId = { Part1: string; Part2: string }
 
 type ArbitraryClass() =
     member this.Value = "ok"
+
+let rec stripFSharpFunc (expression: Expression) =
+    match expression with
+    | :? MethodCallExpression as callExpression when callExpression.Method.Name = "ToFSharpFunc" ->
+        stripFSharpFunc callExpression.Arguments.[0]
+    | _ -> expression
+
+let toLinqExpression expr  =
+    expr
+    |> LeafExpressionConverter.QuotationToExpression
+    |> stripFSharpFunc
+    |> unbox<System.Linq.Expressions.Expression<System.Func<Target, bool>>>
+let greaterThanWithFsharpDateOption =
+    <@ fun (o1: Target) -> o1.FSharpDateTimeOffsetOption >= Some DateTimeOffset.UtcNow  @> |> toLinqExpression
+let lesserThanWithFsharpDateOption = <@ (fun (o1: Target) -> o1.FSharpDateTimeOffsetOption <= Some DateTimeOffset.UtcNow ) @> |> toLinqExpression
+let greaterThanWithFsharpDecimalOption = <@ (fun (o1: Target) -> o1.FSharpDecimalOption >= Some 5m ) @> |> toLinqExpression
+let lesserThanWithFsharpDecimalOption = <@ (fun (o1: Target) -> o1.FSharpDecimalOption <= Some 5m ) @> |> toLinqExpression
+let greaterThanWithFsharpStringOption = <@ (fun (o1: Target) -> o1.FSharpStringOption >= Some "MyString" ) @> |> toLinqExpression
+let lesserThanWithFsharpStringOption = <@ (fun (o1: Target) -> o1.FSharpStringOption <= Some "MyString" ) @> |> toLinqExpression
+
+
--- src/LinqTests/Acceptance/Support/DefaultQueryFixture.cs
+++ src/LinqTests/Acceptance/Support/DefaultQueryFixture.cs
@@ -10,13 +10,7 @@ public class DefaultQueryFixture: TargetSchemaFixture
 {
     public DefaultQueryFixture()
     {
-        Store = ProvisionStore("linq_querying", options =>
-        {
-            options.RegisterFSharpOptionValueTypes();
-            var serializerOptions = JsonFSharpOptions.Default().WithUnwrapOption().ToJsonSerializerOptions();
-            options.UseSystemTextJsonForSerialization(serializerOptions);
-        });
-
+        Store = ProvisionStore("linq_querying");
 
         DuplicatedFieldStore = ProvisionStore("duplicate_fields", o =>
         {
@@ -29,10 +23,30 @@ public class DefaultQueryFixture: TargetSchemaFixture
                 .Duplicate(x => x.Flag)
                 .Duplicate(x => x.Color)
                 .Duplicate(x => x.NumberArray);
+        });
 
-            o.RegisterFSharpOptionValueTypes();
+        FSharpFriendlyStore = ProvisionStore("linq_querying", options =>
+        {
+            options.RegisterFSharpOptionValueTypes();
             var serializerOptions = JsonFSharpOptions.Default().WithUnwrapOption().ToJsonSerializerOptions();
-            o.UseSystemTextJsonForSerialization(serializerOptions);
+            options.UseSystemTextJsonForSerialization(serializerOptions);
+        });
+
+        FSharpFriendlyStoreWithDuplicatedField = ProvisionStore("duplicate_fields", options =>
+        {
+            options.Schema.For<Target>()
+                .Duplicate(x => x.Number)
+                .Duplicate(x => x.Long)
+                .Duplicate(x => x.String)
+                .Duplicate(x => x.Date)
+                .Duplicate(x => x.Double)
+                .Duplicate(x => x.Flag)
+                .Duplicate(x => x.Color)
+                .Duplicate(x => x.NumberArray);
+
+            options.RegisterFSharpOptionValueTypes();
+            var serializerOptions = JsonFSharpOptions.Default().WithUnwrapOption().ToJsonSerializerOptions();
+            options.UseSystemTextJsonForSerialization(serializerOptions);
         });
 
         SystemTextJsonStore = ProvisionStore("stj_linq", o =>
@@ -45,5 +59,19 @@ public class DefaultQueryFixture: TargetSchemaFixture
 
     public DocumentStore DuplicatedFieldStore { get; set; }
 
+    public DocumentStore FSharpFriendlyStore { get; set; }
+    public DocumentStore FSharpFriendlyStoreWithDuplicatedField { get; set; }
+
     public DocumentStore Store { get; set; }
+}
+
+public static class DefaultQueryFixtureExtensions
+{
+    public static void UseFSharp(this DocumentStore store)
+    {
+        var o = store.Options;
+        o.RegisterFSharpOptionValueTypes();
+        var serializerOptions = JsonFSharpOptions.Default().WithUnwrapOption().ToJsonSerializerOptions();
+        o.UseSystemTextJsonForSerialization(serializerOptions);
+    }
 }
