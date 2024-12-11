d4a712b497e46d81d5a9c2f75764a0387829f19c
nkosi23 <hzupgp0ul@mozmail.com>
2024-12-11 01:12:56 +0100

    Added support for nullable strong typed ids (eg: Issue2d?)

--- src/Marten/Util/GenericsExtensions.cs
+++ src/Marten/Util/GenericsExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Marten.Util;
@@ -8,6 +10,39 @@ internal static class GenericsExtensions
     public static bool IsGenericInterfaceImplementation(this object obj, Type openInterfaceType)
     {
         return obj.GetType().GetInterfaces().Any(x => x.IsGenericType && openInterfaceType.IsAssignableFrom(x.GetGenericTypeDefinition()));
+    }
+
+    public static object UnwrapIEnumerableOfNullables(this object obj)
+    {
+        var type = obj.GetType();
+        if (type.GetInterfaces().Any(i =>
+                i.IsGenericType &&
+                i.GetGenericTypeDefinition() == typeof(IEnumerable<>) &&
+                i.GetGenericArguments()[0].IsGenericType &&
+                i.GetGenericArguments()[0].GetGenericTypeDefinition() == typeof(Nullable<>)))
+        {
+            // Get the underlying type of the Nullable<T>
+            var underlyingType = type.GetInterfaces()
+                .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                .GetGenericArguments()[0]
+                .GetGenericArguments()[0];
+
+            // Cast to IEnumerable<Nullable<T>> and filter for non-null values
+            var unwrappedValues = ((IEnumerable)obj)
+                .Cast<object>()
+                .Where(x => x != null)
+                .Select(x => x.GetType().GetProperty("Value").GetValue(x));
+
+            // Create an array of the underlying type
+            obj = Array.CreateInstance(underlyingType, unwrappedValues.Count());
+            var stronglyTypedValues = unwrappedValues.Select(x => Activator.CreateInstance(underlyingType,x)).ToArray();
+            Array.Copy(stronglyTypedValues.ToArray(), (Array)obj, unwrappedValues.Count());
+
+            // Update the type after unwrapping
+            return obj;
+        }
+
+        return obj;
     }
 
     public static object CallGenericInterfaceMethod(this object obj, Type openInterfaceType, string methodName, params object[] parameters)
