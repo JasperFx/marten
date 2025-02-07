880a2ef8015327c0b3acb2d5d432feb2a17fc86a
Jozef Jagla <jozef.jagla@netigate.se>
2025-02-07 16:07:53 +0100

    ProjectionWrapper honoring settings of projections based on EventFilterable class.

---
+++ src/DaemonTests/Bugs/Bug_projetion_base_settings_not_honored_by_projection_wrapper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Marten;
+using Marten.Events;
+using Marten.Events.Projections;
+using Marten.Testing.Harness;
+using Shouldly;
+using Xunit;
+
+namespace DaemonTests.Bugs;
+
+public class Bug_projection_wrapper_should_honor_projetionbase_settings : BugIntegrationContext
+{
+    [Theory]
+    [InlineData(false, typeof(EventA))]
+    [InlineData(true, typeof(EventA))]
+    [InlineData(false, null)]
+    [InlineData(true, null)]
+    public void projection_wrapper_honor_projectionbase_settings(bool includeArchived, Type? filteredEventType)
+    {
+        Type[] filteredTypeArray = filteredEventType is null ? [] : [filteredEventType];
+
+        var projection = new FilterableProjection(includeArchived, filteredTypeArray);
+        var wrapper = new ProjectionWrapper(projection, ProjectionLifecycle.Async);
+        var shard = (wrapper as IProjectionSource).AsyncProjectionShards(theStore);
+
+        shard.First().IncludeArchivedEvents.ShouldBe(includeArchived);
+        shard.First().EventTypes.ShouldBe(filteredTypeArray);
+    }
+
+    private class EventA
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal class FilterableProjection: ProjectionBase, IProjection
+    {
+        public FilterableProjection(bool includeArchivedEvents, params Type[] includedEventTypes)
+        {
+            IncludeArchivedEvents = includeArchivedEvents;
+            if (includedEventTypes.Any())
+            {
+                IncludedEventTypes.AddRange(includedEventTypes);
+            }
+        }
+
+        public void Apply(IDocumentOperations operations, IReadOnlyList<StreamAction> streams)
+        {
+        }
+
+        public Task ApplyAsync(IDocumentOperations operations, IReadOnlyList<StreamAction> streams, CancellationToken cancellation)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
