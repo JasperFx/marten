41d3712b850fff672db107adb3d891009f2d91ee
Søren Trudsø Mahon <strudso@gmail.com>
2025-03-10 18:15:03 +0100

    Created a test for defaultschema

--- src/LinqTests/Bugs/Bug_xxx_NGram_in_other_schema.cs
+++ src/LinqTests/Bugs/Bug_xxx_NGram_in_other_schema.cs
@@ -11,12 +11,34 @@ namespace LinqTests.Bugs;
 public class Bug_xxx_NGram_in_other_schema : BugIntegrationContext
 {
     [Fact]
-    public async Task use_the_query()
+    public async Task not_public_schema()
     {
         StoreOptions(opts =>
         {
             var documentMappingExpression = opts.Schema.For<DifferentSchema>();
             documentMappingExpression.DatabaseSchemaName("not_public");
+            documentMappingExpression.NgramIndex(x => x.Name);
+        });
+
+        var elayne = new Foo { Name = "Elayne", Status = StatusEnum.Active };
+        theSession.Store(elayne);
+        theSession.Store(new DifferentSchema{ Name = "This is something i would like to find using NGRAM"});
+        await theSession.SaveChangesAsync();
+
+        var results = await theSession.Query<DifferentSchema>().Where(x => x.Name.NgramSearch("ing")).ToListAsync();
+
+        results.Any().ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task change_default_schema_to_not_public()
+    {
+        StoreOptions(opts =>
+        {
+            var optsDatabaseSchemaName = "not_public";
+            opts.DatabaseSchemaName = optsDatabaseSchemaName;
+            var documentMappingExpression = opts.Schema.For<DifferentSchema>();
+            documentMappingExpression.DatabaseSchemaName(optsDatabaseSchemaName);
             documentMappingExpression.NgramIndex(x => x.Name);
         });
 
