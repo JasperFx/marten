344d0dd32082d46138c404c49ec652392bbe6d1e
Jakob Andersen <jta@jakobandersen.com>
2025-03-13 14:32:13 +0100

    NGramSearch support for non ascii characters

--- src/DocumentDbTests/Indexes/NgramSearchTests.cs
+++ src/DocumentDbTests/Indexes/NgramSearchTests.cs
@@ -153,4 +153,41 @@ public class NgramSearchTests : Marten.Testing.Harness.OneOffConfigurationsConte
         result.ShouldHaveSingleItem();
         ShouldBeStringTestExtensions.ShouldContain(result[0].Address.Line1, term);
     }
+
+    [Fact]
+    public async Task test_ngram_when_using_non_ascii_characters(){
+         var store = DocumentStore.For(_ =>
+        {
+            _.Connection(Marten.Testing.Harness.ConnectionSource.ConnectionString);
+            _.DatabaseSchemaName = "ngram_test";
+            _.Schema.For<User>().NgramIndex(x => x.UserName);
+        });
+
+        await using var session = store.LightweightSession();
+
+
+        //The ngram uðmu should only exist in bjork, if special characters ignored it will return Umut
+        var umut = new User(1, "Umut Aral");
+        var bjork = new User(2, "Björk Guðmundsdóttir");
+
+        //The ngram øre should only exist in bjork, if special characters ignored it will return Chris Rea
+        var kierkegaard = new User(3, "Søren Kierkegaard");
+        var rea = new User(4, "Chris Rea");
+
+        session.Store(umut);
+        session.Store(bjork);
+        session.Store(kierkegaard);
+        session.Store(rea);
+        
+        await session.SaveChangesAsync();
+
+        var result = await session
+            .Query<User>()
+            .Where(x => x.UserName.NgramSearch("uðmu") || x.UserName.NgramSearch("øre"))
+            .ToListAsync();
+
+        result.Count.ShouldBe(2);
+        result.ShouldContain(x => x.UserName == kierkegaard.UserName);
+        result.ShouldContain(x => x.UserName == bjork.UserName);
+    }
 }
