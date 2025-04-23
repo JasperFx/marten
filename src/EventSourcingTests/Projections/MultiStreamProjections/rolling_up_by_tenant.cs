38e8ca03bb3e92d13f33377dc3e9c4159b23a086
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-04-23 09:48:09 -0500

    Extra test for rolling up a multi stream projection by tenant id using a strong typed identifier because you know someone will insist on doing that

--- src/EventSourcingTests/Projections/MultiStreamProjections/rolling_up_by_tenant.cs
+++ src/EventSourcingTests/Projections/MultiStreamProjections/rolling_up_by_tenant.cs
@@ -51,6 +51,44 @@ public class rolling_up_by_tenant : OneOffConfigurationsContext
         (await theSession.LoadAsync<Rollup>("two")).ACount.ShouldBe(3);
         (await theSession.LoadAsync<Rollup>("three")).ACount.ShouldBe(4);
     }
+
+    [Fact]
+    public async Task track_totals_by_tenant_id_using_strong_typed_id()
+    {
+        StoreOptions(opts =>
+        {
+            opts.Events.TenancyStyle = TenancyStyle.Conjoined;
+            opts.Projections.Add<Rollup2Projection>(ProjectionLifecycle.Async);
+            opts.Events.EnableGlobalProjectionsForConjoinedTenancy = true;
+        });
+
+        var session1 = theStore.LightweightSession("one");
+        session1.Events.StartStream(new AEvent(), new BEvent());
+        session1.Events.StartStream(new BEvent(), new BEvent());
+        session1.Events.StartStream(new BEvent(), new BEvent());
+        await session1.SaveChangesAsync();
+
+        var session2 = theStore.LightweightSession("two");
+        session2.Events.StartStream(new AEvent(), new AEvent());
+        session2.Events.StartStream(new BEvent(), new AEvent());
+        session2.Events.StartStream(new BEvent(), new BEvent());
+        await session2.SaveChangesAsync();
+
+        var session3 = theStore.LightweightSession("three");
+        session3.Events.StartStream(new AEvent(), new AEvent());
+        session3.Events.StartStream(new AEvent(), new AEvent());
+        session3.Events.StartStream(new BEvent(), new BEvent());
+        await session3.SaveChangesAsync();
+
+        using var daemon = await theStore.BuildProjectionDaemonAsync();
+        await daemon.StartAllAsync();
+
+        await daemon.WaitForNonStaleData(30.Seconds());
+
+        (await theSession.LoadAsync<Rollup2>( new TenantId("one"))).ACount.ShouldBe(1);
+        (await theSession.LoadAsync<Rollup2>(new TenantId("two"))).ACount.ShouldBe(3);
+        (await theSession.LoadAsync<Rollup2>(new TenantId("three"))).ACount.ShouldBe(4);
+    }
 }
 
 #region sample_rollup_projection_by_tenant_id
@@ -76,3 +114,26 @@ public class Rollup
 }
 
 #endregion
+
+public record struct TenantId(string Value);
+
+public class Rollup2Projection: MultiStreamProjection<Rollup2, TenantId>
+{
+    public Rollup2Projection()
+    {
+        // This opts into doing the event slicing by tenant id
+        RollUpByTenant();
+    }
+
+    public void Apply(Rollup2 state, AEvent e) => state.ACount++;
+    public void Apply(Rollup2 state, BEvent e) => state.BCount++;
+}
+
+public class Rollup2
+{
+    [Identity]
+    public TenantId TenantId { get; set; }
+    public int ACount { get; set; }
+    public int BCount { get; set; }
+}
+
