e5f853abd02bfc0b66846df8030b992a2a18db9f
nkosi23 <hzupgp0ul@mozmail.com>
2025-04-28 10:28:11 +0200

    restored unintended minor changes

--- src/Marten.Testing/Harness/ConnectionSource.cs
+++ src/Marten.Testing/Harness/ConnectionSource.cs
@@ -10,7 +10,7 @@ public class ConnectionSource: ConnectionFactory
     // Keep the default timeout pretty short
     public static readonly string ConnectionString = Environment.GetEnvironmentVariable("marten_testing_database")
                                                      ??
-                                                     "Host=localhost;Port=5432;Database=marten_linq_testing;Username=postgres;password=postgres;Command Timeout=5";
+                                                     "Host=localhost;Port=5432;Database=marten_testing;Username=postgres;password=postgres;Command Timeout=5";
 
     static ConnectionSource()
     {
