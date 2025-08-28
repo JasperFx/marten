2ac679f26bf6fcebb5056fe36377b6a6cd934de4
Hakan Fıstık <hakam.fstk@gmail.com>
2025-08-28 18:23:48 +0200

    Convert readonly to const (when possible)

--- src/LinqTests/Internals/SimpleExpressionTests.cs
+++ src/LinqTests/Internals/SimpleExpressionTests.cs
@@ -16,8 +16,8 @@ public class SimpleExpressionTests
 {
     private static readonly DocumentQueryableMemberCollection Members = new(DocumentMapping.For<Target>(), new StoreOptions());
 
-    private static readonly int Age = 49;
-    private readonly string Color = "blue";
+    private const int Age = 49;
+    private const string Color = "blue";
 
     private SimpleExpression parse(Expression<Func<bool>> expression)
     {
