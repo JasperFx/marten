50447864ca70c151a50255ddb3fb27220f100f9d
Anne Erdtsieck <anne.erdtsieck@topicus.nl>
2025-10-23 17:25:43 +0200

    Support distinct queries for enums and nullable enums

--- src/Marten/Linq/Selectors/NullableSelector.cs
+++ src/Marten/Linq/Selectors/NullableSelector.cs
@@ -5,17 +5,12 @@ using System.Threading.Tasks;
 namespace Marten.Linq.Selectors;
 
 internal sealed class NullableSelector<T>(ISelector<T> inner): ISelector<T?>
-    where T : struct
 {
-    public T? Resolve(DbDataReader reader)
-    {
-        if (reader.IsDBNull(0)) return null;
-        return inner.Resolve(reader);
-    }
+    public T? Resolve(DbDataReader reader) => reader.IsDBNull(0) ? default : inner.Resolve(reader);
 
     public async Task<T?> ResolveAsync(DbDataReader reader, CancellationToken token)
     {
-        if (await reader.IsDBNullAsync(0, token).ConfigureAwait(false)) return null;
+        if (await reader.IsDBNullAsync(0, token).ConfigureAwait(false)) return default;
         return await inner.ResolveAsync(reader, token).ConfigureAwait(false);
     }
 }
