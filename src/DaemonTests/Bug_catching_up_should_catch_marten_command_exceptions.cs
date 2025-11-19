69b00f4a5af6a580e783813aac9e73f285d75a06
Anne Erdtsieck <anne.erdtsieck@topicus.nl>
2025-11-19 13:41:05 +0100

    Daemon catch-up command should collect exceptions

--- src/DaemonTests/Bug_catching_up_should_catch_marten_command_exceptions.cs
+++ src/DaemonTests/Bug_catching_up_should_catch_marten_command_exceptions.cs
@@ -1,22 +1,19 @@
-using System;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using EventSourcingTests.Aggregation;
-using JasperFx.Core;
-using JasperFx.Events;
 using JasperFx.Events.Projections;
-using Marten.Events.Aggregation;
 using Marten.Exceptions;
 using Marten.Testing.Harness;
 using Shouldly;
 using Xunit;
-using ApplyEventException = JasperFx.Events.Daemon.ApplyEventException;
 
-namespace DaemonTests;
+namespace DaemonTests.Bugs;
 
-public class wait_for_non_stale_data_error_cases : OneOffConfigurationsContext
+public class Bug_catching_up: OneOffConfigurationsContext
 {
     [Fact]
-    public async Task get_a_good_timeout_exception()
+    public async Task should_catch_apply_event_exceptions()
     {
         StoreOptions(opts =>
         {
@@ -34,19 +31,16 @@ public class wait_for_non_stale_data_error_cases : OneOffConfigurationsContext
 
         using var daemon = await theStore.BuildProjectionDaemonAsync();
 
-        await daemon.StartAllAsync();
-
         var aggregated = await Should.ThrowAsync<AggregateException>(async () =>
         {
-            await daemon.WaitForNonStaleData(5.Seconds());
+            await daemon.CatchUpAsync(CancellationToken.None);
         });
 
-        aggregated.InnerExceptions[0].ShouldBeOfType<TimeoutException>();
-        aggregated.InnerExceptions[1].ShouldBeOfType<ApplyEventException>();
+        aggregated.InnerExceptions[0].ShouldBeOfType<JasperFx.Events.Daemon.ApplyEventException>();
     }
 
     [Fact]
-    public async Task get_a_good_command_exception()
+    public async Task should_catch_marten_command_exceptions()
     {
         StoreOptions(opts =>
         {
@@ -62,57 +56,16 @@ public class wait_for_non_stale_data_error_cases : OneOffConfigurationsContext
         await theSession.SaveChangesAsync();
 
         using var daemon = await theStore.BuildProjectionDaemonAsync();
-
-        await daemon.StartAllAsync();
-
         var aggregated = await Should.ThrowAsync<AggregateException>(async () =>
         {
-            await daemon.WaitForNonStaleData(5.Seconds());
+            await daemon.CatchUpAsync(CancellationToken.None);
         });
 
-        aggregated.InnerExceptions[0].ShouldBeOfType<TimeoutException>();
-        aggregated.InnerExceptions[1].ShouldBeOfType<MartenCommandException>();
+        aggregated.InnerExceptions[0].ShouldBeOfType<MartenCommandException>();
 
 
         var failOnSave = await theSession.LoadAsync<FailsOnSave>(failOnSaveId);
         failOnSave.ShouldBeNull();
     }
-}
 
-public record ThrowError(bool ShouldThrow);
-
-public class SometimesFailingLetterCountsProjection: SingleStreamProjection<LetterCounts, Guid>
-{
-    public override LetterCounts Evolve(LetterCounts snapshot, Guid id, IEvent e)
-    {
-        snapshot ??= new() { Id = id };
-        switch (e.Data)
-        {
-            case AEvent _:
-                snapshot.ACount++;
-                break;
-            case BEvent _:
-                snapshot.BCount++;
-                break;
-            case CEvent _:
-                snapshot.CCount++;
-                break;
-            case DEvent _:
-                snapshot.DCount++;
-                break;
-            case ThrowError x:
-                if (x.ShouldThrow) throw new("You stink!");
-                break;
-        }
-
-        return snapshot;
-    }
-}
-
-public record FailsOnSave(Guid Id, string ShouldNotBeNull);
-public record FailsOnSaveEventA;
-
-public class FailsOnSaveProjection: SingleStreamProjection<FailsOnSave, Guid>
-{
-    public static FailsOnSave Create(IEvent<FailsOnSaveEventA> @event) => new(@event.StreamId, null!);
 }
