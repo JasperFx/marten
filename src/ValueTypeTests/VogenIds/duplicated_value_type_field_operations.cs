e21af61afd683d20b4152342c2e9193582511673
Luis Villalaz <18101480+shield1739@users.noreply.github.com>
2026-04-21 10:59:32 -0500

    Adds Duplicated ValueType Field Vogen tests

--- src/ValueTypeTests/VogenIds/duplicated_value_type_field_operations.cs
+++ src/ValueTypeTests/VogenIds/duplicated_value_type_field_operations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using JasperFx.CodeGeneration;
 using JasperFx.Core;
@@ -48,6 +49,17 @@ public class duplicated_value_type_field_operations : IDisposable, IAsyncDisposa
     }
 
     [Fact]
+    public async Task store_a_document_smoke_test()
+    {
+        var duplicatedDoc = new DuplicateValueTypeDoc(){DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        theSession.Store(duplicatedDoc);
+
+        await theSession.SaveChangesAsync();
+
+        (await theSession.Query<DuplicateValueTypeDoc>().AnyAsync()).ShouldBeTrue();
+    }
+
+    [Fact]
     public async Task load_document()
     {
         var duplicatedDoc = new DuplicateValueTypeDoc
@@ -61,6 +73,57 @@ public class duplicated_value_type_field_operations : IDisposable, IAsyncDisposa
 
         (await theSession.LoadAsync<DuplicateValueTypeDoc>(duplicatedDoc.Id))
             .DuplicateValueType.ShouldBe(duplicatedDoc.DuplicateValueType);
+    }
+
+    [Fact]
+    public async Task load_many()
+    {
+        var duplicatedDoc1 = new DuplicateValueTypeDoc{DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        var duplicatedDoc2 = new DuplicateValueTypeDoc{DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        var duplicatedDoc3 = new DuplicateValueTypeDoc{DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        theSession.Store(duplicatedDoc1, duplicatedDoc2, duplicatedDoc3);
+
+        await theSession.SaveChangesAsync();
+
+        var results = await theSession.Query<DuplicateValueTypeDoc>().Where(x => x.Id.IsOneOf(duplicatedDoc1.Id, duplicatedDoc2.Id, duplicatedDoc3.Id)).ToListAsync();
+        results.Count.ShouldBe(3);
+    }
+
+    [Fact]
+    public async Task use_in_LINQ_where_clause()
+    {
+        var duplicatedDoc = new DuplicateValueTypeDoc{DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        theSession.Store(duplicatedDoc);
+
+        await theSession.SaveChangesAsync();
+
+        var loaded = await theSession.Query<DuplicateValueTypeDoc>()
+            .FirstOrDefaultAsync(x => x.DuplicateValueType == duplicatedDoc.DuplicateValueType);
+
+        loaded.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async Task use_in_LINQ_duplicatedDoc_clause()
+    {
+        var duplicatedDoc = new DuplicateValueTypeDoc{DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        theSession.Store(duplicatedDoc);
+
+        await theSession.SaveChangesAsync();
+
+        var loaded = await theSession.Query<DuplicateValueTypeDoc>().OrderBy(x => x.Id).Take(3).ToListAsync();
+    }
+
+    [Fact]
+    public async Task use_in_LINQ_select_clause()
+    {
+        var duplicatedDoc = new DuplicateValueTypeDoc{DuplicateValueType = DuplicateValueType.From(Guid.NewGuid())};
+        theSession.Store(duplicatedDoc);
+
+        await theSession.SaveChangesAsync();
+
+        var loaded = await theSession.Query<DuplicateValueTypeDoc>().Select(x => x.Id).Take(3).ToListAsync();
+
     }
 }
 
